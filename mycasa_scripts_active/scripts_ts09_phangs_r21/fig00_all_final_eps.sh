776bd7e3de1575609b29b45e319d2a6596bb9fbf
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-02-20 12:55:00 +0100

    Commit at 2020-02-20 12:55:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/fig00_all_final_eps.sh
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/fig00_all_final_eps.sh
@@ -1,5 +1,6 @@
 dir_data="/Users/saito/data/myproj_active/proj_ts09_phangs_r21/eps/"
 
+
 ### figure 1
 convert -crop 700x570+75+20 $dir_data"ngc4321_12co10_m0.png" $dir_data"ngc4321_12co10_m0b.png"
 convert -crop 700x570+75+20 $dir_data"ngc4321_12co21_m0.png" $dir_data"ngc4321_12co21_m0b.png"
@@ -17,5 +18,22 @@ rm -rf $dir_data"ngc4321_r21.png" $dir_data"ngc4321_r21b.png"
 rm -rf $dir_data"ngc4321_r21_m8.png" $dir_data"ngc4321_r21_m8b.png"
 rm -rf $dir_data"fig01a.png" $dir_data"fig01b.png"
 
+
 ### figure 2
 convert +append -border 0x0 $dir_data"missingflux_co10.png" $dir_data"missingflux_co21.png" $dir_data"missingflux_r21.png" eps2:$dir_data"f02.eps"
+
+rm -rf $dir_data"missingflux_co10.png" $dir_data"missingflux_co21.png" $dir_data"missingflux_r21.png"
+
+
+### figure 3
+convert -append -border 0x0 $dir_data"ngc4321_scatter_co10_co21.png" $dir_data"ngc4321_scatter_co21_r21.png" eps2:$dir_data"f03.eps"
+
+rm -rf $dir_data"ngc4321_scatter_co10_co21.png" $dir_data"ngc4321_scatter_co21_r21.png"
+
+
+### figure 4
+### figure 5
+
+
+### figure 6
+convert -crop 700x570+75+20 $dir_data"ngc4321_12co10_m0.png" $dir_data"ngc4321_12co10_m0b.png"
