ebe7836ef7ce76afa1de9504297bc82a0162f1c9
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-02-20 12:05:00 +0100

    Commit at 2020-02-20 12:05:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/myim08_r21_mask.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/myim08_r21_mask.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import numpy as np
 import scripts_phangs_r21 as r21
 
 
@@ -17,45 +18,16 @@ percent = 0.01
 for i in range(len(galaxy)):
     # import data for r21 map
     galname = galaxy[i]
-    co10images = glob.glob(dir_proj + galname + "_*/co10*.moment0")
-    co10images.extend(glob.glob(dir_proj + galname + "_*/co10*.moment8"))
-    co10images.sort()
-    co21images = glob.glob(dir_proj + galname + "_*/co21*.moment0")
-    co21images.extend(glob.glob(dir_proj + galname + "_*/co21*.moment8"))
-    co21images.sort()
+    r21images = glob.glob(dir_proj + galname + "_r21/r21*.moment0")
+    r21images.sort()
+    r21image = r21images[0]
 
-    # mkdir
-    dir_ratio = dir_proj + galname + "_r21/"
-    os.system("rm -rf " + dir_ratio)
-    os.mkdir(dir_ratio)
+    shape = imhead(r21image,mode="list")["shape"]
+    box = "0,0,"+str(shape[0]-1)+","+str(shape[1]-1)
+    data = imval(r21image,box=box)
+    pixvalues = data["data"].flatten()
+    pixvalues = pixvalues[abs(pixvalues)!=0]
+    median = np.median(pixvalues)
 
-    # immath
-    for j in range(len(co10images)):
-        outfile = dir_ratio + co10images[j].split("/")[-1].replace("co10","r21")
-        os.system("rm -rf " + outfile + "_tmp")
-        immath(imagename = [co21images[j],
-                            co10images[j]],
-               expr = "iif(IM0>0,IM0/IM1/4.0,0)",
-               outfile = outfile + "_tmp")
-
-        makemask(mode = "copy",
-                 inpimage = outfile + "_tmp",
-                 inpmask = outfile + "_tmp",
-                 output = outfile + "_tmp_mask:mask0",
-                 overwrite = True)
-
-        makemask(mode = "copy",
-                 inpimage = outfile + "_tmp_mask",
-                 inpmask = outfile + "_tmp_mask:mask0",
-                 output = "",
-                 overwrite = True)
-
-        immath(imagename = [outfile + "_tmp",
-                            outfile + "_tmp_mask"],
-               expr = "iif(IM1>=1,IM0,0)",
-               outfile = outfile)
-
-        os.system("rm -rf " + outfile + "_tmp")
-        os.system("rm -rf " + outfile + "_tmp_mask")
 
 os.system("rm -rf *.last")
