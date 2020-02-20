887df70dd82d1e5e6c31d5864a0fff9013b5d290
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-02-20 22:25:01 +0100

    Commit at 2020-02-20 22:25:01

--- mycasa_scripts_active/scripts_ts09_phangs_r21/myim07_sd_mom.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/myim07_sd_mom.py
@@ -33,11 +33,6 @@ for i in range(len(fits_co10)):
            hdkey = "bunit",
            hdvalue = "Jy/beam")
 
-    imhead(imagename = fits_co10[i].replace(".fits",".image_Jypb"),
-    	   mode = "put",
-    	   hdkey = "reffreq",
-    	   hdvalue = "1.15271000e+11Hz")
-
     imsmooth(imagename = fits_co10[i].replace(".fits",".image_Jypb"),
              targetres = True,
              major = "33.0arcsec",
@@ -69,11 +64,6 @@ for i in range(len(fits_co21)):
            mode = "put",
            hdkey = "bunit",
            hdvalue = "Jy/beam")
-
-    imhead(imagename = fits_co21[i].replace(".fits",".image_Jypb"),
-    	   mode = "put",
-    	   hdkey = "reffreq",
-    	   hdvalue = "2.30538000e+11Hz")
 
     os.system("rm -rf " + fits_co21[i].replace(".fits",".regrid"))
     imregrid(imagename = fits_co21[i].replace(".fits",".image_Jypb"),
