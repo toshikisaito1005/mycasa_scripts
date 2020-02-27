27592a239b15d6c9367d8add694f3660b57354de
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-02-27 08:45:00 -0500

    Commit at 2020-02-27 08:45:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/fig07_radial_norm.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/fig07_radial_norm.py
@@ -19,7 +19,8 @@ plt.ioff()
 dir_data = "/Users/saito/data/mycasa_scripts_active/scripts_ts09_phangs_r21/"
 dir_product = "/Users/saito/data/myproj_active/proj_ts09_phangs_r21/eps/"
 gals = ["ngc0628","ngc3627","ngc4321"]
-dist25 = []
+dist25 = [4.9, 5.1, 3.0] # arcmin, Leroy et al. 2019
+scales = [44/1.0, 52/1.3, 103/1.4]
 
 #####################
 ### functions
@@ -40,7 +41,11 @@ def hist_percent(histo,percent):
 for i in range(len(gals)):
     galname = gals[i]
     data = np.loadtxt(dir_data + galname + "_parameter_600pc.txt")
+    # r/r25
     distance = data[;,0]
+
+
+
     r21 = data[;,1]
 
 
