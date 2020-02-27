c7fb69d2ac603064a994cbac94f83fe4f03e81ee
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-02-27 15:50:01 -0500

    Commit at 2020-02-27 15:50:01

--- mycasa_scripts_active/scripts_ts09_phangs_r21/myim_runall.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/myim_runall.py
@@ -7,7 +7,7 @@ execfile("myim06_r21_map.py")
 execfile("myim07_sd_mom.py")
 execfile("myim08_r21_highlowmask.py")
 execfile("myim09_convolve_wise.py")
-execfile("myim10_param_600pc.py")
+execfile("myim10_extract_param_600pc.py")
 
 os.system("rm -rf tempalte.image")
 os.system("rm -rf *.last")
