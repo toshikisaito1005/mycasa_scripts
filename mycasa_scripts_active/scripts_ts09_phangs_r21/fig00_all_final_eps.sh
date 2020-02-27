42ba5239176d3eaf64fd9a60987b29f89baf0bb6
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-02-27 15:20:01 -0500

    Commit at 2020-02-27 15:20:01

--- mycasa_scripts_active/scripts_ts09_phangs_r21/fig00_all_final_eps.sh
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/fig00_all_final_eps.sh
@@ -12,17 +12,22 @@ convert -crop 700x570+75+20 $dir_data"ngc4321_12co10_m0.png" $dir_data"ngc4321_1
 convert -crop 700x570+75+20 $dir_data"ngc4321_12co21_m0.png" $dir_data"ngc4321_12co21_m0b.png"
 convert -crop 700x570+21+20 $dir_data"ngc4321_r21.png" $dir_data"ngc4321_r21b.png"
 
+convert +append -border 0x0 $dir_data"ngc0628_12co10_m0b.png" $dir_data"ngc0628_12co21_m0b.png" $dir_data"ngc0628_r21b.png" $dir_data"fig01a.png"
+convert +append -border 0x0 $dir_data"ngc3627_12co10_m0b.png" $dir_data"ngc3627_12co21_m0b.png" $dir_data"ngc3627_r21b.png" $dir_data"fig01b.png"
+convert +append -border 0x0 $dir_data"ngc4321_12co10_m0b.png" $dir_data"ngc4321_12co21_m0b.png" $dir_data"ngc4321_r21b.png" $dir_data"fig01c.png"
 
-convert +append -border 0x0 $dir_data"ngc4321_12co10_m0b.png" $dir_data"ngc4321_12co21_m0b.png" $dir_data"fig01a.png"
-convert +append -border 0x0 $dir_data"ngc4321_r21b.png" $dir_data"ngc4321_r21_m8b.png" $dir_data"fig01b.png"
+convert -append -border 0x0 $dir_data"fig01a.png" $dir_data"fig01b.png" $dir_data"fig01c.png" eps2:$dir_data"f01.eps"
 
-convert -append -border 0x0 $dir_data"fig01a.png" $dir_data"fig01b.png" eps2:$dir_data"f01.eps"
-
+rm -rf $dir_data"ngc0628_12co10_m0.png" $dir_data"ngc0628_12co10_m0b.png"
+rm -rf $dir_data"ngc0628_12co21_m0.png" $dir_data"ngc0628_12co21_m0b.png"
+rm -rf $dir_data"ngc0628_r21.png" $dir_data"ngc0628_r21b.png"
+rm -rf $dir_data"ngc3627_12co10_m0.png" $dir_data"ngc3627_12co10_m0b.png"
+rm -rf $dir_data"ngc3627_12co21_m0.png" $dir_data"ngc3627_12co21_m0b.png"
+rm -rf $dir_data"ngc3627_r21.png" $dir_data"ngc3627_r21b.png"
 rm -rf $dir_data"ngc4321_12co10_m0.png" $dir_data"ngc4321_12co10_m0b.png"
 rm -rf $dir_data"ngc4321_12co21_m0.png" $dir_data"ngc4321_12co21_m0b.png"
 rm -rf $dir_data"ngc4321_r21.png" $dir_data"ngc4321_r21b.png"
-rm -rf $dir_data"ngc4321_r21_m8.png" $dir_data"ngc4321_r21_m8b.png"
-rm -rf $dir_data"fig01a.png" $dir_data"fig01b.png"
+rm -rf $dir_data"fig01a.png" $dir_data"fig01b.png" $dir_data"fig01c.png"
 
 
 ### figure 2
