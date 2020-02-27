b8ba07f17b5d60780c324aea9743be1b1ddf3057
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-02-27 12:00:00 -0500

    Commit at 2020-02-27 12:00:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/fig08_r21_vs_co21_norm.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/fig08_r21_vs_co21_norm.py
@@ -21,7 +21,7 @@ dir_product = "/Users/saito/data/myproj_active/proj_ts09_phangs_r21/eps/"
 gals = ["ngc0628","ngc3627","ngc4321"]
 dist25 = [4.9, 5.1, 3.0] # arcmin, Leroy et al. 2019
 scales = [44/1.0, 52/1.3, 103/1.4]
-nbins = 8
+nbins = 5
 percents = [0.15,0.025,0.010]
 
 #####################
@@ -43,11 +43,8 @@ def hist_percent(histo,percent):
 figure = plt.figure(figsize=(9,9))
 gs = gridspec.GridSpec(nrows=9, ncols=9)
 ax1 = plt.subplot(gs[0:7,0:7])
-ax2 = plt.subplot(gs[0:7,7:9])
-ax2b = ax2.twinx()
 plt.rcParams["font.size"] = 16
 
-histdata = []
 for i in range(len(gals)):
     galname = gals[i]
     data = np.loadtxt(dir_data + galname + "_parameter_600pc.txt")
@@ -72,52 +69,26 @@ for i in range(len(gals)):
     mean = sy / n
     std = np.sqrt(sy2/n - mean*mean)
 
-    ax1.plot(
-        (_[1:] + _[:-1])/2, mean,
+    ax1.errorbar(
+        (_[1:] + _[:-1])/2, mean, yerr=std,  
         color=cm.brg(i/2.5), lw=7, alpha=0.5,
         label = galname.replace("ngc","NGC ")
         )
     """
     ax1.scatter(
-        galdist, norm_r21,
+        co21, norm_r21,
         color=cm.brg(i/2.5),
         lw=0, alpha=0.2, s=50,
         label = galname.replace("ngc","NGC "))
     """
-    histdata.extend(norm_r21.tolist())
-
-dathist = ax2.hist(
-    histdata,orientation="horizontal",range=[0,2],
-    bins=100,lw=0,color="grey",alpha=0.6)
-
-range_p = dathist[1][hist_percent(dathist[0],0.843)]
-range_l = dathist[1][hist_percent(dathist[0],0.157)]
-
-ax1.plot([0,1],[range_l,range_l],lw=5,linestyle="--",alpha=0.8,color="black")
-ax1.plot([0,1],[range_p,range_p],lw=5,linestyle="--",alpha=0.8,color="black")
-ax2.plot([0,dathist[0].max()*1.25],[range_l,range_l],
-         lw=5,linestyle="--",alpha=0.8,color="black")
-ax2.plot([0,dathist[0].max()*1.25],[range_p,range_p],
-         lw=5,linestyle="--",alpha=0.8,color="black")
-ax2.text(0.6*dathist[0].max()*1.25,range_p-0.1,str(range_p))
-ax2.text(0.6*dathist[0].max()*1.25,range_l-0.1,str(range_l))
 
 ax1.grid()
 ax1.legend(ncol=2)
-ax1.set_xlim([0,1])
+#ax1.set_xlim([0,1])
+ax1.set_xscale("log")
 ax1.set_ylim([0,2])
 ax1.set_xlabel("r/r25")
 ax1.set_ylabel("$R_{21}$/$Med(R_{21})$")
-
-ax2.set_ylim([0,2])
-ax2b.set_ylim([0,2])
-ax2.grid(axis="both")
-ax2.tick_params(labelbottom=False,labelleft=False,labeltop=False)
-ax2b.tick_params(labelbottom=False,labelleft=False,labeltop=False)
-ax2.spines["top"].set_visible(False)
-ax2.spines["left"].set_visible(False)
-ax2.spines["bottom"].set_visible(False)
-ax2b.set_ylabel("$R_{21}$/$Med(R_{21})$")
 
 ax1.set_title("Radial $R_{21}$/$Med(R_{21})$ Distribution")
 plt.savefig(dir_product+"radial_r21_vs_co21_mom0.png",dpi=200)
