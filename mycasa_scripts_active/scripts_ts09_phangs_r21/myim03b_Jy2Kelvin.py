a67416f668650406f076b679e6fde73fdb7127b1
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-02-27 17:35:00 -0500

    Commit at 2020-02-27 17:35:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/myim03b_Jy2Kelvin.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/myim03b_Jy2Kelvin.py
@@ -19,6 +19,20 @@ for i in range(len(gals)):
     dir_co10 = dir_proj + galname + "_co10/"
     dir_co21 = dir_proj + galname + "_co21/"
     co10image = dir_co10 + "co10_" + beams[i] + ".moment0"
+    co21image = dir_co21 + "co21_" + beams[i] + ".moment0"
 
+    # header
+    co10header = imhead(co10image,mode="list")
+    co10beamwsize = co10header["beammajor"]["value"]
+    co21header = imhead(co21image,mode="list")
+    co21beamwsize = co21header["beammajor"]["value"]
+
+    # J2K factor
+    J2K_co10 = 1.222e6 / co10beamwsize**2 / 115.27120**2
+    J2K_co21 = 1.222e6 / co21beamwsize**2 / 230.53800**2
+
+    # apply
+    os.system("rm -rf " + co10image + "_Kelvin")
+    immath
 
 os.system("rm -rf *.last")
