00853ab28f8d1244a72f02476c010c3c5e710f07
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-02-27 16:10:00 -0500

    Commit at 2020-02-27 16:10:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f00_all_final_eps.sh
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f00_all_final_eps.sh
@@ -2,6 +2,12 @@ dir_data="/Users/saito/data/myproj_active/proj_ts09_phangs_r21/eps/"
 
 
 ### figure 1
+convert +append -border 0x0 $dir_data"missingflux_co10.png" $dir_data"missingflux_co21.png" $dir_data"missingflux_r21.png" eps2:$dir_data"f01.eps"
+
+rm -rf $dir_data"missingflux_co10.png" $dir_data"missingflux_co21.png" $dir_data"missingflux_r21.png"
+
+
+### figure 2
 convert -crop 600x570+75+20 $dir_data"ngc0628_12co10_m0.png" $dir_data"ngc0628_12co10_m0b.png"
 convert -crop 600x570+75+20 $dir_data"ngc0628_12co21_m0.png" $dir_data"ngc0628_12co21_m0b.png"
 convert -crop 700x570+21+20 $dir_data"ngc0628_r21.png" $dir_data"ngc0628_r21b.png"
@@ -16,7 +22,7 @@ convert +append -border 0x0 $dir_data"ngc0628_12co10_m0b.png" $dir_data"ngc0628_
 convert +append -border 0x0 $dir_data"ngc3627_12co10_m0b.png" $dir_data"ngc3627_12co21_m0b.png" $dir_data"ngc3627_r21b.png" $dir_data"fig01b.png"
 convert +append -border 0x0 $dir_data"ngc4321_12co10_m0b.png" $dir_data"ngc4321_12co21_m0b.png" $dir_data"ngc4321_r21b.png" $dir_data"fig01c.png"
 
-convert -append -border 0x0 $dir_data"fig01a.png" $dir_data"fig01b.png" $dir_data"fig01c.png" eps2:$dir_data"f01.eps"
+convert -append -border 0x0 $dir_data"fig01a.png" $dir_data"fig01b.png" $dir_data"fig01c.png" eps2:$dir_data"f02.eps"
 
 rm -rf $dir_data"ngc0628_12co10_m0.png" $dir_data"ngc0628_12co10_m0b.png"
 rm -rf $dir_data"ngc0628_12co21_m0.png" $dir_data"ngc0628_12co21_m0b.png"
@@ -28,12 +34,6 @@ rm -rf $dir_data"ngc4321_12co10_m0.png" $dir_data"ngc4321_12co10_m0b.png"
 rm -rf $dir_data"ngc4321_12co21_m0.png" $dir_data"ngc4321_12co21_m0b.png"
 rm -rf $dir_data"ngc4321_r21.png" $dir_data"ngc4321_r21b.png"
 rm -rf $dir_data"fig01a.png" $dir_data"fig01b.png" $dir_data"fig01c.png"
-
-
-### figure 2
-convert +append -border 0x0 $dir_data"missingflux_co10.png" $dir_data"missingflux_co21.png" $dir_data"missingflux_r21.png" eps2:$dir_data"f02.eps"
-
-rm -rf $dir_data"missingflux_co10.png" $dir_data"missingflux_co21.png" $dir_data"missingflux_r21.png"
 
 
 ### figure 3
