7b091b969346c32c9f0c2da7ed5a1217a0edccc1
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-02-27 22:40:01 -0500

    Commit at 2020-02-27 22:40:01

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f03_peak_vs_integ.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f03_peak_vs_integ.py
@@ -17,6 +17,7 @@ percents = [0.15,0.025,0.010]
 ### Main Procedure
 #####################
 figure = plt.figure(figsize=(8,8))
+histo = []
 for i in range(len(gals)):
 	galname = gals[i]
 	data = np.loadtxt(dir_data + galname + "_parameter_matched_res.txt")
@@ -40,9 +41,15 @@ for i in range(len(gals)):
 	plt.xlim([-1.2,0.7])
 	plt.ylim([-1.2,0.7])
 	plt.scatter(np.log10(r21), np.log10(p21),
-		c=cm.brg(i/2.5), alpha=0.1, lw=0, s=20)
+		c=cm.brg(i/2.5), alpha=0.1, lw=0, s=20,
+		label = galname.replace("ngc","NGC "))
 	plt.plot([-1.2,0.7],[-1.2,0.7],"k-",lw=1)
-	plt.plot([-1.2,0.7],[-1.0,0.9],"k--",lw=1)
-	plt.plot([-1.2,0.7],[-1.4,0.5],"k--",lw=1)
+	plt.plot([-1.2,0.7],[-1.1,0.8],"k--",lw=1)
+	plt.plot([-1.2,0.7],[-1.3,0.6],"k--",lw=1)
 
+	histo.extend(p21/r21)
+
+plt.xlabel("log Integrated Intensity Ratio")
+plt.ylabel("log Peak Temperature Ratio")
+plt.legend(loc = "upper left")
 plt.savefig(dir_product+"figure_r21_vs_p21.png",dpi=100)
