bdc2265ee5e1743ab43a3c5d2c92a3e5e7ffa066
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-02-27 21:55:01 -0500

    Commit at 2020-02-27 21:55:01

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f03_peak_vs_integ.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f03_peak_vs_integ.py
@@ -8,9 +8,13 @@ import scipy
 #####################
 ### parameters
 #####################
-
+dir_data = "/Users/saito/data/myproj_active/proj_ts09_phangs_r21/"
+gals = ["ngc0628","ngc3627","ngc4321"]
 
 
 #####################
 ### Main Procedure
 #####################
+for i in range(len(gals)):
+	galname = gals[i]
+	dir_
