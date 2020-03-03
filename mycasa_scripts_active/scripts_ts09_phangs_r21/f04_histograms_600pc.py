237b21da6e59bee83b67d2c4925679e59a4d4888
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-03-02 21:45:00 -0500

    Commit at 2020-03-02 21:45:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f04_histograms_600pc.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f04_histograms_600pc.py
@@ -79,6 +79,13 @@ for i in range(len(gals)):
 	r21err = r21 * np.sqrt((1./co10snr)**2 + (1./co21snr)**2)
 
     ### plot data
+def (ax, ):
+
+
+
+	fig = plt.figure()
+	ax1 = fig.add_subplot(1,3,1)
+
 
 	### plot data
 	plt.figure(figsize=(18,3))
