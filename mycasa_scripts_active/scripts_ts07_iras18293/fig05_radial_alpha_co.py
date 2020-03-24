43480acec3cc5f447bb888f064e0b0fb1cb1140a
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-03-24 15:35:00 +0900

    Commit at 2020-03-24 15:35:00

--- mycasa_scripts_active/scripts_ts07_iras18293/fig05_radial_alpha_co.py
+++ mycasa_scripts_active/scripts_ts07_iras18293/fig05_radial_alpha_co.py
@@ -319,9 +319,12 @@ plt.rcParams["font.size"] = 16
 plt.subplots_adjust(bottom=0.20, left=0.15, right=0.85, top=0.85)
 
 cscatter = ax1.scatter(list_x,list_q,c=list_alpha,cmap='rainbow',s=60,lw=0,marker='s')
-ax1.contour(map_X*1e5, map_Q, map_alpha, levels=[11.7,14.0,17.1])
+cont = ax1.contour(map_X*1e5,map_Q,map_alpha,linewidths=[2,4,2],levels=[11.7,14.0,17.1],colors=['black'])
+cont.clabel(fmt='%1.1f')
 ax1.set_xlim([min(list_x),max(list_x)])
 ax1.set_ylim([min(list_q),max(list_q)])
+ax1.set_xlabel('$X_{CI}$/10$^{-5}$')
 
 cbar = plt.colorbar(cscatter)#cax=cax)
+cbar.set_clabel("")
 plt.savefig(dir_data+"eps/heatmap_alpha_ci.png",dpi=300)
