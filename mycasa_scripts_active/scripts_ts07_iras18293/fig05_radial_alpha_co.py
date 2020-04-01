b1c28de4fc056bacee7c180b1b11e1e4c3142bb8
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-01 14:15:01 +0900

    Commit at 2020-04-01 14:15:01

--- mycasa_scripts_active/scripts_ts07_iras18293/fig05_radial_alpha_co.py
+++ mycasa_scripts_active/scripts_ts07_iras18293/fig05_radial_alpha_co.py
@@ -88,7 +88,7 @@ def gas_mass_from_dust_flux(
     nu850 = 352.6970094 # GHz
     Sig_rj = sigma_rj(tdust,nuobs,z)
     Sig_0 = sigma_rj(tdust,nu850,0)
-    mass = 1.78e10 * flux * (1+z)**-4.8 * (nu850/nuobs)**3.8 * dist**2 * (Sig_rj/Sig_0)
+    mass = 1.78e10 * flux * (1+z)**-4.8 * (nu850/nuobs)**3.8 * dist**2 * (Sig_0/Sig_rj)
 
     return mass
 
