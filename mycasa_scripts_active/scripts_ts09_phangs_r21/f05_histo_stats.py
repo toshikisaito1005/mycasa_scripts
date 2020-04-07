a9975bf4e9b4d70ebbae4f94e6770b7afdb59f35
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-07 12:10:00 +0900

    Commit at 2020-04-07 12:10:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f05_histo_stats.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f05_histo_stats.py
@@ -34,15 +34,25 @@ stats_n4321_wco21 = data[10:15]
 
 ### plot
 # setup
-fig = plt.figure(figsize=(15,10))
+fig = plt.figure(figsize=(20,10))
 ax1 = fig.add_subplot(111)
 ax1.grid(which='major',linestyle='--')
 plt.rcParams["font.size"] = 22
 plt.subplots_adjust(bottom=0.15, left=0.15, right=0.85, top=0.85)
 #
-ax1.plot(stats_n0628_no[:,0],stats_n0628_no[:,1],'-o')
+ax1.plot(stats_n0628_no[:,0],stats_n0628_no[:,1],'-o',alpha=0.4,c=cm.brg(0/2.5))
+ax1.plot(stats_n0628_wco10[:,0],stats_n0628_wco10[:,1],'-o',alpha=0.4,c=cm.brg(0/2.5))
+ax1.plot(stats_n0628_wco21[:,0],stats_n0628_wco21[:,1],'-o',alpha=0.4,c=cm.brg(0/2.5))
+ax1.plot(stats_n3627_no[:,0],stats_n3627_no[:,1],'-o',alpha=0.4,c=cm.brg(1/2.5))
+ax1.plot(stats_n3627_wco10[:,0],stats_n3627_wco10[:,1],'-o',alpha=0.4,c=cm.brg(1/2.5))
+ax1.plot(stats_n3627_wco21[:,0],stats_n3627_wco21[:,1],'-o',alpha=0.4,c=cm.brg(1/2.5))
+ax1.plot(stats_n4321_no[:,0],stats_n4321_no[:,1],'-o',alpha=0.4,c=cm.brg(2/2.5))
+ax1.plot(stats_n4321_wco10[:,0],stats_n4321_wco10[:,1],'-o',alpha=0.4,c=cm.brg(2/2.5))
+ax1.plot(stats_n4321_wco21[:,0],stats_n4321_wco21[:,1],'-o',alpha=0.4,c=cm.brg(2/2.5))
 
 ax1.set_xlim([0,16])
-ax1.set_ylim([0.4,1.0])
+ax1.set_ylim([0.3,1.1])
 
 plt.savefig(dir_product+"stats_histo_600pc.png",dpi=300)
+
+
