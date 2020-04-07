9e06b4dbfae5f31b6f13b771420ce2d33551e1af
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-07 13:35:00 +0900

    Commit at 2020-04-07 13:35:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f05_histo_stats.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f05_histo_stats.py
@@ -61,7 +61,9 @@ ax1.plot(stats_n4321_wco10[:,0]+2,stats_n4321_wco10[:,1]/stats_n4321_no[2][1],
 ax1.plot(stats_n4321_wco21[:,0]+2,stats_n4321_wco21[:,1]/stats_n4321_no[2][1],
 	'-o',alpha=0.4,c=cm.brg(2/2.5),lw=7)
 
-ax1.text(3,1.4,"test")
+ax1.text(3,1.42,'NGC 0628',horizontalalignment='center')
+ax1.text(9,1.42,'NGC 3627',horizontalalignment='center')
+ax1.text(15,1.42,'NGC 4321',horizontalalignment='center')
 
 ax1.set_xlim([0.5,17.5])
 ax1.set_ylim([0.7,1.5])
@@ -71,5 +73,3 @@ plt.xticks([1,2,3,4,5,7,8,9,10,11,13,14,15,16,17],["84%","Mean","Median","Mode",
 plt.xticks(rotation=45)
 
 plt.savefig(dir_product+"stats_histo_600pc.png",dpi=300)
-
-
