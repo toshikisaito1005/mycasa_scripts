94be8be51ce0ebd59b2656b338663d1a6071ce8d
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-08 16:15:00 +0900

    Commit at 2020-04-08 16:15:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/myim04_mom_spectralcube.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/myim04_mom_spectralcube.py
@@ -1,8 +1,18 @@
 import os
 import glob
-import scripts_phangs_r21 as r21
 
+from spectral_cube import SpectralCube
+import astropy.units as u
+from astropy.io import fits
+import numpy as np
+from radio_beam import Beam
 import scDerivativeRoutines as scderive
 import scMaskingRoutines as scmask
 
-imagename = 
+import scMoments as scmoments
+
+scmoments.moment_generator(
+  cubefile = "co10_cube_04p0.fits",
+  root_name = "/Users/saito/data/myproj_active/proj_ts09_phangs_r21/ngc0628_co10/")
+
+scderive.write_moment0
