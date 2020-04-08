27c238ac077ff997939e3855441215c4fad9786b
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-08 15:20:00 +0900

    Commit at 2020-04-08 15:20:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/fig_runall.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/fig_runall.py
@@ -1,10 +1,14 @@
 
-#execfile("fig04_histograms_600pc.py")
-#execfile("fig06_r21_mask.py")
 
-execfile("fig07a_radial.py")
-execfile("fig07b_radial_norm.py")
-execfile("fig08a_r21_vs_co21_mom0.py")
-execfile("fig08b_r21_vs_co21_tpeak.py")
-execfile("fig08c_r21_vs_co21_disp.py")
+execfile("f01_missingflux.py")
+execfile("f02_images.py")
+execfile("f03_peak_vs_integ.py")
+execfile("f04_histograms_600pc.py")
+execfile("f05_histo_stats.py")
+
+#execfile("fig07a_radial.py")
+#execfile("fig07b_radial_norm.py")
+#execfile("fig08a_r21_vs_co21_mom0.py")
+#execfile("fig08b_r21_vs_co21_tpeak.py")
+#execfile("fig08c_r21_vs_co21_disp.py")
 
