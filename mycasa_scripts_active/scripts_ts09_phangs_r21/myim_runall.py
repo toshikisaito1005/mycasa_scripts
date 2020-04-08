40d51c0bd3920765e26561602b4bd5593c08c79e
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-08 16:30:01 +0900

    Commit at 2020-04-08 16:30:01

--- mycasa_scripts_active/scripts_ts09_phangs_r21/myim_runall.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/myim_runall.py
@@ -1,7 +1,7 @@
 import os
 
-execfile("myim01_prep.py")
-execfile("myim02_regrid.py")
+#execfile("myim01_prep.py")
+#execfile("myim02_regrid.py")
 execfile("myim03a_mom_native.py")
 execfile("myim03b_Jy2Kelvin.py")
 execfile("myim04_multibeam.py")
