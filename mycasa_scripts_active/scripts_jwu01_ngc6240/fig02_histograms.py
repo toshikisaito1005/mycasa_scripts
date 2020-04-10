0573ebb378bc365be9ef1c74a40fa0f546890294
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-10 23:25:00 +0900

    Commit at 2020-04-10 23:25:00

--- mycasa_scripts_active/scripts_jwu01_ngc6240/fig02_histograms.py
+++ mycasa_scripts_active/scripts_jwu01_ngc6240/fig02_histograms.py
@@ -10,9 +10,20 @@ co10 = data[:,1]
 co21 = data[:,2]
 
 # plotに向けての下準備です。
-fig = plt.figure(figsize=(10,10))                # 図の大きさの設定
-ax1 = fig.add_subplot(111)                       # これはおまじないです
-ax1.grid(which='major',linestyle='--')           # 図にグリッドを表示させます
-plt.rcParams["font.size"] = 22                   # 図中の文字サイズの設定
+fig = plt.figure(figsize=(10,10))            # 図の大きさの設定
+ax1 = fig.add_subplot(111)                   # これはおまじないです
+ax1.grid(which='major',linestyle='--')       # 図にグリッドを表示させます
+plt.rcParams["font.size"] = 22               # 図中の文字サイズの設定
 
-ax1.hist(co10,)
+# ヒストグラムの準備
+bins = 20
+historange = [0.0,2.0]
+
+ax1.hist(co10,
+	     bins = bins,
+	     range = historange,
+	     )
+
+ax1.set_xlim(historange)                     # xの範囲の指定
+
+plt.savefig("/Users/saito/Desktop/figure_histo_co10.png",dpi=300)
