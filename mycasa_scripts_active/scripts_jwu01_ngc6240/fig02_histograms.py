dc3329c7c971bb00443919a4edb56388e39c08b4
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-10 23:55:01 +0900

    Commit at 2020-04-10 23:55:01

--- mycasa_scripts_active/scripts_jwu01_ngc6240/fig02_histograms.py
+++ mycasa_scripts_active/scripts_jwu01_ngc6240/fig02_histograms.py
@@ -16,12 +16,12 @@ co21 = data[:,2]
 fig = plt.figure(figsize=(10,5))             # 図の大きさの設定
 ax1 = fig.add_subplot(111)                   # これはおまじないです
 ax1.grid(axis="x",linestyle='--')            # 図にグリッドを表示させます
-plt.rcParams["font.size"] = 22               # 図中の文字サイズの設定
+plt.rcParams["font.size"] = 20               # 図中の文字サイズの設定
 plt.subplots_adjust(bottom=0.15, left=0.12, right=0.95, top=0.9)  # 図の余白設定
 
 
 # ヒストグラムの準備
-bins = 100
+bins = 80
 historange = [0.001,25.]
 hist_co10 = np.histogram(co10, bins = bins, range = historange)
 hist_co21 = np.histogram(co21, bins = bins, range = historange)
@@ -30,19 +30,19 @@ hist_co21 = np.histogram(co21, bins = bins, range = historange)
 ax1.step(np.delete(hist_co10[1],-1),
 	      hist_co10[0]/float(sum(hist_co10[0])),
 	      alpha=0.4,
-	      lw = 4,
+	      lw = 2,
 	      color = "blue",
 	      label = "CO(1-0)")
 
 ax1.step(np.delete(hist_co21[1],-1),
 	      hist_co21[0]/float(sum(hist_co21[0])),
 	      alpha=0.4,
-	      lw = 4,
+	      lw = 2,
 	      color = "red",
 	      label = "CO(2-1)")
 
-ax1.set_xlim(historange)                      # xの範囲の指定
-ax1.set_ylim([0,0.2])                         # yの範囲の指定
+ax1.set_xlim(historange)                     # xの範囲の指定
+ax1.set_ylim([0,0.25])                       # yの範囲の指定
 
 ax1.set_ylabel("Normlized Count")
 ax1.set_xlabel("Brightness Temperature (K)")
@@ -56,24 +56,38 @@ plt.savefig("/Users/saito/Desktop/figure_histo_temp.png",dpi=300)
 fig = plt.figure(figsize=(10,5))             # 図の大きさの設定
 ax1 = fig.add_subplot(111)                   # これはおまじないです
 ax1.grid(axis="x",linestyle='--')            # 図にグリッドを表示させます
-plt.rcParams["font.size"] = 22               # 図中の文字サイズの設定
+plt.rcParams["font.size"] = 20               # 図中の文字サイズの設定
 plt.subplots_adjust(bottom=0.15, left=0.12, right=0.95, top=0.9)  # 図の余白設定
 
 
 # ヒストグラムの準備
-bins = 100
+bins = 80
 historange = [0.001,4.]
 hist_r21 = np.histogram(co21/co10, bins = bins, range = historange)
+hist_r21_wco10 = np.histogram(co21/co10, bins = bins, range = historange, weights = co10)
+hist_r21_wco21 = np.histogram(co21/co10, bins = bins, range = historange, weights = co21)
 
 
 ax1.step(np.delete(hist_r21[1],-1),
 	      hist_r21[0]/float(sum(hist_r21[0])),
 	      alpha=0.4,
-	      lw = 4,
+	      lw = 2,
 	      color = "blue")
 
+ax1.step(np.delete(hist_r21_wco10[1],-1),
+	      hist_r21_wco10[0]/float(sum(hist_r21_wco10[0])),
+	      alpha=0.4,
+	      lw = 2,
+	      color = "green")
+
+ax1.step(np.delete(hist_r21_wco21[1],-1),
+	      hist_r21_wco21[0]/float(sum(hist_r21_wco21[0])),
+	      alpha=0.4,
+	      lw = 2,
+	      color = "red")
+
 ax1.set_xlim(historange)                      # xの範囲の指定
-ax1.set_ylim([0,0.1])                         # yの範囲の指定
+ax1.set_ylim([0,0.15])                        # yの範囲の指定
 
 ax1.set_ylabel("Normlized Count")
 ax1.set_xlabel("Brightness Temperature Ratio")
