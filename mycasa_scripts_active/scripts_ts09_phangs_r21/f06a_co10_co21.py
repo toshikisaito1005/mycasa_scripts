59268d56cb639c3a5476928cdf13d9ff17faaa14
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-14 15:05:00 +0900

    Commit at 2020-04-14 15:05:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f06a_co10_co21.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f06a_co10_co21.py
@@ -128,10 +128,10 @@ def plot_scatter(
 	# plot text
 	ax1.text(xlim[0]+(xlim[1]-xlim[0])*0.1, ylim[1]-(ylim[1]-ylim[0])*0.08, text)
 	ax1.text(xlim[0]+(xlim[1]-xlim[0])*0.1, ylim[1]-(ylim[1]-ylim[0])*0.16, galname)
-	ax1.text(
-		xlim[0]+(xlim[1]-xlim[0])*0.05,
-		ylim[1]-(ylim[1]-ylim[0])*0.87,
+	ax1.text(xlim[0]+(xlim[1]-xlim[0])*0.02, ylim[1]-(ylim[1]-ylim[0])*0.90,
 		"1:1", rotation=45, fontsize=12)
+	ax1.text(xlim[0]+(xlim[1]-xlim[0])*0.20, ylim[1]-(ylim[1]-ylim[0])*0.90,
+		"1:0.7", rotation=45, fontsize=12)
 
 
 #####################
