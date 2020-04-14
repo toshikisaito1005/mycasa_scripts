d547c60b26655572b11bd198502e905aa7cd3572
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-14 12:40:00 +0900

    Commit at 2020-04-14 12:40:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f06a_co10_co21.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f06a_co10_co21.py
@@ -35,13 +35,16 @@ beam = [[4.0,6.0,8.0,10.0,12.0,14.0,16.0,18.0,20.0],
 ### Main Procedure
 #####################
 for i in range(len(gals)):
-	for j in range(len(beam[i])):
-		print()
-dir_gal = dir_proj + gals[i]
-beamname = str(beam[i][j]).replace(".","p").zfill(4)
-images_co10 = dir_gal + "_co10/co10_" + beamname + ".moment0"
+    dir_gal = dir_proj + gals[i]
+    for j in range(len(beam[i])):
+        beamname = str(beam[i][j]).replace(".","p").zfill(4)
+        beamfloat = beam[i][j]
+        images_co10 = dir_gal + "_co10/co10_" + beamname + ".moment0"
+        images_co21 = dir_gal + "_co21/co21_" + beamname + ".moment0"
 
-
+        image_reft = imhead(images_co10,mode="list")["shape"][0] - 1
+        image_top = imhead(images_co21,mode="list")["shape"][1] - 1
+        box = 
 
 
 ### plot
