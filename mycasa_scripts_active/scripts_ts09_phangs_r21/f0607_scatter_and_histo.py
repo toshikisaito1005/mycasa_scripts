0640f6ec056b811e88c09e4c67385109cf7e3b97
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-15 14:55:01 +0900

    Commit at 2020-04-15 14:55:01

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f0607_scatter_and_histo.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f0607_scatter_and_histo.py
@@ -142,16 +142,16 @@ def plot_scatter(
 			binx, mean, std = get_binned_dist(x,y,binrange)
 			ax.errorbar(binx, mean, yerr = std, color = "dimgrey", ecolor = "dimgrey", lw=4)
 		# plot error bar
-		mean_snr_x = np.mean(snr_x)
-		mean_snr_y = np.mean(snr_y)
-		posx = 1.25
-		posy = -0.75
+		mean_snr_x = np.mean(snr_x[i])
+		mean_snr_y = np.mean(snr_y[i])
+		posx = 1.4
+		posy = 0.2 - i*0.12
 		bar_right  = np.log10(10**posx + 10**posx/mean_snr_x)
 		bar_left   = np.log10(10**posx - 10**posx/mean_snr_x)
 		bar_top    = np.log10(10**posy + 10**posy/mean_snr_y)
 		bar_bottom = np.log10(10**posy - 10**posy/mean_snr_y)
-
-		ax.plot([bar_left,bar_right],[bar_bottom,bar_top],color=color)
+		ax.plot([bar_left,bar_right], [posy,posy], color=color)
+		ax.plot([posx,posx], [bar_top,bar_bottom], color=color)
 		#
 	# plot annotation
 	if annotation=="flux":
