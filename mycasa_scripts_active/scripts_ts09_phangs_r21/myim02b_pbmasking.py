414911a97eacad7d473615bbd92a6479556cb56a
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-16 22:20:00 +0900

    Commit at 2020-04-16 22:20:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/myim02b_pbmasking.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/myim02b_pbmasking.py
@@ -10,5 +10,13 @@ dir_data = "/Users/saito/data/myproj_active/proj_ts09_phangs_r21/data_ready/"
 #####################
 ### Main Procedure
 #####################
-images = glob.glob(dir_data + "ngc*.image")
-pbmasks = glob.glob(dir_data + "ngc*.pbmask")
+co10images = glob.glob(dir_data + "ngc*co10*.image")
+co21images = glob.glob(dir_data + "ngc*co21*.image")
+co10pbmasks = glob.glob(dir_data + "ngc*co10*.pbmask")
+co21pbmasks = glob.glob(dir_data + "ngc*co21*.pbmask")
+
+for i in range(len(co10images)):
+	# combine mask
+	combinepbmask = co10pbmasks + ".combined"
+	os.system("rm -rf " + combinepbmask)
+	immath(imagename = )
