868cf2bfc37c7aa9d5e5ba73ea78a06f2da87f81
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-17 13:30:00 +0900

    Commit at 2020-04-17 13:30:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/myim_runall.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/myim_runall.py
@@ -4,34 +4,64 @@ import os
 #execfile("myim01_prep.py")
 #execfile("myim02a_regrid.py")
 #execfile("myim02b_pbmasking.py")
+
+print("### running myim03")
 execfile("myim03a_mom_native.py")
 execfile("myim03b_Jy2Kelvin.py")
+
+print("### running myim04")
 execfile("myim04_multibeam.py")
+
+print("### running myim05")
 execfile("myim05_mom_multibeam.py")
+
+print("### running myim06")
 execfile("myim06_r21_map.py")
+
+print("### running myim07")
 execfile("myim07_sd_mom.py")
 
 
+print("### running f01")
 execfile("f01a_noise.py")
 execfile("f01b_missingflux.py")
 
 
+print("### running myim08")
 beam = [4.0, 8.0, 4.0]
 execfile("myim08_r21_highlowmask.py")
 beam = [13.6, 15.0, 8.5]
 execfile("myim08_r21_highlowmask.py")
+
+print("### running myim09")
 execfile("myim09_convolve_wise.py")
+
+print("### running myim10")
 execfile("myim10_extract_param_600pc.py")
+
+print("### running myim11")
 execfile("myim11_extract_param_best.py")
 
 
+print("### running f02")
 execfile("f02_images.py")
+
+print("### running f03")
 execfile("f03_peak_vs_integ.py")
+
+print("### running f04")
 execfile("f04_histograms_600pc.py")
+
+print("### running f05")
 execfile("f05_histo_stats.py")
+
+print("### running f06_07")
 execfile("f06_07_scatter_and_histo.py")
+
+print("### running f08")
 execfile("f08_violins.py")
 
 
+print("### cleanup")
 os.system("rm -rf tempalte.image")
 os.system("rm -rf *.last")
