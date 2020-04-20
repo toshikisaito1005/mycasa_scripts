0b9b9a6c7fac5e55b999d8e91ee8c42eafa26b82
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-20 15:20:00 +0900

    Commit at 2020-04-20 15:20:00

---
+++ mycasa_scripts_active/scripts_phangs05_v3_vs_v4/script02_regrid.py
@@ -0,0 +1,12 @@
+import os, sys, glob
+
+
+dir_data = "/Users/saito/data/phangs/compare_v3p4_v4/data/"
+# v3 channel width = 2.54 km/s
+# v4 channel width = 2.22 km/s
+
+# v3 freq width = 
+
+####################
+### main
+####################
