ca41ea8ceb461f42dbf96af5ec9103f19a4cb582
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-20 18:15:00 +0900

    Commit at 2020-04-20 18:15:00

--- mycasa_scripts_active/scripts_santoro01_ngc1365/cprops2mask_conv_ellipse.py
+++ mycasa_scripts_active/scripts_santoro01_ngc1365/cprops2mask_conv_ellipse.py
@@ -80,8 +80,9 @@ for i in range(len(gmc_ra_dgr)):
     majoraxis = str(gmc_radius_arcsec[i])+"arcsec"
     minoraxis = str(gmc_minor_arcsec[i] * gmc_radius_arcsec[i]/gmc_major_arcsec[i]) + "arcsec"
     direction = "J2000 " + str(gmc_ra_dgr[i])+"deg " + str(gmc_decl_dgr[i])+"deg"
+    area = float(majoraxis.replace("arcsec","")) * float(minoraxis.replace("arcsec","")) * np.pi
     cl.addcomponent(dir=direction,
-                    flux=float(gmc_num[i])*float(gmc_npix[i]),
+                    flux=int(float(gmc_num[i])*area*(4*np.log(2))),
                     # flux=1.0,
                     fluxunit="Jy",
                     freq=str(obsfreq)+"GHz",
