36ab7895096e63bd2d735fb1e20eb39baa8107e7
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-21 21:50:00 +0900

    Commit at 2020-04-21 21:50:00

--- mycasa_scripts_active/scripts_phangs05_v3_vs_v4/script11_get_fits_smallscalebias_cyclefactor.py
+++ mycasa_scripts_active/scripts_phangs05_v3_vs_v4/script11_get_fits_smallscalebias_cyclefactor.py
@@ -29,8 +29,48 @@ for i in range(len(v4_image)):
 	# get names
 	imagename = v4_image[i]
 	template = v3_image[i]
-	output = dir_ready + v4_image[i].split("/")[-1]
+	output = dir_ready + v4_image[i].split("/")[-1].replace("ngc4303_7m_co21","ngc4303_7m_co21_v4")
 	# imregrid
 	os.system("rm -rf " + output)
 	imregrid(imagename=imagename, template=template, output=output)
 
+
+# move v3 to the ready directory
+print("### mv v3")
+for i in range(len(v3_image)):
+	imagename = v3_image[i]
+	output = dir_ready + imagename.split("/")[-1]
+	os.system("rm -rf " + output)
+	shutil.copytree(imagename, output)
+
+
+# smooth v3
+print("### smooth v3")
+imagename = dir_ready + "ngc4303_7m_co21_v3.image"
+outfile = dir_ready + "ngc4303_7m_co21_v3.image.smooth"
+os.system("rm -rf " + outfile)
+imsmooth(imagename=imagename,targetres=True,major=targetbeam,minor=targetbeam,pa="0deg",outfile=outfile)
+
+
+# smooth v4
+print("### smooth v4")
+targetbeam = "9.0arcsec"
+imagename = dir_ready + "ngc4303_7m_co21_v4_bias0p6_cycf1p0.image"
+outfile = dir_ready + "ngc4303_7m_co21_v4_bias0p6_cycf1p0.image.smooth"
+os.system("rm -rf " + outfile)
+imsmooth(imagename=imagename,targetres=True,major=targetbeam,minor=targetbeam,pa="0deg",outfile=outfile)
+
+imagename = dir_ready + "ngc4303_7m_co21_v4_bias0p6_cycf3p0.image"
+outfile = dir_ready + "ngc4303_7m_co21_v4_bias0p6_cycf3p0.image.smooth"
+os.system("rm -rf " + outfile)
+imsmooth(imagename=imagename,targetres=True,major=targetbeam,minor=targetbeam,pa="0deg",outfile=outfile)
+
+imagename = dir_ready + "ngc4303_7m_co21_v4_bias0p9_cycf3p0.image"
+outfile = dir_ready + "ngc4303_7m_co21_v4_bias0p9_cycf3p0.image.smooth"
+os.system("rm -rf " + outfile)
+imsmooth(imagename=imagename,targetres=True,major=targetbeam,minor=targetbeam,pa="0deg",outfile=outfile)
+
+imagename = dir_ready + "ngc4303_7m_co21_v4_bias0p9_cycf3p0.image"
+outfile = dir_ready + "ngc4303_7m_co21_v4_bias0p9_cycf3p0.image.smooth"
+os.system("rm -rf " + outfile)
+imsmooth(imagename=imagename,targetres=True,major=targetbeam,minor=targetbeam,pa="0deg",outfile=outfile)
