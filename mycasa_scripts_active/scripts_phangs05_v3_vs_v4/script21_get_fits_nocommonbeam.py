9912918b41ab147b502b6aae8abd8a4fa2ebee87
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-22 10:00:00 +0900

    Commit at 2020-04-22 10:00:00

--- mycasa_scripts_active/scripts_phangs05_v3_vs_v4/script21_get_fits_nocommonbeam.py
+++ mycasa_scripts_active/scripts_phangs05_v3_vs_v4/script21_get_fits_nocommonbeam.py
@@ -4,7 +4,6 @@ import shutil
 
 dir_data = "/Users/saito/data/phangs/compare_v3p4_v4/data/"
 dir_ready = "/Users/saito/data/phangs/compare_v3p4_v4/data_ready_nocommonbeam/"
-targetbeam = "9.0arcsec"
 
 
 ####################
@@ -16,15 +15,16 @@ if not done:
 	os.mkdir(dir_ready)
 
 
-# get v4 CASA files
+# get CASA files
 v3_image = glob.glob(dir_data + "ngc4303_7m_co21_v3_nocommonbeam_dirty.psf")[0]
 v4_image = glob.glob(dir_data + "ngc4303_7m_co21_v4_nocommonbeam_dirty.psf")[0]
 
 
-# move v3 to the ready directory
-print("### mv v3")
-for i in range(len(v3_image)):
-	imagename = v3_image[0]
-	output = dir_ready + imagename.split("/")[-1]
-	os.system("rm -rf " + output)
-	shutil.copytree(imagename, output)
+# move to the ready directory
+output = dir_ready + v3_image.split("/")[-1]
+os.system("rm -rf " + output)
+shutil.copytree(v3_image, output)
+
+output = dir_ready + v4_image.split("/")[-1]
+os.system("rm -rf " + output)
+shutil.copytree(v3_image, output)
