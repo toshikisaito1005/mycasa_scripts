b7353f7b9da7d50d0309840fac864fb841fb47f2
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-25 18:05:00 +0900

    Commit at 2020-04-25 18:05:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f99_table03.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f99_table03.py
@@ -15,59 +15,60 @@ txtfile2 = glob.glob("table03_*_co21vsr21.txt")
 def table03_galname(txtfile1,txtfile2):
 	"""
 	"""
-	txtdata1 = np.loadtxt(txtfile1,dtype="str")
-	txtdata2 = np.loadtxt(txtfile2,dtype="str")
-	
+	for i in range(len(txtfile1)):
+		txtdata1 = np.loadtxt(txtfile1[i],dtype="str")
+		txtdata2 = np.loadtxt(txtfile2[i],dtype="str")
+		for j in range(len(txtdata1)):
+			onerow = extract_onerow(txtdata1[j],txtdata2[j])
+			print(onerow)
+
 
 def extract_onerow(txtdata1,txtdata2):
 	"""
 	"""
-	l1  = [s.replace("00","0") for s in txtdata1[:,0]]
-	l2  = txtdata1[:,1]
-	l3a = txtdata1[:,2]
-	l3b = txtdata1[:,3]
-	l4a = txtdata1[:,4]
-	l4b = txtdata1[:,5]
-	l5  = txtdata2[:,1]
-	l6a = txtdata2[:,2]
-	l6b = txtdata2[:,3]
-	l7a = txtdata2[:,4]
-	l7b = txtdata2[:,5]
-	#
-	for i in range(len(l1)):
-		# l1
-		if len(l1[i])==3:
-			t1 = "\phantom{0}" + l1[i]
-		else:
-			t1 = l1[i]
-		# l2
-		t2 = l2[i]
-		# l3
-		t3 = l3a[i] + " \pm " + l3b[i]
-		# l4
-		if "-" in l4a[i]:
-			t4 = l4a[i].replace("-","$-$") + " \pm " + l4b[i]
-		else:
-			t4 = "\phantom{$-$}" + l4a[i] + " \pm " + l4b[i]
-		# l5
-		if "-" in l5[i]:
-			t5 = l5[i].replace("-","$-$")
-		else:
-			t5 = "\phantom{$-$}" + l5[i]
-		# l6
-		if "-" in l6a[i]:
-			t6 = l6a[i].replace("-","$-$") + " \pm " + l6b[i]
-		else:
-			t6 = "\phantom{$-$}" + l6a[i] + " \pm " + l6b[i]
-		# l7
-		if "-" in l7a[i]:
-			t7 = l7a[i].replace("-","$-$") + " \pm " + l7b[i]
-		else:
-			t7 = "\phantom{$-$}" + l7a[i] + " \pm " + l7b[i]
+	l1  = txtdata1[0].replace("00","0")
+	l2  = txtdata1[1]
+	l3a = txtdata1[2]
+	l3b = txtdata1[3]
+	l4a = txtdata1[4]
+	l4b = txtdata1[5]
+	l5  = txtdata2[1]
+	l6a = txtdata2[2]
+	l6b = txtdata2[3]
+	l7a = txtdata2[4]
+	l7b = txtdata2[5]
+	# l1
+	if len(l1)==3:
+		t1 = "\phantom{0}" + l1
+	else:
+		t1 = l1
+	# l2
+	t2 = l2
+	# l3
+	t3 = l3a + " \pm " + l3b
+	# l4
+	if "-" in l4a:
+		t4 = l4a.replace("-","$-$") + " \pm " + l4b
+	else:
+		t4 = "\phantom{$-$}" + l4a + " \pm " + l4b
+	# l5
+	if "-" in l5:
+		t5 = l5.replace("-","$-$")
+	else:
+		t5 = "\phantom{$-$}" + l5
+	# l6
+	if "-" in l6a:
+		t6 = l6a.replace("-","$-$") + " \pm " + l6b
+	else:
+		t6 = "\phantom{$-$}" + l6a + " \pm " + l6b
+	# l7
+	if "-" in l7a:
+		t7 = l7a.replace("-","$-$") + " \pm " + l7b
+	else:
+		t7 = "\phantom{$-$}" + l7a + " \pm " + l7b
 
-		onerow = t1+" & "+t2+" & "+t3+" & "+t4+" && "+t5+" & "+t6+" & "+t7
-
-		return onerow
+	onerow = t1+" & "+t2+" & "+t3+" & "+t4+" && "+t5+" & "+t6+" & "+t7
+	return onerow
 
 
 #####################
@@ -75,5 +76,6 @@ def extract_onerow(txtdata1,txtdata2):
 #####################
 for i in range(len(txtfile1)):
 	galname = txtfile1[i].split("_")[1].replace("ngc","NGC ")
+	table03_galname(txtfile1,txtfile2)
 
 
