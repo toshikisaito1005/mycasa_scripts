f515ad8410fa895e01c2c5e84dabec22cd7cb188
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-25 15:55:00 +0900

    Commit at 2020-04-25 15:55:00

--- mycasa_scripts_active/scripts_ts07_iras18293/fig04_ratio.py
+++ mycasa_scripts_active/scripts_ts07_iras18293/fig04_ratio.py
@@ -15,6 +15,7 @@ ylim = [10, -10]
 value = None
 clip = 0.10027554936773037 # 10**6.0
 freq_factor = 115.27120*115.27120/492.16065100/492.16065100
+cidust_factor = 1.
 
 done = glob.glob(dir_data + "eps/")
 if not done:
@@ -38,27 +39,22 @@ immath(imagename = [ci,co + ".complete"],
        expr = "iif(IM0 >= 0, IM0/IM1*"+str(freq_factor)+", 0.0)",
        outfile = dir_data + "image_ci10/ratio.moment0")
 
+
+# ci dust ratio
+ci = dir_data + "image_ci10/ci10.moment0"
+dust = dir_data + "image_b8contin/b8contin.flux"
+#cliplevel = clip * imstat(co)["max"][0] * 100000.
+
+os.system("rm -rf " + dir_data + "image_ci10/ratio.moment0")
+immath(imagename = [ci,dust],
+       expr = "iif(IM0 >= 0, IM0/IM1*"+str(cidust_factor)+", 0.0)",
+       outfile = dir_data + "image_b8contin/ratio.moment0")
+
 imagenames = glob.glob(dir_data + "image_*/*.moment0")
 for i in range(len(imagenames)):
     os.system("rm -rf " + imagenames[i] + ".fits")
     exportfits(imagename = imagenames[i],
                fitsimage = imagenames[i] + ".fits")
-
-
-# ci dust ratio
-ci = dir_data + "image_ci10/ci10.moment0"
-dust = dir_data + "image_co10/co10.moment0"
-cliplevel = clip * imstat(co)["max"][0] * 100000.
-
-os.system("rm -rf " + co + ".complete")
-immath(imagename = co,
-       expr = "iif(IM0 <= " + str(cliplevel) + ", IM0, 0.0)",
-       outfile = co + ".complete")
-
-os.system("rm -rf " + dir_data + "image_ci10/ratio.moment0")
-immath(imagename = [ci,co + ".complete"],
-       expr = "iif(IM0 >= 0, IM0/IM1*"+str(freq_factor)+", 0.0)",
-       outfile = dir_data + "image_ci10/ratio.moment0")
 
 
 ###12CO(1-0)
@@ -93,3 +89,32 @@ myim.fits2eps(dir_data = dir_data,
               ylim = ylim,
               clim = [0.0,0.26])# [0,0.26])
 
+# moment 0 color + moment 0 contour
+imagename_contour = "image_co10/co10.moment0.fits"
+imagename_color = "image_b8contin/ratio.moment0.fits"
+contour = [0.025, 0.05, 0.10, 0.2, 0.4, 0.8, 0.95]
+title = "[CI]/Dust Luminosity Ratio"
+colorscale = "rainbow"
+color_contour = "black"
+color_beam = "black"
+colorlog = False
+colorbar = True
+colorbar_label = "Ratio"
+output = "eps/iras18293_ratio_ciduxt_m0.png"
+myim.fits2eps(dir_data = dir_data,
+              imagename_color = imagename_color,
+              imagename_contour = imagename_contour,
+              ra_center = ra_center,
+              dec_center = dec_center,
+              title = title,
+              colorbar_label = colorbar_label,
+              output = output,
+              colorscale = colorscale,
+              colorlog = colorlog,
+              color_contour = color_contour,
+              color_beam = color_beam,
+              colorbar = colorbar,
+              value = value,
+              contour = contour,
+              xlim = xlim,
+              ylim = ylim)# [0,0.26])
