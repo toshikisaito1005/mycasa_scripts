a5cff444feff8cd29b2059d14beec006f4a64a67
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-25 17:50:00 +0900

    Commit at 2020-04-25 17:50:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f99_table03.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f99_table03.py
@@ -18,8 +18,23 @@ l7b = data_co21vsr21[:,5]
 
 for i in range(len(l1)):
 	# l1
-	if len(l1[i])==4:
+	if len(l1[i])==3:
 		t1 = "\phantom{0}" + l1[i]
 	else:
 		t1 = l1[i]
-	print(t1)
+	# l2
+	t2 = l2[i]
+	# l3
+	t3 = l3a[i] + " \pm " + l3b[i]
+	# l4
+	if "-" in l4a[i]:
+		t4 = l4a[i].replace("-","$-$") + " \pm " + l4b[i]
+	else:
+		t4 = "\phantom{$-$}" + l4a[i] + " \pm " + l4b[i]
+	# l5
+	if l5[i]:
+		t1 = "\phantom{0}" + l1[i]
+	else:
+		t1 = l1[i]
+
+	print(t1 + " & " + t2 + " & " + t3 + " & " + t4)
