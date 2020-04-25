aa67f7adfa0a785b7f92267df8af44061948a2a1
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-25 17:40:00 +0900

    Commit at 2020-04-25 17:40:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f99_table03.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f99_table03.py
@@ -3,4 +3,8 @@ import numpy as np
 
 data_co21vsco21 = np.loadtxt("table03_ngc0628_co10vsco21.txt",dtype="str")
 
-data_co21vsco21[:,0]
+l1 = [s.replace("00","0") for s in data_co21vsco21[:,0]]
+l2 = data_co21vsco21[:,1]
+l3a = data_co21vsco21[:,2]
+l3b = data_co21vsco21[:,3]
+l4a = data_co21vsco21[:,4]
