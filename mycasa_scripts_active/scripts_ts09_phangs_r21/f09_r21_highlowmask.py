a1f0d7e0b6bfa1ca8969c5dd91c80e1add49556f
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-26 16:35:01 +0900

    Commit at 2020-04-26 16:35:01

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f09_r21_highlowmask.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f09_r21_highlowmask.py
@@ -29,8 +29,8 @@ if not done:
 ### Main Procedure
 #####################
 # common
-clim = [-1.2,1.2]
-color_contour = "grey" # "grey"
+clim = [-1.3,1.3]
+color_contour = "black" # "grey"
 
 # ngc0628
 ra_center = "01:36:41.8"
@@ -44,7 +44,7 @@ value = None
 color_beam = "grey"
 imagename_contour = "ngc0628_co21/co21_04p0.moment0.fits"
 imagename_color = "ngc0628_r21/r21_04p0.moment0.highlowmask.fits"
-contour = [0.04, 0.08, 0.16, 0.32, 0.64, 0.96]
+contour = [0.04]#[0.04, 0.08, 0.16, 0.32, 0.64, 0.96]
 title = "NGC 0628 Line Ratio Mask"
 colorscale = "rainbow"
 colorlog = False
@@ -83,7 +83,7 @@ value = None
 color_beam = "grey"
 imagename_contour = "ngc4321_co21/co21_04p0.moment0.fits"
 imagename_color = "ngc4321_r21/r21_04p0.moment0.highlowmask.fits"
-contour = [0.01, 0.02, 0.04, 0.08, 0.16, 0.32, 0.64, 0.96]
+contour = [0.01]#[0.01, 0.02, 0.04, 0.08, 0.16, 0.32, 0.64, 0.96]
 title = "NGC 4321 Line Ratio Mask"
 colorscale = "rainbow"
 colorlog = False
@@ -121,7 +121,7 @@ value = None
 # R21 color + CO(2-1) moment 0 contour
 imagename_contour = "ngc3627_co21/co21_08p0.moment0.fits"
 imagename_color = "ngc3627_r21/r21_08p0.moment0.highlowmask.fits"
-contour = [0.02, 0.04, 0.08, 0.16, 0.32, 0.64, 0.96]
+contour = [0.02]#[0.02, 0.04, 0.08, 0.16, 0.32, 0.64, 0.96]
 title = "NGC 3627 Line Ratio Mask"
 colorscale = "rainbow"
 colorlog = False
@@ -147,46 +147,4 @@ myim.fits2eps(dir_data = dir_data,
               ylim = ylim,
               clim = clim)
 
-
-### ngc4254
-"""
-ra_center = "12:18:49.3"
-dec_center = "14.25.01.0"
-xlim = [-120, 120]
-ylim = [120, -120]
-value = None
-
-### R21
-# R21 color + CO(2-1) moment 0 contour
-imagename_contour = "ngc4254_co21/co21_08p0.moment0.fits"
-imagename_color = "ngc4254_r21/r21_08p0.moment0.highlowmask.fits"
-contour = [0.04, 0.08, 0.16, 0.32, 0.64, 0.96]
-title = "NGC 4254 Line Ratio Mask"
-colorscale = "bwr"
-colorlog = False
-colorbar = False
-colorbar_label = "Line Ratio"
-output = "eps/ngc4254_r21_mask.png"
-clim = [-1.5,1.5]
-myim.fits2eps(dir_data = dir_data,
-              imagename_color = imagename_color,
-              imagename_contour = imagename_contour,
-              ra_center = ra_center,
-              dec_center = dec_center,
-              title = title,
-              colorbar_label = colorbar_label,
-              output = output,
-              colorscale = colorscale,
-              colorlog = colorlog,
-              color_contour = color_contour,
-              color_beam = color_beam,
-              colorbar = colorbar,
-              value = value,
-              contour = contour,
-              xlim = xlim,
-              ylim = ylim,
-              clim = clim)
-"""
-
 os.system("rm -rf *.last")
-
