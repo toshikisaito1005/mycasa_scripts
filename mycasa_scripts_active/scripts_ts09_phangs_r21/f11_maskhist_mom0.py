ac9459d24c6aec2680157f013fdd23dd21b89261
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-26 21:40:00 +0900

    Commit at 2020-04-26 21:40:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f11_maskhist_mom0.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f11_maskhist_mom0.py
@@ -108,13 +108,21 @@ def plotter(
     _, y_high = np.delete(hist_high[1],-1), hist_high[0]
     y_high = y_high / float(sum(y_high))
     #
-    ax.step(x, y_low, color="blue", lw=1, where="mid")
-    ax.step(x, y_mid, color="green", lw=1, where="mid")
-    ax.step(x, y_high, color="red", lw=1, where="mid")
+    ax.step(x, y_low, color="blue", lw=1.5, where="mid")
+    ax.step(x, y_mid, color="green", lw=1.5, where="mid")
+    ax.step(x, y_high, color="red", lw=1.5, where="mid")
     ax.bar(x, y_low, lw=0, color="blue", alpha=0.2, width=x[1]-x[0], align="center")
     ax.bar(x, y_mid, lw=0, color="green", alpha=0.2, width=x[1]-x[0], align="center")
     ax.bar(x, y_high, lw=0, color="red", alpha=0.2, width=x[1]-x[0], align="center")
     ax.set_ylim(0.0005,np.max([y_low,y_mid,y_high])*1.4)
+
+def plotter_stats(
+    ax,
+    txtdata,
+    ):
+    """
+    """
+    p84
 
 
 #####################
