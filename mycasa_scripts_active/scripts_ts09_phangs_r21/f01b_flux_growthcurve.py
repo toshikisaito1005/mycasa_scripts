0dc5f4e9dde7459945d9e02ddd3fdb61f59327d6
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-26 13:45:00 +0900

    Commit at 2020-04-26 13:45:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f01b_flux_growthcurve.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f01b_flux_growthcurve.py
@@ -44,7 +44,7 @@ def get_beam_intensities(images,freq):
 ### co10
 plt.figure(figsize=(10,10))
 plt.rcParams["font.size"] = 22
-plt.ylim([0,2])
+plt.ylim([0.5,1.5])
 plt.ylabel("Total CO(1-0) Flux Relative To EMPIRE")
 plt.xlabel("Spatial Resolution (kpc)")
 plt.legend(loc = "lower right")
@@ -53,14 +53,19 @@ plt.grid(color="grey")
 for i in range(len(gals)):
     # get data
     images_co10 = glob.glob(dir_data + gals[i] + "_co10/*co10*.moment0")
+    print(images_co10)
     freq = 115.27120 # GHz
     data = get_beam_intensities(images_co10,freq)
     # plot
-    plt.plot(data[:,0]*(scales[i]/1000), data[:,2], size=10, lw=5, alpha=0.4, c=cm.brg(i/2.5), label="NGC 0628")
+    markers, bars = plt.plot(
+        data[:,0]*(scales[i]/1000), data[:,2], "o-", markersize=20, lw=5, c=cm.brg(i/2.5), label="NGC 0628")
+    [bar.set_alpha(0.5) for bar in bars]
+    #
+plt.savefig(dir_data + "eps/missingflux_co10.png", dpi=100)
 
 
 
-plt.savefig(dir_data + "../eps/missingflux_co10.png", dpi=100)
+
 
 ## co21 plot
 plt.figure(figsize=(10,10))
