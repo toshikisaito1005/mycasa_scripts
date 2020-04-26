c6b7e4d80e1416ff33b11ff58fac9cfa7adf36f8
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-26 17:40:00 +0900

    Commit at 2020-04-26 17:40:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f11_maskhist_mom0.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f11_maskhist_mom0.py
@@ -4,8 +4,6 @@ import glob
 import math
 import numpy as np
 import scipy.optimize
-from scipy.optimize import curve_fit
-import matplotlib as mpl
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import matplotlib.patches as pat
@@ -16,7 +14,6 @@ plt.ioff()
 #####################
 ### parameters
 #####################
-dir_data = "/Users/saito/data/mycasa_scripts_active/scripts_ts09_phangs_r21/"
 dir_product = "/Users/saito/data/myproj_active/proj_ts09_phangs_r21/eps/"
 gals = ["ngc0628","ngc3627","ngc4321"]
 dist25 = [4.9, 5.1, 3.0] # arcmin, Leroy et al. 2019
@@ -27,70 +24,22 @@ percents = [0.15,0.025,0.010]
 #####################
 ### functions
 #####################
-def hist_percent(histo,percent):
-    dat_sum = np.sum(histo)
-    dat_sum_from_zero,i = 0,0
-    while dat_sum_from_zero < dat_sum * percent:
-        dat_sum_from_zero += histo[i]
-        i += 1
-    
-    return i
+
 
 
 #####################
 ### main
 #####################
-figure = plt.figure(figsize=(9,9))
-gs = gridspec.GridSpec(nrows=9, ncols=9)
-ax1 = plt.subplot(gs[0:7,0:7])
-plt.rcParams["font.size"] = 16
-
 for i in range(len(gals)):
     galname = gals[i]
-    data = np.loadtxt(dir_data + galname + "_parameter_600pc.txt")
-    # median-subtracted r21
+    galnamelabel = galname.replace("ngc","NGC ")
+    data = np.loadtxt(dir_product + galname + "_parameter_600pc.txt")
+    #
+    dist = data[:,0]
     r21 = data[:,1]
-    med_r21 = np.median(r21[r21>0])
-    norm_r21 = r21 / med_r21
-    # co21
-    co21 = data[:,4]
-    co21snr = data[:,5]
-    # cut data
-    cut_r21 = (r21 > 0)
-    cut_co21 = (co21 > co21.max() * percents[i])
-    cut_all = np.where((cut_r21) & (cut_co21))
-
-    norm_r21 = norm_r21[cut_all]
-    co21 = co21[cut_all]
-    # radial binning
-    n, _ = np.histogram(co21, bins=nbins)
-    sy, _ = np.histogram(co21, bins=nbins, weights=norm_r21)
-    sy2, _ = np.histogram(co21, bins=nbins, weights=norm_r21*norm_r21)
-    mean = sy / n
-    std = np.sqrt(sy2/n - mean*mean)
-
-    ax1.errorbar(
-        (_[1:] + _[:-1])/2, mean, yerr=std,
-        color=cm.brg(i/2.5), lw=4, #alpha=0.5,
-        label = galname.replace("ngc","NGC ")
-        )
-    """
-    ax1.scatter(
-        co21, norm_r21,
-        color=cm.brg(i/2.5),
-        lw=0, alpha=0.2, s=50,
-        label = galname.replace("ngc","NGC "))
-    """
-
-ax1.grid()
-ax1.legend(ncol=2, loc="upper right")
-#ax1.set_xlim([0,1])
-ax1.set_xscale("log")
-ax1.set_ylim([0,2])
-ax1.set_xlabel("$I_{CO(2-1)}$ (currently Jy/b, change to K)")
-ax1.set_ylabel("$R_{21}$/$Med(R_{21})$")
-
-ax1.set_title("$R_{21}$/$Med(R_{21})$ vs. $I_{CO(2-1)}$")
-plt.savefig(dir_product+"radial_r21_vs_co21_mom0.png",dpi=200)
-
-os.system("rm -rf *.last")
+    r21err = data[:,2]
+    co21 = data[:,3]
+    co21snr = data[:,4]
+    co10 = data[:,5]
+    co10snr = data[:,6]
+    tpeak = data[:,7]
