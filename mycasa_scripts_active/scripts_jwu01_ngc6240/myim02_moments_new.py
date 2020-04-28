8a197556b6c43f8b57804296a1ecdc423a06c0e0
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-28 21:10:00 +0900

    Commit at 2020-04-28 21:10:00

--- mycasa_scripts_active/scripts_jwu01_ngc6240/myim02_moments_new.py
+++ mycasa_scripts_active/scripts_jwu01_ngc6240/myim02_moments_new.py
@@ -8,198 +8,272 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 plt.ioff()
 
-# CASA imports
-from taskinit import *
-from importfits import importfits
-from imregrid import imregrid
-from imsmooth import imsmooth
-from imval import imval
-from immoments import immoments
-from immath import immath
-from makemask import makemask
-from imhead import imhead
-from imstat import imstat
-from exportfits import exportfits
-import analysisUtils as aU
-mycl = aU.createCasaTool(cltool)
-mycs = aU.createCasaTool(cstool)
-myia = aU.createCasaTool(iatool)
-myrg = aU.createCasaTool(rgtool)
-myqa = aU.createCasaTool(qatool)
+imageco10 = "co10_cube.image"
+imageco21 = "co21_cube.image"
 
+### set signal-to-noise ratio you want to use
+snr_mom = 3.0
 
-#####################
-### Parameters
-#####################
-dir_data = "/Users/saito/data/myproj_active/proj_jwu01_ngc6240/data/"
-imagenames = glob.glob(dir_data + "*.smooth.regrid")
-imagenames.sort()
-noises = [0.0011,0.005,0.0006,0.0004,0.0004]
-pbcuts = [0.75,0.35,0.985,0.985,0.985]
-snr = 2.0
-
-
-#####################
-### Functions
-#####################
-def createmask(imagename,thres,outmask):
-    """
-    for moment map creation
-    """
-    os.system("rm -rf " + outmask)
-    immath(imagename = imagename,
-           mode = "evalexpr",
-           expr = "iif(IM0 >= " + str(thres) + ", 1.0, 0.0)",
-           outfile = outmask)
-    imhead(imagename = outmask,
-           mode = "del",
-           hdkey = "beammajor")
-
+##################################################
+### define some functions
+##################################################
 def func1(x, a, c):
-    return a*np.exp(-(x)**2/(2*c**2))
+    return a * np.exp(-(x)**2/(2*c**2))
 
-def noisehist(imagename,noises_byeye,output,bins=200,thres=0.00001):
+def noisehist(
+    imagename,
+    noises_byeye,
+    output,
+    snr,
+    title,
+    bins=200,
+    thres=0.00001,
+    logscale=True,
+    plotter=True,
+    ):
     """
     """
-    shape = imhead(imagename,mode="list")["shape"]
+    ### get pixel values from a datacube
+    shape = imhead(imagename, mode="list")["shape"]
     box = "0,0,"+str(shape[0]-1)+","+str(shape[1]-1)
-    data = imval(imagename,box=box)
+    data = imval(imagename, box=box)
     pixvalues = data["data"].flatten()
     pixvalues = pixvalues[abs(pixvalues)>thres]
-    
-    # plot
-    histrange = [pixvalues.min()/1.5-0.02,-pixvalues.min()/1.5+0.02]
+    ### plot
+    # get plot range
+    histrange = [pixvalues.min()/1.5 - 0.02, -pixvalues.min()/1.5 + 0.02]
+    # prepare for plot
     plt.figure(figsize=(10,10))
     plt.rcParams["font.size"] = 22
-    histdata = plt.hist(pixvalues,
-                        bins=bins,
-                        range=histrange,
-                        lw=0,
-                        log=True,
-                        color="blue",
-                        alpha=0.5)
-        
-    popt, pcov = curve_fit(func1,
-                           histdata[1][2:][histdata[1][2:]<noises_byeye],
-                           histdata[0][1:][histdata[1][2:]<noises_byeye],
-                           p0 = [np.max(histdata[0][1:][histdata[1][2:]<noises_byeye]),
-                                 noises_byeye],
-                           maxfev = 10000)
-                        
+    plt.subplots_adjust(bottom=0.08, left=0.10, right=0.99, top=0.95)
+    # plot
+    histdata = \
+        plt.hist(pixvalues,
+                 bins=bins,
+                 range=histrange,
+                 lw=0,
+                 log=logscale,
+                 color="blue",
+                 alpha=0.3,
+                 label="positive pixels")
+    plt.hist(pixvalues * -1,
+             bins=bins,
+             range=histrange,
+             lw=0,
+             log=logscale,
+             color="red",
+             alpha=0.3,
+             label="negative pixels (reversed)")
+    # fit the histogram using a Gaussian
+    popt, pcov = \
+        curve_fit(
+            func1,
+            histdata[1][2:][histdata[1][2:]<noises_byeye],
+            histdata[0][1:][histdata[1][2:]<noises_byeye],
+            p0 = [np.max(histdata[0][1:][histdata[1][2:]<noises_byeye]),
+                  noises_byeye],
+            maxfev = 10000)
+    #
     x = np.linspace(histdata[1][1], histdata[1][-1], 200)
-    plt.plot(x, func1(x, popt[0], popt[1]),
-             '-', c="black", lw=5, label = "1 sigma = " + str(np.round(popt[1]*1000,2)) + " mJy beam$^{-1}$")
-    plt.plot([0,0],
-             [1e1,np.max(histdata[0][1:][histdata[1][2:]<noises_byeye])*8.0],
-             '-',color='black',lw=2)
-    plt.plot([popt[1],popt[1]],
-             [1e1,np.max(histdata[0][1:][histdata[1][2:]<noises_byeye])*8.0],
-             '--',color='black',lw=2)
-    plt.plot([popt[1]*2.5,popt[1]*2.5],
-             [1e1,np.max(histdata[0][1:][histdata[1][2:]<noises_byeye])*8.0],
-             '--',color='black',lw=4)
-    plt.plot([-popt[1],-popt[1]],
-             [1e1,np.max(histdata[0][1:][histdata[1][2:]<noises_byeye])*8.0],
-             '--',color='black',lw=2)
-                        
-    plt.title(imagename.split("/")[-1])
-    plt.xlim(histrange)
-    plt.ylim([1e1,np.max(histdata[0][1:][histdata[1][2:]<noises_byeye])*8.0])
+    plt.plot(x, func1(x, popt[0], popt[1]), '-', c="black", lw=5)
+    plt.plot([0, 0],
+             [2e1, np.max(histdata[0][1:][histdata[1][2:]<noises_byeye]) * 1.2],
+             "-",
+             color="black",
+             lw=2)
+    plt.plot([popt[1], popt[1]],
+             [2e1, np.max(histdata[0][1:][histdata[1][2:]<noises_byeye]) * 1.2],
+             "--",
+             color="black",
+             lw=2,
+             label="1.0 sigma = " + str(np.round(popt[1]*1000.,2)) + " mJy beam$^{-1}$")
+    plt.plot([popt[1]*snr,popt[1]*snr],
+             [2e1, np.max(histdata[0][1:][histdata[1][2:]<noises_byeye]) * 1.2],
+             "--",
+             color="black",
+             lw=5,
+             label=str(snr) + " sigma = " + str(np.round(popt[1]*snr*1000.,2)) + " mJy beam$^{-1}$")
+    plt.plot([-popt[1], -popt[1]],
+             [2e1, np.max(histdata[0][1:][histdata[1][2:]<noises_byeye]) * 1.2],
+             "--",
+             color="black",
+             lw=2)
+    #
+    plt.xlim(0, histrange[1])
+    plt.ylim([2e1, np.max(histdata[0][1:][histdata[1][2:]<noises_byeye]) * 1.2])
     plt.xlabel("Pixel value (Jy beam$^{-1}$)")
     plt.ylabel("Number of pixels")
+    plt.title(title)
     plt.legend(loc = "upper right")
-    plt.savefig(output,dpi=100)
-                        
+    if plotter==True:
+      plt.savefig(output,dpi=100)
+
     return popt[1]
 
+def eazy_immoments(
+    imagename,
+    snr_mom,
+    nchan,
+    maskname=None,
+    ):
+    """
+    """
+    ### create mask
+    smoothcube1 = imagename + ".smooth1"
+    smoothcube2 = imagename + ".smooth2"
+    smoothcube3 = imagename + ".smooth3"
 
-#####################
-### Main
-#####################
-dir_eps = dir_data + "../eps/"
-done = glob.glob(dir_eps)
-if not done:
-    os.mkdir(dir_eps)
 
-for i in range(len(imagenames)):
-    # prepare workinf directory e.g., image_co10
-    name_line = imagenames[i].split("ngc6240_")[1].split("_")[0]
-    dir_image = dir_data+"../image_"+name_line+"/"
-    pbimage = dir_image + name_line + "_cube.pb"
-    cubeimage = dir_image + name_line + "_cube.image"
-    os.system("rm -rf " + dir_image)
-    os.system("mkdir " + dir_image)
-    os.system("cp -r " + imagenames[i] + " " + cubeimage)
-    os.system("cp -r " + imagenames[i].replace("smooth","pb") + " " + pbimage)
 
-    print("### woking on " + name_line)
-    # noise histgrams
-    output = dir_eps + "noise_" + name_line + ".png"
-    popt1 = noisehist(imagenames[i],noises[i],output,bins=200,thres=0.0001)
-
-    # imsmooth
-    cubesmooth1 = cubeimage.replace(".image",".smooth1") # 4.0 mJy
-    imsmooth(imagename = cubeimage,
-             targetres = True,
-             major = "1.2arcsec",
-             minor = "1.2arcsec",
-             pa = "0deg",
-             outfile = cubesmooth1)
-
-    cubesmooth2 = cubeimage.replace(".image",".smooth2") # 10 mJy
-    imsmooth(imagename = cubeimage,
-             targetres = True,
-             major = "3.5arcsec",
-             minor = "3.5arcsec",
-             pa = "0deg",
-             outfile = cubesmooth2)
-
-    # create mask
-    createmask(cubeimage,popt1*1.*2.0,dir_image+"/"+name_line+"_mask0.image")
-    createmask(cubesmooth1,popt1*2.*4.5,dir_image+"/"+name_line+"_mask1.image")
-    createmask(cubesmooth2,popt1*5.*9.0,dir_image+"/"+name_line+"_mask2.image")
+    if maskname==None:
+        os.system("rm -rf " + cubeimage+".masked")
+        #os.system("rm -rf " + dir_image+"*.noise")
+        os.system("rm -rf " + dir_image+"*.mask*")
     
-    immath(imagename = [dir_image+"/"+name_line+"_mask0.image",
-                        dir_image+"/"+name_line+"_mask1.image",
-                        dir_image+"/"+name_line+"_mask2.image",
-                        dir_image+"/"+name_line+"_cube.pb"],
-           expr = "iif(IM0+IM1+IM2 >= 2.0, 1.0, 0.0)",
-           outfile = dir_image+"/"+name_line+"_mask_tmp.image")
-        
-    immath(imagename = [dir_image+"/"+name_line+"_mask_tmp.image",
-                        dir_image+"/"+name_line+"_cube.pb"],
-           expr = "iif(IM1 >= "+str(pbcuts[i])+", IM0, 0.0)",
-           outfile = dir_image+"/"+name_line+"_mask.image")
+        # imsmooth
+        cubesmooth1 = cubeimage.replace(".image",".smooth1") # 4.0 mJy
+        bmaj = imhead(cubeimage,"list")["beammajor"]["value"]
+        imsmooth(imagename = cubeimage,
+                 targetres = True,
+                 major = str(bmaj*3.0) + "arcsec",#1.2) + "arcsec",
+                 minor = str(bmaj*3.0) + "arcsec",#1.2) + "arcsec",
+                 pa = "0deg",
+                 outfile = cubesmooth1)
+        cubesmooth2 = cubeimage.replace(".image",".smooth2") # 10 mJy
+        imsmooth(imagename = cubeimage,
+                 targetres = True,
+                 major = str(bmaj*5.0) + "arcsec",
+                 minor = str(bmaj*5.0) + "arcsec",
+                 pa = "0deg",
+                 outfile = cubesmooth2)
+        cubesmooth3 = cubeimage.replace(".image",".smooth3") # 10 mJy
+        imsmooth(imagename = cubeimage,
+                 targetres = True,
+                 major = str(bmaj*7.0) + "arcsec",
+                 minor = str(bmaj*7.0) + "arcsec",
+                 pa = "0deg",
+                 outfile = cubesmooth3)
+        # noise
+        noisesmooth1 = noisehist(cubesmooth1,0.02,"test",3.0,bins=200,thres=0.0001,plotter=False)
+        noisesmooth2 = noisehist(cubesmooth2,0.02,"test",3.0,bins=200,thres=0.0001,plotter=False)
+        noisesmooth3 = noisehist(cubesmooth3,0.02,"test",3.0,bins=200,thres=0.0001,plotter=False)
+        # create mask
+        #tscreatemask(cubeimage,noise*1.*2.,dir_image+name_line+"_mask0.image")
+        tscreatemask(cubesmooth1,noisesmooth1*0.0,dir_image+name_line+"_mask1.image")
+        tscreatemask(cubesmooth2,noisesmooth2*0.0,dir_image+name_line+"_mask2.image")
+        tscreatemask(cubesmooth3,noisesmooth3*0.0,dir_image+name_line+"_mask3.image")
 
-    os.system("rm -rf "+dir_image+"/"+name_line+"_cube.smooth1")
-    os.system("rm -rf "+dir_image+"/"+name_line+"_cube.smooth2")
-    os.system("rm -rf "+dir_image+"/"+name_line+"_mask0.image")
-    os.system("rm -rf "+dir_image+"/"+name_line+"_mask1.image")
-    os.system("rm -rf "+dir_image+"/"+name_line+"_mask2.image")
-    os.system("rm -rf "+dir_image+"/"+name_line+"_mask_tmp.image")
+        immath(imagename = [dir_image+name_line+"_mask1.image", dir_image+name_line+"_mask2.image", dir_image+name_line+"_mask3.image"],
+               expr = "iif(IM0+IM1 >= 2.0, 1.0, 0.0)",
+               outfile = dir_image+name_line+"_"+beamp+"_mask.image")
+
+        os.system("rm -rf "+cubesmooth1)
+        os.system("rm -rf "+cubesmooth2)
+        os.system("rm -rf "+cubesmooth3)
+        os.system("rm -rf "+dir_image+name_line+"_mask0.image")
+        os.system("rm -rf "+dir_image+name_line+"_mask1.image")
+        os.system("rm -rf "+dir_image+name_line+"_mask2.image")
+
+        mask_use_here = dir_image+name_line+"_"+beamp+"_mask.image"
+
+    else:
+        mask_use_here = dir_image+name_line+"_"+beamp+"_mask.image"
+        os.system("cp -r " + maskname + " " + mask_use_here)
+
+    immath(imagename = [cubeimage,mask_use_here],
+           expr = "iif( IM0>=" + str(noise*snr_mom) + ", IM0*IM1, 0.0)",
+           outfile = cubeimage+".masked")
+    #os.system("rm -rf " + mask_use_here)
+
+    vch = abs(imhead(cubeimage,mode="list")["cdelt4"]) / 115.27120e9 * 299792.458
     
-    impbcor(imagename = cubeimage,
-            pbimage = pbimage,
-            cutoff = 0.3,
-            outfile = cubeimage+".pbcor")
-            
-    immath(imagename = [cubeimage+".pbcor",dir_image+"/"+name_line+"_mask.image"],
-           expr = "IM0*IM1",
-           outfile = cubeimage+".pbcor.masked")
-    
-    immoments(imagename = cubeimage+".pbcor.masked",
+    immath(imagename = cubeimage+".masked",
+           expr = "iif( IM0>0, 1.0/" + str(vch) + ", 0.0)",
+           outfile = cubeimage+".maskedTF")
+
+    immoments(imagename = cubeimage+".maskedTF",
               moments = [0],
-              includepix = [popt1*snr,100000.],
-              outfile = dir_image+"/"+name_line+".moment0")
-        
-    immoments(imagename = cubeimage+".pbcor.masked",
+              outfile = dir_image+name_line+".moment0.noise_tmp") # Nch
+
+    beamarea_pix = tsbeam_area(dir_image+name_line+".moment0.noise_tmp")
+    immath(dir_image+name_line+".moment0.noise_tmp",
+           expr = str(vch)+"*sqrt(IM0)*"+str(noise),#+"/"+str(np.sqrt(beamarea_pix)),
+           outfile = dir_image+name_line+"_"+beamp+".moment0.noise")
+
+    immoments(imagename = cubeimage+".masked",
+              moments = [0],
+              #includepix = [noises[i]*3.0,10000.],
+              outfile = dir_image+name_line+".moment0_tmp")
+
+    immoments(imagename = cubeimage+".masked",
               moments = [1],
-              includepix = [popt1*snr,100000.],
-              outfile = dir_image+"/"+name_line+".moment1")
-              
-    immoments(imagename = cubeimage+".pbcor.masked",
+              #includepix = [noises[i]*3.0,10000.],
+              outfile = dir_image+name_line+".moment1_tmp")
+
+    immoments(imagename = cubeimage+".masked",
               moments = [8],
-              includepix = [popt1*snr,100000.],
-              outfile = dir_image+"/"+name_line+".moment8")
+              #includepix = [noises[i]*3.0,10000.],
+              outfile = dir_image+name_line+".moment8_tmp")
 
+    # masking
+    peak = imstat(dir_image+name_line+".moment0_tmp")["max"][0]
+    
+    #
+    immath(imagename = [dir_image+name_line+".moment0_tmp",
+                        dir_image+name_line+".moment0_tmp"],
+           expr = "iif( IM0>=" + str(peak*percent) + ", IM1, 0.0)",
+           outfile = dir_image+name_line+"_"+beamp+".moment0_tmp2")
+    
+    immath(imagename = [dir_image+name_line+"_"+beamp+".moment0_tmp2",
+                        dir_image+name_line+".moment0.noise_tmp"],
+           expr = "iif( IM1>="+str(nchan)+", IM0, 0.0)",
+           outfile = dir_image+name_line+"_"+beamp+".moment0")
 
+    #
+    immath(imagename = [dir_image+name_line+".moment0_tmp",
+                        dir_image+name_line+".moment1_tmp"],
+           expr = "iif( IM0>=" + str(peak*percent) + ", IM1, 0.0)",
+           outfile = dir_image+name_line+"_"+beamp+".moment1_tmp2")
+    
+    immath(imagename = [dir_image+name_line+"_"+beamp+".moment1_tmp2",
+                        dir_image+name_line+".moment0.noise_tmp"],
+           expr = "iif( IM1>="+str(nchan)+", IM0, 0.0)",
+           outfile = dir_image+name_line+"_"+beamp+".moment1")
+
+    #      
+    immath(imagename = [dir_image+name_line+".moment0_tmp",
+                        dir_image+name_line+".moment8_tmp"],
+           expr = "iif( IM0>=" + str(peak*percent) + ", IM1, 0.0)",
+           outfile = dir_image+name_line+"_"+beamp+".moment8_tmp2")
+    
+    immath(imagename = [dir_image+name_line+"_"+beamp+".moment8_tmp2",
+                        dir_image+name_line+".moment0.noise_tmp"],
+           expr = "iif( IM1>="+str(nchan)+", IM0, 0.0)",
+           outfile = dir_image+name_line+"_"+beamp+".moment8")
+
+    #
+    immath(imagename = [dir_image+name_line+"_"+beamp+".moment0",
+                         dir_image+name_line+"_"+beamp+".moment0.noise"],
+           expr = "IM0/IM1",
+           outfile = dir_image+name_line+"_"+beamp+".moment0.snratio_tmp")
+    
+    immath(imagename = [dir_image+name_line+"_"+beamp+".moment0.snratio_tmp",
+                        dir_image+name_line+".moment0.noise_tmp"],
+           expr = "iif( IM1>="+str(nchan)+", IM0, 0.0)",
+           outfile = dir_image+name_line+"_"+beamp+".moment0.snratio")
+
+    os.system("rm -rf " + cubeimage+".maskedTF")
+    os.system("rm -rf " + dir_image+name_line+".moment0.noise_tmp")
+    os.system("rm -rf " + dir_image+name_line+"_"+beamp+".moment0.snratio_tmp")
+    os.system("rm -rf " + dir_image+name_line+".moment0_tmp")
+    os.system("rm -rf " + dir_image+name_line+".moment1_tmp")
+    os.system("rm -rf " + dir_image+name_line+".moment8_tmp")
+    os.system("rm -rf " + dir_image+name_line+"_"+beamp+".moment0_tmp2")
+    os.system("rm -rf " + dir_image+name_line+"_"+beamp+".moment1_tmp2")
+    os.system("rm -rf " + dir_image+name_line+"_"+beamp+".moment8_tmp2")
+
+    return mask_use_here
+
+##################################################
+### main part
+##################################################
