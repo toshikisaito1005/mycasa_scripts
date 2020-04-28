1d2942af2add715fdb02baacb7926119791e90ec
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-28 16:50:00 +0900

    Commit at 2020-04-28 16:50:00

--- mycasa_scripts_active/scripts_phangs05_v3_vs_v4/script31_get_beam_200428.py
+++ mycasa_scripts_active/scripts_phangs05_v3_vs_v4/script31_get_beam_200428.py
@@ -1,4 +1,6 @@
 import os, sys
+import matplotlib.pyplot as plt
+plt.ioff()
 
 dir_data = "/Users/saito/data/phangs/compare_v3p4_v4/data/"
 dir_product = "/Users/saito/data/phangs/compare_v3p4_v4/product/"
@@ -21,7 +23,31 @@ imgaename = dir_data + imgaename
 # get beam
 data = imhead(imgaename, mode="list")["perplanebeams"]
 
+# get bmaj
+list_chan = []
 list_bmaj = []
-for i in range(len(data)-3):
+list_bmin = []
+for i in range(len(data)-4):
 	bmaj = data["*"+str(i)]["major"]["value"]
+	bmin = data["*"+str(i)]["minor"]["value"]
+	list_chan.append(i)
 	list_bmaj.append(bmaj)
+	list_bmin.append(bmin)
+
+# plot
+plt.figure(figsize=(8,3))
+plt.grid()
+plt.subplots_adjust(left=0.15, right=0.95, top=0.90, bottom=0.15)
+plt.rcParams["font.size"] = 14
+gs = gridspec.GridSpec(nrows=9, ncols=9)
+ax1 = plt.subplot(gs[0:9,0:9])
+ax2 = ax1.twinx()
+
+ax1.scatter(list_chan, list_bmaj, lw=0, color="red", label="bmaj")
+ax2.scatter(list_chan, list_bmin, lw=0, color="blue", label="bmin")
+
+
+plt.xlabel("Channel")
+plt.ylabel("bmaj (arcsec)")
+plt.legend()
+plt.savefig(dir_product + "ngc4304_bmaj_200428.png", dpi=300)
