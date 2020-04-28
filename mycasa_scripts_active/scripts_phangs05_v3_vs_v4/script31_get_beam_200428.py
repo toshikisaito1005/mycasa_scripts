809577d8e14c6a47c05df110d9b86d1303477094
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-28 16:55:00 +0900

    Commit at 2020-04-28 16:55:00

--- mycasa_scripts_active/scripts_phangs05_v3_vs_v4/script31_get_beam_200428.py
+++ mycasa_scripts_active/scripts_phangs05_v3_vs_v4/script31_get_beam_200428.py
@@ -1,5 +1,6 @@
 import os, sys
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 plt.ioff()
 
 dir_data = "/Users/saito/data/phangs/compare_v3p4_v4/data/"
@@ -37,17 +38,22 @@ for i in range(len(data)-4):
 # plot
 plt.figure(figsize=(8,3))
 plt.grid()
-plt.subplots_adjust(left=0.15, right=0.95, top=0.90, bottom=0.15)
+plt.subplots_adjust(left=0.10, right=0.85, top=0.90, bottom=0.15)
 plt.rcParams["font.size"] = 14
+
 gs = gridspec.GridSpec(nrows=9, ncols=9)
 ax1 = plt.subplot(gs[0:9,0:9])
 ax2 = ax1.twinx()
+ax1.grid()
 
 ax1.scatter(list_chan, list_bmaj, lw=0, color="red", label="bmaj")
 ax2.scatter(list_chan, list_bmin, lw=0, color="blue", label="bmin")
 
-
-plt.xlabel("Channel")
-plt.ylabel("bmaj (arcsec)")
-plt.legend()
+ax1.set_xlabel("Channel")
+ax1.set_ylabel("bmaj (arcsec)")
+ax2.set_ylabel("bmin (arcsec)")
+ax1.set_ylim([7.33,7.44])
+ax2.set_ylim([4.24,(7.44-7.33)*4.24/7.33+4.24])
+ax1.legend(loc="upper left")
+ax2.legend(loc="upper right")
 plt.savefig(dir_product + "ngc4304_bmaj_200428.png", dpi=300)
