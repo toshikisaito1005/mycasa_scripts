2f905bb0b87b75e4a22eff014e8e1e4253c77cff
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-29 14:50:00 +0900

    Commit at 2020-04-29 14:50:00

--- mycasa_scripts_active/scripts_phangs05_v3_vs_v4/script32_get_fits.py
+++ mycasa_scripts_active/scripts_phangs05_v3_vs_v4/script32_get_fits.py
@@ -1,8 +1,8 @@
 import os, sys, glob
 import shutil
 
-dir_v3 = "/Users/saito/data/phangs/compare_v3p4_v4/data/"
-dir_v4 = "/Users/saito/data/phangs/compare_v3p4_v4/data_pipe_200428/"
+dir_v3 = "/Users/saito/data/phangs/compare_v3p4_v4/data_oldpipe_v3_200429/"
+dir_v4 = "/Users/saito/data/phangs/compare_v3p4_v4/data_pipe_v4_200428/"
 dir_output = "/Users/saito/data/phangs/compare_v3p4_v4/data_ready_200428/"
 
 
@@ -14,12 +14,12 @@ done = glob.glob(dir_output)
 if not done:
 	os.mkdir(dir_output)
 
-v3_image    = glob.glob(dir_v3 + "ngc4303_7m_co21_v3.image")[0]
-v3_model    = glob.glob(dir_v3 + "ngc4303_7m_co21_v3.model")[0]
-v3_pb       = glob.glob(dir_v3 + "ngc4303_7m_co21_v3.pb")[0]
-v3_residual = glob.glob(dir_v3 + "ngc4303_7m_co21_v3.residual")[0]
-v3_weight   = glob.glob(dir_v3 + "ngc4303_7m_co21_v3.weight")[0]
-v3_mask     = glob.glob(dir_v3 + "ngc4303_7m_co21_v3.mask")[0]
+v3_image    = glob.glob(dir_v3 + "ngc4303_7m_co21.image")[0]
+v3_model    = glob.glob(dir_v3 + "ngc4303_7m_co21.model")[0]
+v3_pb       = glob.glob(dir_v3 + "ngc4303_7m_co21.pb")[0]
+v3_residual = glob.glob(dir_v3 + "ngc4303_7m_co21.residual")[0]
+v3_weight   = glob.glob(dir_v3 + "ngc4303_7m_co21.weight")[0]
+v3_mask     = glob.glob(dir_v3 + "ngc4303_7m_co21.mask")[0]
 v3_products = [v3_image, v3_model, v3_pb, v3_residual, v3_weight, v3_mask]
 
 v4_image    = glob.glob(dir_v4 + "ngc4303_7m_co21.image")[0]
@@ -32,7 +32,7 @@ v4_products = [v4_image, v4_model, v4_pb, v4_residual, v4_weight, v4_mask]
 
 for i in range(len(v3_products)):
     imagename = v3_products[i]
-    output = dir_output + imagename.split("/")[-1]
+    output = dir_output + imagename.split("/")[-1].replace("_7m_co21","_7m_co21_v3")
     os.system("rm -rf " + output)
     shutil.copytree(imagename, output)
 
