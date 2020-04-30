586470bf8eddb8c828bede3d8b3268735d00c51d
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-30 17:55:01 +0900

    Commit at 2020-04-30 17:55:01

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f10b_histo.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f10b_histo.py
@@ -9,6 +9,7 @@ plt.ioff()
 dir_data = "/Users/saito/data/myproj_active/proj_ts09_phangs_r21/ngc0628_r21/"
 maskname = dir_data + "r21_04p0.moment0.highlowmask"
 imagename = dir_data + "r21_04p0.moment0"
+bins=150
 
 
 ### maskname
@@ -16,18 +17,17 @@ shape = imhead(maskname,mode="list")["shape"]
 box = "0,0,"+str(shape[0]-1)+","+str(shape[1]-1)
 data = imval(maskname,box=box)
 data = data["data"][data["mask"]==True]
-data = data.flatten()
-#
-mask_low = data[data<0]
-mask_mid = data[data==0]
-mask_high = data[data>0]
+mask = data.flatten()
+
 
 ### imagename
 data = imval(imagename,box=box)
 data = data["data"][data["mask"]==True]
 data = data.flatten()
 #
-data_low = data[mask_low]
+data_low = data[mask==-1]
+data_mid = data[mask==0]
+data_high = data[mask==1]
 
 
 ###
@@ -35,8 +35,8 @@ plt.figure()#figsize=(10,10))
 plt.rcParams["font.size"] = 14
 plt.grid(axis="x")
 
-plt.hist(data_low,range=[0,1.2],bins=100,color="blue")
-plt.hist(data_mid,range=[0,1.2],bins=100,color="green")
-plt.hist(data_high,range=[0,1.2],bins=100,color="red")
+plt.hist(data_low,range=[0,1.5],bins=bins,color="blue",lw=0,alpha=0.5)
+plt.hist(data_mid,range=[0,1.5],bins=bins,color="green",lw=0,alpha=0.5)
+plt.hist(data_high,range=[0,1.5],bins=bins,color="red",lw=0,alpha=0.5)
 
 plt.savefig(dir_data+"../eps/fig_mask_histo.png",dpi=100)
