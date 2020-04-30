5aaa296fcad5d1b17e3b659abcd93d1c9bd7e53b
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-30 17:45:00 +0900

    Commit at 2020-04-30 17:45:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f10b_histo.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f10b_histo.py
@@ -14,5 +14,14 @@ imagename = dir_data + "r21_04p0.moment0.highlowmask"
 shape = imhead(imagename,mode="list")["shape"]
 box = "0,0,"+str(shape[0]-1)+","+str(shape[1]-1)
 data = imval(imagename,box=box)
-data = data["data"] * data["mask"]
-pixvalues = data.flatten()
+data = data["data"][data["mask"]==True]
+data = data.flatten()
+
+#
+data_low = data[data<0]
+data_mid = data[data==0]
+data_high = data[data>0]
+
+#
+plt.figure()#figsize=(10,10))
+plt.rcParams["font.size"] = 14
