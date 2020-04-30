46e91f1265d5066dba82455797aa3d7cff9503f0
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-30 18:10:00 +0900

    Commit at 2020-04-30 18:10:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f10b_histo.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f10b_histo.py
@@ -36,12 +36,18 @@ line_low = np.percentile(data,50-33.3/2)
 
 ###
 plt.figure(figsize=(8,8))#figsize=(10,10))
-plt.rcParams["font.size"] = 14
+plt.rcParams["font.size"] = 16
 plt.grid(axis="x")
 
 plt.hist(data_low,range=[0,1.5],bins=bins,color="blue",lw=0,alpha=0.5,label="Low")
 plt.hist(data_mid,range=[0,1.5],bins=bins,color="green",lw=0,alpha=0.5,label="Mid ")
 plt.hist(data_high,range=[0,1.5],bins=bins,color="red",lw=0,alpha=0.5,label="high")
+
+plt.plot([line_high,line_high],[0,400],"--",lw=3,color="black")
+plt.plot([line_low,line_low],[0,400],"--",lw=3,color="black")
+
+plt.text(line_high+0.02,380,"66.7%")
+plt.text(line_low-0.15,380,"33.3%")
 
 plt.legend()
 plt.savefig(dir_data+"../eps/fig_mask_histo.png",dpi=100)
