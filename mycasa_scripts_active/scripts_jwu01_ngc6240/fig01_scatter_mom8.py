efde59b37e613be9070bdd932e0f967d070e39a1
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-04-30 15:05:00 +0900

    Commit at 2020-04-30 15:05:00

--- mycasa_scripts_active/scripts_jwu01_ngc6240/fig01_scatter_mom8.py
+++ mycasa_scripts_active/scripts_jwu01_ngc6240/fig01_scatter_mom8.py
@@ -3,38 +3,38 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import Normalize
 plt.ioff()
 
-# データを読み込む。
+#
 data = np.loadtxt("n6240_mom8_data.txt")
 dist = data[:,0]
 co10 = data[:,1]
 co21 = data[:,2]
 
-# plotに向けての下準備です。
-fig = plt.figure(figsize=(10,10))                # 図の大きさの設定
-ax1 = fig.add_subplot(111)                       # これはおまじないです
-ax1.grid(which='both',linestyle='--')            # 図にグリッドを表示させます
-plt.rcParams["font.size"] = 22                   # 図中の文字サイズの設定
-plt.subplots_adjust(bottom=0.15, left=0.15, right=0.95, top=0.85)  # 図の余白設定
+#
+fig = plt.figure(figsize=(10,10))                #
+ax1 = fig.add_subplot(111)                       #
+ax1.grid(which='both',linestyle='--')            #
+plt.rcParams["font.size"] = 22                   #
+plt.subplots_adjust(bottom=0.15, left=0.15, right=0.95, top=0.85)  #
 
-cscatter = ax1.scatter(np.log10(co10),           # data for x-axis
-                       np.log10(co21),           # data for y-axis
-                       lw = 0,                   # 点の輪郭の太さ
-                       c = dist,                 # data for colorbar
-                       cmap = "jet",             # color code for c
-                       alpha = 0.5,              # 点の透明度
-                       s = 40,                   # 点の大きさ
-                       norm=Normalize(vmin=0, vmax=7)) # おまじないです
+cscatter = ax1.scatter(np.log10(co10),           #
+                       np.log10(co21),           #
+                       lw = 0,                   #
+                       c = dist,                 #
+                       cmap = "jet",             #
+                       alpha = 0.5,              #
+                       s = 40,                   #
+                       norm=Normalize(vmin=0, vmax=7)) #
 
-ax1.plot([-10,10],[-10,10],"k-",lw=2)            # 1:1の線を引く
+ax1.plot([-10,10],[-10,10],"k-",lw=2)            #
 
 cbar = plt.colorbar(cscatter)
 
-ax1.set_xlim([-0.7,1.7])                         # xの範囲の指定
-ax1.set_ylim([-0.7,1.7])                         # yの範囲の指定
-cbar.set_clim([0,7])                             # colorbarの範囲の指定
+ax1.set_xlim([-0.7,1.7])                         #
+ax1.set_ylim([-0.7,1.7])                         #
+cbar.set_clim([0,7])                             #
 
-ax1.set_xlabel("CO(1-0) Brightness Temperature (K)") # xlabel
-ax1.set_ylabel("CO(2-1) Brightness Temperature (K)") # ylabel
-cbar.set_label("Distance (kpc)")                 # colorbar label
+ax1.set_xlabel("CO(1-0) Brightness Temperature (K)") #
+ax1.set_ylabel("CO(2-1) Brightness Temperature (K)") #
+cbar.set_label("Distance (kpc)")                 #
 
 plt.savefig("figure_co21_vs_co10.png",dpi=300)
--- mycasa_scripts_active/scripts_jwu01_ngc6240/myim03_pixel_extraction.py
+++ mycasa_scripts_active/scripts_jwu01_ngc6240/myim03_pixel_extraction.py
@@ -1,53 +1,56 @@
 import math
 import numpy as np
 
-imageco10 = "n6240_co10_moment8_Kelvin.image"
-imageco21 = "n6240_co21_moment8_Kelvin.image"
+dir_proj = "/Users/saito/data/myproj_active/proj_jwu01_ngc6240/"
+imageco10 = dir_proj + "image_co10/n6240_co10_mom8_Kelvin.image"
+imageco21 = dir_proj + "image_co21/n6240_co21_mom8_Kelvin.image"
 
-### 中心からの距離を計算する関数を作る。
+###
 def distance(x, y):
-    ra_cnt = 253.245   # degree unit, ngc6240の中心のR.A.
-    dec_cnt = 2.40111  # degree unit, ngc6240の中心のDeclination
-    scale = 0.48       # 1 arcsec = 0.48 kpc
+    ra_cnt = 253.245
+    dec_cnt = 2.40111
+    scale = 0.48
     x_new = x - ra_cnt
     y_new = y - dec_cnt
     r = np.sqrt(x_new**2 + y_new**2) * 3600 * scale
     return r
 
-### imageの縦横のピクセル数を知る。
+###
 length_x = imhead(imageco10,mode="list")["shape"][0] - 1
 length_y = imhead(imageco10,mode="list")["shape"][1] - 1
 
-### ピクセルの値を取ってくる領域をボックスで指定する。今回は全ピクセルです。
+###
 box = "0,0,"+str(length_x)+","+str(length_y)
 
-### CASA task imvalでピクセル毎の値を取ってくる。
+###
 valueco10_tmp = imval(imageco10,box=box)["data"]
 valueco10 = valueco10_tmp.flatten()
 
-### CO(2-1)も同様。
+###
 valueco21_tmp = imval(imageco21,box=box)["data"]
 valueco21 = valueco21_tmp.flatten()
 
-### CO(1-0) and/or CO(2-1)の値が0のpixelを省く。
+###
 cut_zero = np.where((valueco10 > 0) & (valueco21 > 0))
 valueco10_cut = valueco10[cut_zero]
 valueco21_cut = valueco21[cut_zero]
 
-### 座標を中心からの距離に変換する。
-# まずR.A.を読み込む。
+###
+#
 valuera_tmp = imval(imageco10,box=box)["coords"][:,:,0] * 180 / np.pi
 valuera_tmp2 = valuera_tmp.flatten()
 valuera_cut = valuera_tmp2[cut_zero]
 
-# そしてDeclinationを読み込む。
+#
 valuedec_tmp = imval(imageco10,box=box)["coords"][:,:,1] * 180 / np.pi
 valuedec_tmp2 = valuedec_tmp.flatten()
 valuedec_cut = valuedec_tmp2[cut_zero]
 
-# 距離を計算する関数で距離を各ピクセルの中心からの距離を算出する。
+#
 valuedist_cut = distance(valuera_cut, valuedec_cut)
 
-# txtに保存する。
+#
 datatable = np.c_[valuedist_cut,valueco10_cut,valueco21_cut]
 np.savetxt("n6240_mom8_data.txt",datatable)
+
+os.system("rm -rf *.last")
