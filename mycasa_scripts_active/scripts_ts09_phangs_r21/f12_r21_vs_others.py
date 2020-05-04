156f518c5097ed446e64ba33bc385607b166cd56
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-05-04 18:15:00 +0900

    Commit at 2020-05-04 18:15:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f12_r21_vs_others.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f12_r21_vs_others.py
@@ -13,7 +13,8 @@ plt.ioff()
 #####################
 ### parameters
 #####################
-dir_data = "/Users/saito/data/mycasa_scripts_active/scripts_ts09_phangs_r21/"
+dir_data = "/Users/saito/data/mycasa_scripts_active/scripts_ts09_phangs_r21/eps/"
+galaxy = ["ngc0628","ngc3627","ngc4321"]
 
 
 #####################
@@ -25,9 +26,14 @@ dir_data = "/Users/saito/data/mycasa_scripts_active/scripts_ts09_phangs_r21/"
 #####################
 ### Main Procedure
 #####################
+txtfiles = glob.glob(dir_data + "*_parameter_600pc.txt")
+
+
+
 plt.figure(figsize=(10,0))
 plt.rcParams["font.size"] = 14
-gs = gridspec.GridSpec(nrows=9, ncols=16)
-ax1 = plt.subplot(gs[1:7,0:4])
-ax2 = plt.subplot(gs[1:7,4:8])
-ax3 = plt.subplot(gs[1:7,8:12])
+gs = gridspec.GridSpec(nrows=5, ncols=15)
+ax1 = plt.subplot(gs[0:5,0:5])
+ax2 = plt.subplot(gs[0:5,5:10])
+ax3 = plt.subplot(gs[0:5,10:15])
+
