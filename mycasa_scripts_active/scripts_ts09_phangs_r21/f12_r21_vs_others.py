6ad7a2755e65cf0d4a1f1f61eee84907ae32a012
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-05-04 18:10:00 +0900

    Commit at 2020-05-04 18:10:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f12_r21_vs_others.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f12_r21_vs_others.py
@@ -25,3 +25,9 @@ dir_data = "/Users/saito/data/mycasa_scripts_active/scripts_ts09_phangs_r21/"
 #####################
 ### Main Procedure
 #####################
+plt.figure(figsize=(10,0))
+plt.rcParams["font.size"] = 14
+gs = gridspec.GridSpec(nrows=9, ncols=16)
+ax1 = plt.subplot(gs[1:7,0:4])
+ax2 = plt.subplot(gs[1:7,4:8])
+ax3 = plt.subplot(gs[1:7,8:12])
