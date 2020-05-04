990598e5c162788b813c10439a89652ead88b584
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-05-04 18:25:00 +0900

    Commit at 2020-05-04 18:25:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f12_r21_vs_others.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f12_r21_vs_others.py
@@ -20,7 +20,43 @@ gals = ["ngc0628","ngc3627","ngc4321"]
 #####################
 ### functions
 #####################
+def get_data(txtdata,col):
+    """
+    """
+    data = np.loadtxt(txtdata)
+    #
+    dist = data[:,0]
+    r21 = data[:,1]
+    r21err = data[:,2]
+    co21 = data[:,3]
+    co21snr = data[:,4]
+    co10 = data[:,5]
+    co10snr = data[:,6]
+    tpeak = data[:,7]
+    disp = data[:,8]
+    w1 = data[:,9]
+    w2 = data[:,10]
+    w3 = data[:,11]
+    mask = data[:,12]
+    #
+    data4use = np.log10(data[:,col])
+    data4use[np.isinf(data4use)] = 0
+    data4use[np.isnan(data4use)] = 0
+    #xlim = [0,data4use.max()*1.1]
+    #
+    cut_co21 = (co21 != 0)
+    cut_low = (mask==-1)
+    cut_mid = (mask==0)
+    cut_high = (mask==1)
+    cut_low = np.where((cut_co21) & (cut_low))
+    cut_mid = np.where((cut_co21) & (cut_mid))
+    cut_high = np.where((cut_co21) & (cut_high))
+    #
+    data_low  = data4use[cut_low]
+    data_mid  = data4use[cut_mid]
+    data_high = data4use[cut_high]
 
+    return data_low, data_mid, data_high
 
 
 #####################
@@ -30,6 +66,11 @@ gals = ["ngc0628","ngc3627","ngc4321"]
 data_0628 = np.loadtxt(dir_data + "ngc0628_parameter_600pc.txt")
 data_3627 = np.loadtxt(dir_data + "ngc3627_parameter_600pc.txt")
 data_4321 = np.loadtxt(dir_data + "ngc4321_parameter_600pc.txt")
+data_gals = [data_0628, data_3627, data_4321]
+data_all = np.r_[data_0628, data_3627, data_4321]
+
+for i in range(len(gals)):
+	r21 = get_data(data_gal,1)
 
 
 plt.figure(figsize=(10,0))
