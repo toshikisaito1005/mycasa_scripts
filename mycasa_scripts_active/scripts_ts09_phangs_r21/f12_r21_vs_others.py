b219b9d7f7c7444665a5afb04803758641fa3732
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-05-04 18:05:01 +0900

    Commit at 2020-05-04 18:05:01

---
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f12_r21_vs_others.py
@@ -0,0 +1,27 @@
+import os
+import re
+import sys
+import glob
+import scipy
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.ticker as ticker
+plt.ioff()
+
+
+#####################
+### parameters
+#####################
+dir_data = "/Users/saito/data/mycasa_scripts_active/scripts_ts09_phangs_r21/"
+
+
+#####################
+### functions
+#####################
+
+
+
+#####################
+### Main Procedure
+#####################
