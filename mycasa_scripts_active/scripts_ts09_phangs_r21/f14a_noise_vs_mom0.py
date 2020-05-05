867a54c6acf6957ec358b8c04723861b85cbcb09
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-05-05 22:15:00 +0900

    Commit at 2020-05-05 22:15:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f14a_noise_vs_mom0.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f14a_noise_vs_mom0.py
@@ -36,6 +36,42 @@ def Jy2Kelvin(
 
 	return data
 
+def getdata(
+	co10_mom0,
+	co10_noise,
+	co21_mom0,
+	co21_noise,
+	freqco10,
+	freqco21,
+	):
+	"""
+	"""
+	# get beam
+	beamstr = co10_mom0.split("/")[-1].replace(".moment0","").split("_")[-1].replace("p",".")
+	beamfloat = float(beamstr)
+	#
+	# get data
+	data_co10_mom0  = r21.import_data(co10_mom0, mode="data")
+	data_co10_noise = r21.import_data(co10_noise, mode="data")
+	data_co21_mom0  = r21.import_data(co21_mom0, mode="data")
+	data_co21_noise = r21.import_data(co21_noise, mode="data")
+	#
+	# select data
+	cut_all = np.where((data_co10_mom0>0) & (data_co10_noise>0) & (data_co21_mom0>0) & (data_co21_noise>0))
+	#
+	data_co10_mom0  = data_co10_mom0[cut_all]
+	data_co10_noise = data_co10_noise[cut_all]
+	data_co21_mom0  = data_co21_mom0[cut_all]
+	data_co21_noise = data_co21_noise[cut_all]
+	#
+	# Jy-to-Kelvin
+	log_co10_mom0_k  = np.log10(Jy2Kelvin(data_co10_mom0, beamfloat, freqco10))
+	log_co10_noise_k = np.log10(Jy2Kelvin(data_co10_noise, beamfloat, freqco10))
+	log_co21_mom0_k  = np.log10(Jy2Kelvin(data_co21_mom0, beamfloat, freqco21))
+	log_co21_noise_k = np.log10(Jy2Kelvin(data_co21_noise, beamfloat, freqco21))
+
+	return log_co10_mom0_k, log_co10_noise_k, log_co21_mom0_k, log_co21_noise_k
+
 
 #####################
 ### Main Procedure
@@ -45,39 +81,26 @@ co10_noise = dir_proj + "ngc0628_co10/co10_04p0.moment0.noise"
 co21_mom0  = dir_proj + "ngc0628_co21/co21_04p0.moment0"
 co21_noise = dir_proj + "ngc0628_co21/co21_04p0.moment0.noise"
 
-# get beam
-beamstr = co10_mom0.split("/")[-1].replace(".moment0","").split("_")[-1].replace("p",".")
-beamfloat = float(beamstr)
-#
-# get data
-data_co10_mom0  = r21.import_data(co10_mom0, mode="data")
-data_co10_noise = r21.import_data(co10_noise, mode="data")
-data_co21_mom0  = r21.import_data(co21_mom0, mode="data")
-data_co21_noise = r21.import_data(co21_noise, mode="data")
-#
-# select data
-cut_all = np.where((data_co10_mom0>0) & (data_co10_noise>0) & (data_co21_mom0>0) & (data_co21_noise>0))
-#
-data_co10_mom0  = data_co10_mom0[cut_all]
-data_co10_noise = data_co10_noise[cut_all]
-data_co21_mom0  = data_co21_mom0[cut_all]
-data_co21_noise = data_co21_noise[cut_all]
-#
-# Jy-to-Kelvin
-log_co10_mom0_k  = np.log10(Jy2Kelvin(data_co10_mom0, beamfloat, freqco10))
-log_co10_noise_k = np.log10(Jy2Kelvin(data_co10_noise, beamfloat, freqco10))
-log_co21_mom0_k  = np.log10(Jy2Kelvin(data_co21_mom0, beamfloat, freqco21))
-log_co21_noise_k = np.log10(Jy2Kelvin(data_co21_noise, beamfloat, freqco21))
+log_co10_mom0_k, log_co10_noise_k, log_co21_mom0_k, log_co21_noise_k = \
+	getdata(co10_mom0, co10_noise, co21_mom0, co21_noise, freqco10, freqco21)
 
 # plot
-figure = plt.figure(figsize=(10,4))
-gs = gridspec.GridSpec(nrows=8, ncols=18)
+figure = plt.figure(figsize=(10,8))
+gs = gridspec.GridSpec(nrows=8, ncols=8)
 plt.subplots_adjust(bottom=0.15, left=0.10, right=0.98, top=0.88)
-ax1 = plt.subplot(gs[0:8,0:8])
-ax2 = plt.subplot(gs[0:8,10:18])
+ax1 = plt.subplot(gs[0:4,0:8])
+ax2 = plt.subplot(gs[4:8,0:8])
 ax1.grid(axis="both")
 ax2.grid(axis="both")
 plt.rcParams["font.size"] = 16
 
+# ax1
+ax1.plot(log_co10_mom0_k,log_co10_noise_k,"o")
+
+# ax2
+ax2.plot(log_co21_mom0_k,log_co21_noise_k,"o")
 
 
+plt.savefig(dir_proj + "eps/noise_vs_mom0.png",dpi=200)
+
+os.system("rm -rf *.last")
