b0bc1bb7746a209d6662da6a367b120af6b3336e
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-05-05 15:15:00 +0900

    Commit at 2020-05-05 15:15:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f06_histall.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f06_histall.py
@@ -108,19 +108,26 @@ plt.rcParams["font.size"] = 16
 ylim = [0.0001, y_all.max()*1.2]
 ax1.step(x_all, y_all, "black", lw=1, alpha=1.0, where="mid")
 ax1.bar(x_all, y_all, lw=0, color="black", alpha=0.2, width=x_all[1]-x_all[0], align="center")
-ax1.plot(p50_all, ylim[1]/1.2*1.1, "o", markeredgewidth=0, c="black", markersize=7, zorder=1)
-ax1.plot([p16_all, p84_all], [ylim[1]/1.2*1.1, ylim[1]/1.2*1.1], "-", c="black", lw=2, zorder=0)
+ax1.plot(p50_all, ylim[1]/1.2*1.05, "o", markeredgewidth=0, c="black", markersize=7, zorder=1)
+ax1.plot([p16_all, p84_all], [ylim[1]/1.2*1.05, ylim[1]/1.2*1.05], "-", c="black", lw=2, zorder=0)
 #
-ax1.text(p50_all, ylim[1]/1.2*1.1, str(np.round(p50_all,2)), fontsize=12)
+ax1.text(p16_all, ylim[1]/1.2*1.1, str(np.round(p16_all,2)), fontsize=12, ha="right")
+ax1.text(p50_all, ylim[1]/1.2*1.1, str(np.round(p50_all,2)), fontsize=12, ha="center")
+ax1.text(p84_all, ylim[1]/1.2*1.1, str(np.round(p84_all,2))+"0", fontsize=12, ha="left")
 #
+ax1.set_xlabel("$R_{21}$")
 ax1.set_ylim(ylim)
 
 # ax2
 ylim = [0.0001, y_norm.max()*1.2]
 ax2.step(x_norm, y_norm, "black", lw=1, alpha=1.0, where="mid")
 ax2.bar(x_norm, y_norm, lw=0, color="black", alpha=0.2, width=x_norm[1]-x_norm[0], align="center")
-ax2.plot(p50_norm, ylim[1]/1.2*1.1, "o", markeredgewidth=0, c="black", markersize=7, zorder=1)
-ax2.plot([p16_norm, p84_norm], [ylim[1]/1.2*1.1, ylim[1]/1.2*1.1], "-", c="black", lw=2, zorder=0)
+ax2.plot(p50_norm, ylim[1]/1.2*1.05, "o", markeredgewidth=0, c="black", markersize=7, zorder=1)
+ax2.plot([p16_norm, p84_norm], [ylim[1]/1.2*1.05, ylim[1]/1.2*1.05], "-", c="black", lw=2, zorder=0)
+#
+ax2.text(p16_norm, ylim[1]/1.2*1.1, str(np.round(p16_norm,2)), fontsize=12, ha="right")
+ax2.text(p50_norm, ylim[1]/1.2*1.1, str(np.round(p50_norm,2))+"0", fontsize=12, ha="center")
+ax2.text(p84_norm, ylim[1]/1.2*1.1, str(np.round(p84_norm,2)), fontsize=12, ha="left")
 #
 ax2.set_ylim(ylim)
 
