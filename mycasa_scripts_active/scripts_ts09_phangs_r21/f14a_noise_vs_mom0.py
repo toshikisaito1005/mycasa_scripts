507073483988cba4a5b6473ccab842baac0098be
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-05-05 22:00:00 +0900

    Commit at 2020-05-05 22:00:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f14a_noise_vs_mom0.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f14a_noise_vs_mom0.py
@@ -4,6 +4,9 @@ import matplotlib.gridspec as gridspec
 from scipy.optimize import curve_fit
 plt.ioff()
 
+#
+import scripts_phangs_r21 as r21
+
 
 #####################
 ### parameters
@@ -25,9 +28,28 @@ def function(x, a, b):
 #####################
 co10_mom0  = dir_proj + "ngc0628_co10/co10_04p0.moment0"
 co10_noise = dir_proj + "ngc0628_co10/co10_04p0.moment0.noise"
+co21_mom0  = dir_proj + "ngc0628_co21/co21_04p0.moment0"
+co21_noise = dir_proj + "ngc0628_co21/co21_04p0.moment0.noise"
 
 # get beam
 beamstr = co10_mom0.split("/")[-1].replace(".moment0","").split("_")[-1].replace("p",".")
 beamfloat = float(beamstr)
-
+#
 # get data
+data_co10_mom0  = r21.import_data(co10_mom0, mode="data")
+data_co10_noise = r21.import_data(co10_noise, mode="data")
+data_co21_mom0  = r21.import_data(co21_mom0, mode="data")
+data_co21_noise = r21.import_data(co21_noise, mode="data")
+#
+# select data
+data_co10_mom0 = data_co10_mom0[data_co10_mom0>0]
+data_co10_noise = data_co10_noise[data_co10_mom0>0]
+data_co21_mom0 = data_co21_mom0[data_co10_mom0>0]
+data_co21_noise = data_co21_noise[data_co10_mom0>0]
+#
+data_co10_mom0 = data_co10_mom0[data_co10_noise>0]
+data_co10_noise = data_co10_noise[data_co10_noise>0]
+data_co21_mom0 = data_co21_mom0[data_co10_noise>0]
+data_co21_noise = data_co21_noise[data_co10_noise>0]
+#
+
