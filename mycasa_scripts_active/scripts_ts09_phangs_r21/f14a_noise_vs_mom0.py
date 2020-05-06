f435b83da6237e36218111132defd1f79c022122
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-05-06 19:55:00 +0900

    Commit at 2020-05-06 19:55:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f14a_noise_vs_mom0.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f14a_noise_vs_mom0.py
@@ -258,10 +258,10 @@ def add_noise(
 ### Main Procedure
 #####################
 ### get filenames
-co10_mom0  = dir_proj + "ngc0628_co10/co10_04p0.moment0"
-co10_noise = dir_proj + "ngc0628_co10/co10_04p0.moment0.noise"
-co21_mom0  = dir_proj + "ngc0628_co21/co21_04p0.moment0"
-co21_noise = dir_proj + "ngc0628_co21/co21_04p0.moment0.noise"
+co10_mom0  = dir_proj + "ngc4321_co10/co10_04p0.moment0"
+co10_noise = dir_proj + "ngc4321_co10/co10_04p0.moment0.noise"
+co21_mom0  = dir_proj + "ngc4321_co21/co21_04p0.moment0"
+co21_noise = dir_proj + "ngc4321_co21/co21_04p0.moment0.noise"
 #
 ### plot noise vs. mom-0
 log_co10_mom0_k, log_co10_noise_k, log_co21_mom0_k, log_co21_noise_k = getdata(co10_mom0, co10_noise, co21_mom0, co21_noise, freqco10, freqco21)
