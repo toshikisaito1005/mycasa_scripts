dd66aa4407f4127ef435a06e9857ab00bcdcc059
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-05-06 18:35:01 +0900

    Commit at 2020-05-06 18:35:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f14a_noise_vs_mom0.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f14a_noise_vs_mom0.py
@@ -263,11 +263,8 @@ best_lognorm_co10 = best_lognorm_co10[best_lognorm_co21<log_co21_mom0_k.max()]
 best_lognorm_co10 = best_lognorm_co10[best_lognorm_co21>log_co21_mom0_k.min()]
 #
 ## adding scatter
-best_lognorm_co10_w_scatter = add_scatter(best_lognorm_co10, 0.5)
-best_lognorm_co21_w_scatter = add_scatter(best_lognorm_co21, 0.1)
-cut_all = np.where((best_lognorm_co10_w_scatter>xbins_co10.min()) & ((best_lognorm_co10_w_scatter<xbins_co10.max())) & (best_lognorm_co21_w_scatter>xbins_co21.min()) & ((best_lognorm_co21_w_scatter<xbins_co21.max())))
-best_lognorm_co10_w_scatter = best_lognorm_co10_w_scatter[cut_all]
-best_lognorm_co21_w_scatter = best_lognorm_co21_w_scatter[cut_all]
+best_lognorm_co10_w_scatter = add_scatter(best_lognorm_co10, 0.01)
+best_lognorm_co21_w_scatter = add_scatter(best_lognorm_co21, 0.01)
 #
 ## adding noise
 best_lognorm_co10_w_scatter_noise = add_noise(best_lognorm_co10_w_scatter, log_co10_noise_k, xbins_co10)
@@ -314,7 +311,7 @@ plt.rcParams["font.size"] = 16
 
 # ax1
 ax1.plot(best_lognorm_co10, best_lognorm_co21, "o", color="black", alpha=1.0, markersize=3, markeredgewidth=0, zorder=1e22)
-ax1.plot(best_lognorm_co10_w_scatter_noise, best_lognorm_co21_w_scatter_noise, "o", color="red", alpha=0.2, markersize=5, markeredgewidth=0, zorder=1e20, label="scatter and noise")
+#ax1.plot(best_lognorm_co10_w_scatter_noise, best_lognorm_co21_w_scatter_noise, "o", color="red", alpha=0.2, markersize=5, markeredgewidth=0, zorder=1e20, label="scatter and noise")
 ax1.plot(best_lognorm_co10_w_scatter, best_lognorm_co21_w_scatter, "o", color="blue", alpha=0.2, markersize=5, markeredgewidth=0, zorder=1e18, label="scatter")
 ax1.plot(log_co10_mom0_k, log_co21_mom0_k, "o", color="grey", alpha=0.2, markersize=10, markeredgewidth=0)
 #
@@ -322,6 +319,7 @@ ax1.set_xlim([-0.5,2.0])
 ax1.set_ylim([-0.5,2.0])
 
 #
+ax1.legend()
 plt.savefig(dir_proj + "eps/fig_obs_vs_model_mom0.png",dpi=200)
 #
 os.system("rm -rf *.last")
