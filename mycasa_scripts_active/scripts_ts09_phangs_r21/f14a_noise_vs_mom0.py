342c5d0f41154d52ca1d783a3b57d133b78b271f
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-05-08 08:35:00 +0900

    Commit at 2020-05-08 08:35:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f14a_noise_vs_mom0.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f14a_noise_vs_mom0.py
@@ -289,7 +289,7 @@ def get_best_co10_parameter(
 	#
 	range_popt1   = popt[1] + np.linspace(-0.2, 0.2, 11)
 	range_popt2   = popt[2] + np.linspace(-0.02, 0.02, 11)
-	range_scatter = np.linspace(0.001, 0.301, 11)
+	range_scatter = np.logspace(np.log10(0.001), np.log10(1), 21)
 	#
 	list_popt1 = []
 	list_popt2 = []
@@ -381,7 +381,7 @@ def get_best_co21_parameter(
 	#
 	range_slope = np.linspace(1.0, 1.3, 11)
 	range_intercept = np.linspace(-0.35, -0.15, 11)
-	range_scatter = np.linspace(0.01, 1.0, 21)
+	range_scatter = np.logspace(np.log10(0.001), np.log10(1), 21)
 	#
 	list_slope = []
 	list_intercept = []
@@ -390,7 +390,7 @@ def get_best_co21_parameter(
 	list_p = []
 	list_output = []
 	numiter = 0
-	numall = 11*11*11
+	numall = 11*11*21
 	done = glob.glob(dir_proj+"eps/best_co21_model_parameter.txt")
 	if not done:
 		for i, j, k in itertools.product(range_slope, range_intercept, range_scatter):
