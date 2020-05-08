4861efbfcb1d4450c917b45376afe97038f2888c
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-05-08 09:05:00 +0900

    Commit at 2020-05-08 09:05:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f14a_noise_vs_mom0.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f14a_noise_vs_mom0.py
@@ -287,7 +287,7 @@ def get_best_co10_parameter(
 	num_co10 = len(log_co10_mom0_k)
 	popt = fit_norm(log_co10_mom0_k, range_co10_input, nbins)
 	#
-	range_popt1   = popt[1] + np.linspace(-0.2, 0.2, 11)
+	range_popt1   = popt[1] + np.linspace(-0.1, 0.1, 11)
 	range_popt2   = popt[2] + np.linspace(-0.02, 0.02, 11)
 	range_scatter = np.logspace(np.log10(0.001), np.log10(1), 21)
 	#
@@ -380,7 +380,7 @@ def get_best_co21_parameter(
 	num_co21 = len(log_co21_mom0_k)
 	#
 	range_slope = np.linspace(1.0, 1.3, 11)
-	range_intercept = np.linspace(-0.35, -0.15, 11)
+	range_intercept = np.linspace(-0.30, -0.10, 11)
 	range_scatter = np.logspace(np.log10(0.001), np.log10(1), 21)
 	#
 	list_slope = []
