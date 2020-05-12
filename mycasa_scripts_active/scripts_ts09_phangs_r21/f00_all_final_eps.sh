1d88d5cbeedb3f4325708d8bb5cfb2a5b40d7f52
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-05-12 12:50:00 +0900

    Commit at 2020-05-12 12:50:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f00_all_final_eps.sh
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f00_all_final_eps.sh
@@ -138,4 +138,9 @@ rm -rf $dir_data"fig_r21_vs_w3.png" $dir_data"fig_r21_vs_w3b.png"
 
 ### figure 13
 convert +append -border 0x0 $dir_data"fig_noise_vs_mom0_ngc0628.png" $dir_data"fig_obs_vs_model_histo_ngc0628.png" $dir_data"fig13a.png"
-convert +append -border 0x0 $dir_data"" $dir_data"" $dir_data"fig13a.png"
+convert +append -border 0x0 $dir_data"fig_obs_vs_model_mom0_ngc0628.png" $dir_data"fig_obs_vs_model_r21_ngc0628.png" $dir_data"fig13b.png"
+
+convert -append -border 0x0 $dir_data"fig13a.png" $dir_data"fig13b.png" eps2:$dir_data"fig13.eps"
+
+rm -rf $dir_data"fig_noise_vs_mom0_ngc0628.png" $dir_data"fig_obs_vs_model_histo_ngc0628.png" $dir_data"fig13a.png"
+rm -rf $dir_data"fig_obs_vs_model_mom0_ngc0628.png" $dir_data"fig_obs_vs_model_r21_ngc0628.png" $dir_data"fig13b.png"
