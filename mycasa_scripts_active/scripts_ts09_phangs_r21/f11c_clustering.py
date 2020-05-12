75780a93e56c4ced4b27f192c41d9d9f5d935558
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-05-12 21:30:00 +0900

    Commit at 2020-05-12 21:30:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f11c_clustering.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f11c_clustering.py
@@ -1,3 +1,15 @@
 import numpy as np
 
 dir_data = "/Users/saito/data/myproj_active/proj_ts09_phangs_r21/"
+
+
+#####################
+### Main Procedure
+#####################
+txtdata = np.loadtxt(dir_data + "eps/ngc0628_parameter_matched_res.txt")
+
+# get data
+xydata = np.c_[txtdata[:,14], txtdata[:,15]] # parsec
+# calculate distance matrix
+all_diffs = np.expand_dims(xydata, axis=1) - np.expand_dims(xydata, axis=0)
+distance = np.sqrt(np.sum(all_diffs ** 2, axis=-1))
