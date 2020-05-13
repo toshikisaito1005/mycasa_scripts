d6a6c248902ea28303e6aa4267ef9eab9880c1b4
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-05-13 13:35:00 +0900

    Commit at 2020-05-13 13:35:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f15b_plot_best.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f15b_plot_best.py
@@ -465,8 +465,8 @@ plt.savefig(dir_proj + "eps/fig_obs_vs_model_r21_"+galname+".png",dpi=200)
 
 #
 #np.savetxt(dir_proj + "eps/ngc0628_model.txt", np.c_[log_co10_mom0_k_model, log_co21_mom0_k_model])
-np.savetxt(dir_proj + "eps/ngc0628_model_scatter.txt", np.c_[log_co10_mom0_k_model_scatter_cut, log_co21_mom0_k_model_scatter_cut])
-np.savetxt(dir_proj + "eps/ngc0628_model_scatter_noise.txt", np.c_[log_co10_mom0_k_model_scatter_noise_cut, log_co21_mom0_k_model_scatter_noise_cut])
+np.savetxt(dir_proj + "eps/ngc0628_model_scatter.txt", np.c_[log_co10_mom0_k_model_scatter, log_co21_mom0_k_model_scatter])
+np.savetxt(dir_proj + "eps/ngc0628_model_scatter_noise.txt", np.c_[log_co10_mom0_k_model_scatter_noise, log_co21_mom0_k_model_scatter_noise])
 
 #
 os.system("rm -rf *.last")
--- mycasa_scripts_active/scripts_ts09_phangs_r21/f16_model_scatter.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f16_model_scatter.py
@@ -18,22 +18,34 @@ bins = 8
 #####################
 ### main
 #####################
-data_ngc0628 = np.loadtxt(dir_proj + "eps/ngc0628_model_scatter.txt")
-co10 = data_ngc0628[:,0]
-co21 = data_ngc0628[:,1]
-
+###
+data_scatter_ngc0628 = np.loadtxt(dir_proj + "eps/ngc0628_model_scatter.txt")
+co10_scatter = data_scatter_ngc0628[:,0]
+co21_scatter = data_scatter_ngc0628[:,1]
 #
+data_scatter_noise_ngc0628 = np.loadtxt(dir_proj + "eps/ngc0628_model_scatter_noise.txt")
+co10_scatter_noise = data_scatter_noise_ngc0628[:,0]
+co21_scatter_noise = data_scatter_noise_ngc0628[:,1]
+
+###
 xwdith = co10.max() - co10.min()
 xlim = [co10.min(), co10.max()]
 
+###
+n_scatter, _ = np.histogram(co10_scatter, bins=bins, range=xlim)
+sy_scatter, _ = np.histogram(co10_scatter, bins=bins, weights=co21_scatter, range=xlim)
+sy2_scatter, _ = np.histogram(co10_scatter, bins=bins, weights=co21_scatter*co21_scatter, range=xlim)
 #
-n, _ = np.histogram(co10, bins=bins, range=xlim)
-sy, _ = np.histogram(co10, bins=bins, weights=co21, range=xlim)
-sy2, _ = np.histogram(co10, bins=bins, weights=co21*co21, range=xlim)
+n_scatter_noise, _ = np.histogram(co10_scatter_noise, bins=bins, range=xlim)
+sy_scatter_noise, _ = np.histogram(co10_scatter_noise, bins=bins, weights=co21_scatter_noise, range=xlim)
+sy2_scatter_noise, _ = np.histogram(co10_scatter_noise, bins=bins, weights=co21_scatter_noise*co21_scatter_noise, range=xlim)
 
+###
+mean_scatter = sy_scatter / n_scatter
+std_scatter = np.sqrt(sy2_scatter/n_scatter - mean_scatter*mean_scatter)
 #
-mean = sy / n
-std = np.sqrt(sy2/n - mean*mean)
+mean_scatter_noise = sy_scatter_noise / n_scatter_noise
+std_scatter_noise = np.sqrt(sy2_scatter_noise/n_scatter_noise - mean_scatter_noise*mean_scatter_noise)
 
 #
 plt.figure(figsize=(12,5))
@@ -44,8 +56,9 @@ gs = gridspec.GridSpec(nrows=5, ncols=15)
 ax = plt.subplot(gs[0:5,0:15])
 ax.grid(axis='both')
 #
-ax.plot((_[1:] + _[:-1])/2, std, "-", color=cm.brg(0/2.5), alpha=0.5, lw=3)
+ax.plot((_[1:] + _[:-1])/2, std_scatter, "-", color=cm.brg(0/2.5), alpha=0.5, lw=3)
+ax.plot((_[1:] + _[:-1])/2, std_scatter_noise, "--", color=cm.brg(0/2.5), alpha=0.5, lw=3)
 ax.set_xlim([0.2,1.8])
-ax.set_ylim([0,0.2])
+ax.set_ylim([0,0.4])
 #
 plt.savefig(dir_proj + "eps/model_scatter.png",dpi=200)
