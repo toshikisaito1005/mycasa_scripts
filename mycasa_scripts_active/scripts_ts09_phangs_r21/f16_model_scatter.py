6b20a1b1fcbce0165c95082e59d16249c6c8694f
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-05-13 13:25:00 +0900

    Commit at 2020-05-13 13:25:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f16_model_scatter.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f16_model_scatter.py
@@ -6,9 +6,6 @@ from scipy.optimize import curve_fit
 from scipy import stats
 plt.ioff()
 
-#
-import scripts_phangs_r21 as r21
-
 
 #####################
 ### parameters
@@ -20,4 +17,21 @@ dir_proj = "/Users/saito/data/myproj_active/proj_ts09_phangs_r21/"
 ### main
 #####################
 data_ngc0628 = np.loadtxt(dir_proj + "eps/ngc0628_model_scatter.txt")
+co10 = data_ngc0628[:,0]
+co21 = data_ngc0628[:,1]
 
+#
+xwdith = co10.max() - co10.min()
+xlim = [co10.min(), co10.max()]
+
+#
+n, _ = np.histogram(co10, bins=bins, range=xlim)
+sy, _ = np.histogram(co10, bins=bins, weights=co21, range=xlim)
+sy2, _ = np.histogram(co10, bins=bins, weights=co21*co21, range=xlim)
+
+#
+mean = sy / n
+std = np.sqrt(sy2/n - mean*mean)
+
+#
+ax.plot((_[1:] + _[:-1])/2, std, "-", color="")
