6bcb42e4955bb367f32ac63d088fea00882dc093
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-05-13 13:30:00 +0900

    Commit at 2020-05-13 13:30:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f16_model_scatter.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f16_model_scatter.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from scipy.optimize import curve_fit
 from scipy import stats
+import matplotlib.cm as cm
 plt.ioff()
 
 
@@ -11,6 +12,7 @@ plt.ioff()
 ### parameters
 #####################
 dir_proj = "/Users/saito/data/myproj_active/proj_ts09_phangs_r21/"
+bins = 8
 
 
 #####################
@@ -34,4 +36,16 @@ mean = sy / n
 std = np.sqrt(sy2/n - mean*mean)
 
 #
-ax.plot((_[1:] + _[:-1])/2, std, "-", color="")
+plt.figure(figsize=(12,5))
+plt.rcParams["font.size"] = 14
+plt.rcParams["legend.fontsize"] = 11
+plt.subplots_adjust(bottom=0.15, left=0.10, right=0.98, top=0.88)
+gs = gridspec.GridSpec(nrows=5, ncols=15)
+ax = plt.subplot(gs[0:5,0:15])
+ax.grid(axis='both')
+#
+ax.plot((_[1:] + _[:-1])/2, std, "-", color=cm.brg(0/2.5), alpha=0.5, lw=3)
+ax.set_xlim([0.2,1.8])
+ax.set_ylim([0,0.2])
+#
+plt.savefig(dir_proj + "eps/model_scatter.png",dpi=200)
