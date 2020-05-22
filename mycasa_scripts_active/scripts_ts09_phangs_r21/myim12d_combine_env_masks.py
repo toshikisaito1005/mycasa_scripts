116fe39a1a802906dff51dce8dc953a29de9eaf5
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-05-22 11:05:01 +0900

    Commit at 2020-05-22 11:05:01

--- mycasa_scripts_active/scripts_ts09_phangs_r21/myim12d_combine_env_masks.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/myim12d_combine_env_masks.py
@@ -17,8 +17,12 @@ gals = ["ngc0628","ngc3627","ngc4321"]
 ### Main Procedure
 #####################
 galname = gals[i]
-fitsimages = glob.glob(dir_project + galname + "_r21/env_*.fits")
-
+fits_arm = glob.glob(dir_project + galname + "_r21/env_*arm*.fits")
+fits_bulge = glob.glob(dir_project + galname + "_r21/env_*bulge*.fits")
+fits_bar = glob.glob(dir_project + galname + "_r21/env_*bar_*.fits")
+fits_barend = glob.glob(dir_project + galname + "_r21/env_*barend_*.fits")
+fitsimages = np.r_[fits_arm, fits_bulge, fits_bar, fits_barend]
+#
 
 
 os.system("rm -rf *.last")
