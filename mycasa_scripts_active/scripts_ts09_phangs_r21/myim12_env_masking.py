5783a41dd9ded358e1090296107c768d48562c96
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-05-22 10:40:00 +0900

    Commit at 2020-05-22 10:40:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/myim12_env_masking.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/myim12_env_masking.py
@@ -17,57 +17,48 @@ spiral_fits = ["NGC0628_mask_v5_sp_arms.image",    # >0
 mom0_fits = ["../../ngc0628_r21/r21_04p0.moment0",
 			 "../../ngc3627_r21/r21_08p0.moment0",
 			 "../../ngc4321_r21/r21_04p0.moment0"]
-output = ["../../ngc0628_r21/env_04p0.mask.fits",
-		  "../../ngc3627_r21/env_08p0.mask.fits",
-		  "../../ngc4321_r21/env_04p0.mask.fits"]
+output = ["../../ngc0628_r21/env_disk_04p0.mask.fits",
+		  "../../ngc3627_r21/env_disk_08p0.mask.fits",
+		  "../../ngc4321_r21/env_disk_04p0.mask.fits"]
 expr = ["iif(IM0>0,1,0)",
         "iif(IM0=5,1,0)",
         "iif(IM0>0,1,0)"]
 # output = "ngc1365_cprops_mask_1p38.fits"
-snr = 5.0 # peak signal-to-noise ratio threshold to identify clouds
-scales = [44/1.0,52/1.3,103/1.4] # parsec / arcsec
-image_ra_cnt = ["01:36:41.790",
-				"11:20:15.181",
-				"12:22:54.961"]
-image_decl_cnt = ["15.46.58.400",
-				  "12.59.28.137",
-				  "15.49.20.263"]
-convolution_scale = [np.sqrt((44/1.0*4.0)**2-120**2),
-					 np.sqrt((52/1.3*8.0)**2-120**2),
-					 np.sqrt((103/1.4*4.0)**2-120**2),
-					 ]
-
-i=0
 
 
 #####################
 ### Main Procedure
 #####################
-expr = expr[i]
-spiral_fits = dir_fits + spiral_fits[i]
-mom0_fits = dir_fits + mom0_fits[i]
-output = dir_fits + output[i]
-image_ra_cnt = image_ra_cnt[i]
-image_decl_cnt = image_decl_cnt[i]
-scale = scales[i]
-convolution_scale = convolution_scale[i]
+for i in range(len(spiral_fits)):
+  this_expr = expr[i]
+  this_spiral_fits = dir_fits + spiral_fits[i]
+  this_mom0_fits = dir_fits + mom0_fits[i]
+  this_output = dir_fits + output[i]
+  convolution_scale = convolution_scale[i]
+  #
+  os.system("rm -rf " + this_spiral_fits.replace(".image",".mask"))
+  immath(imagename = this_spiral_fits,
+    expr = this_expr,
+    outfile = this_spiral_fits.replace(".image",".mask"))
 
+  done = glob.glob(dir_product)
+  if not done:
+      os.mkdir(dir_product)
 
-#
-os.system("rm -rf " + spiral_fits.replace(".image",".mask"))
-immath(imagename = spiral_fits,
-  expr = expr,
-  outfile = spiral_fits.replace(".image",".mask"))
+  os.system("rm -rf " + this_output.replace(".fits",".image"))
+  imregrid(imagename = this_spiral_fits.replace(".image",".mask"),
+    template = this_mom0_fits,
+    output = this_output.replace(".fits",".image"))
 
-done = glob.glob(dir_product)
-if not done:
-    os.mkdir(dir_product)
+  immath(imagename = this_output.replace(".fits",".image"),
+    expr = "iif(IM0>0,1,0)",
+    outfile = this_output.replace(".fits",".image2"))
 
-imregrid(imagename = spiral_fits.replace(".image",".mask"),
-  template = mom0_fits,
-  output = output.replace(".fits",".image"))
+  os.system("rm -rf " + this_output)
+  exportfits(imagename = this_output.replace(".fits",".image2"),
+    fitsimage = this_output)
 
-exportfits(imgaename = output.replace(".fits",".image"),
-  fitsimage = output)
+  os.system("rm -rf " + this_output.replace(".fits",".image*"))
+
 
 os.system("rm -rf *.last")
