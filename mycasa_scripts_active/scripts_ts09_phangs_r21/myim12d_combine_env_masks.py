497703900451972e092bc7c741dac293915e2d58
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-05-22 11:20:00 +0900

    Commit at 2020-05-22 11:20:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/myim12d_combine_env_masks.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/myim12d_combine_env_masks.py
@@ -11,27 +11,36 @@ from astropy.coordinates import SkyCoord
 #####################
 dir_project = "/Users/saito/data/myproj_active/proj_ts09_phangs_r21/"
 gals = ["ngc0628","ngc3627","ngc4321"]
-exprs = ["IM0 + IM1*2",
+exprs = ["IM0 + IM1*2", "iif()"
          "IM0 + IM1*2 + IM2*3 + IM3*4",
          "IM0 + IM1*2"]
-outputs = ["env_all_04p0.mask.fits",
-           "env_all_08p0.mask.fits",
-           "env_all_04p0.mask.fits"]
+outputs = ["env_all_04p0.mask.image",
+           "env_all_08p0.mask.image",
+           "env_all_04p0.mask.image"]
 
 #####################
 ### Main Procedure
 #####################
-galname = gals[i]
-expr = exprs[i]
-#
-fits_arm = glob.glob(dir_project + galname + "_r21/env_*arm*.fits")
-fits_bulge = glob.glob(dir_project + galname + "_r21/env_*bulge*.fits")
-fits_bar = glob.glob(dir_project + galname + "_r21/env_*bar_*.fits")
-fits_barend = glob.glob(dir_project + galname + "_r21/env_*barend_*.fits")
-fitsimages = np.r_[fits_arm, fits_bulge, fits_bar, fits_barend]
-#
-immath(imagename = fitsimages,
-  )
+for i in range(len(gals)):
+  galname = gals[i]
+  expr = exprs[i]
+  output = outputs[i]
+  #
+  fits_arm = glob.glob(dir_project + galname + "_r21/env_*arm*.fits")
+  fits_bulge = glob.glob(dir_project + galname + "_r21/env_*bulge*.fits")
+  fits_bar = glob.glob(dir_project + galname + "_r21/env_*bar_*.fits")
+  fits_barend = glob.glob(dir_project + galname + "_r21/env_*barend_*.fits")
+  fitsimages = np.r_[fits_arm, fits_bulge, fits_bar, fits_barend]
+  #
+  os.system("rm -rf " + output)
+  immath(imagename = fitsimages,
+    expr = expr,
+    outfile = output)
+
+  os.system("rm -rf " + output.replace(".image",".fits"))
+  exportfits(imagename = output,
+    fitsimage = dir_project + galname + "_r21/" + output.replace(".image",".fits"))
+  os.system("rm -rf " + output)
 
 
 os.system("rm -rf *.last")
