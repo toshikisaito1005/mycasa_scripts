2b667d3956a4fc47f9d4ac4fc3cb1fd042e9db59
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-05-25 18:40:01 +0900

    Commit at 2020-05-25 18:40:01

--- mycasa_scripts_active/scripts_phangs06_ssc_leroy20/myim03_clean.py
+++ mycasa_scripts_active/scripts_phangs06_ssc_leroy20/myim03_clean.py
@@ -6,6 +6,7 @@ a=datetime.datetime.now()
 
 
 dir_proj = "/Users/saito/data/myproj_active/proj_phangs06_ssc/sim_phangs/"
+robust = 0.5
 
 
 ##############################
@@ -33,6 +34,76 @@ def get_info(dir_sim,mosaic_def2,robust):
 
     return galname, start, ra, dec, weighting, wt
 
+def dirty_map(
+  dir_sim,
+  galname,
+  wt,
+  width,
+  start,
+  ra,
+  dec,
+  weighting,
+  robust,
+  dir_mask,
+  imsize):
+  outputname = dir_sim + "/dirty_" + galname + "_7m_"+wt
+  os.system("rm -rf "+outputname+"*")
+  tclean(vis = dir_sim + "/sim_" + galname + ".aca.cycle5.noisy.ms",
+	imagename = outputname,
+	field = "",
+	specmode = "cube",
+	width = width,
+	start = start,
+	restfreq = "230.53800GHz",
+	outframe = "LSRK",
+	niter = 0,
+	threshold = "",
+	cyclefactor = 4,
+	interactive = False,
+	imsize = imsize,
+	cell = "1.0arcsec",
+	phasecenter = "J2000 "+ra+" "+dec,
+	weighting = weighting,
+	robust = robust,
+	gridder = "mosaic",
+	deconvolver = "multiscale",
+	scales = [0,2,5],
+	nchan = nchan,
+	cycleniter = 50,
+	usemask = "user",
+	restoringbeam = "common",
+	startmodel = "",
+	mask = "")
+
+    # masking dirty map
+    os.system("rm -rf " + dir_sim + "/_tmp_inverse.mask")
+    imregrid(imagename = dir_mask + galname + "_12m+7m+tp_co21_hybridmask.mask",
+    	template = outputname + ".image",
+    	output = dir_sim + "/_tmp_inverse.mask")
+
+    os.system("rm -rf " + dir_sim + "/inverse.mask")
+    immath(imagename = dir_sim + "/_tmp_inverse.mask",
+    	mode = "evalexpr",
+    	expr = "iif(IM0>=1,0,1)",
+    	outfile = dir_sim + "/inverse.mask")
+
+    os.system("rm -rf " + dir_sim + "/inverse.mask.fits")
+    exportfits(imagename = dir_sim + "/inverse.mask",
+               fitsimage = dir_sim + "/inverse.mask.fits")
+
+    os.system("rm -rf " + dir_sim + "/inverse.mask")
+    os.system("rm -rf inverse.mask")
+    importfits(fitsimage = dir_sim + "/inverse.mask.fits",
+               imagename = "inverse.mask",
+               defaultaxes=True,
+               defaultaxesvalues=["RA","Dec","Frequency","Stokes"])
+    os.system("rm -rf " + dir_sim + "/inverse.mask.fits")
+
+    rms = imstat(imagename = outputname + ".image",
+                 mask = "inverse.mask")["rms"][0]
+
+    return rms
+
 
 ##############################
 ### main
@@ -49,6 +120,14 @@ dir_mask = dir_proj + "v3p3_hybridmask/"
 
 #for i in range(len(dir_sim)):
 for i in [0]:
-    # define name
-    galname,start,ra,dec,weighting,wt = def_name(dir_sim[i],mosaic_def2,robust)
+	#
+	this_dir_sim = dir_sim[i]
+    # get info
+    galname, start, ra, dec, weighting, wt = \
+        def_name(this_dir_sim, mosaic_def2, robust)
+    title = galname + ", " + str(i+1) + "/" + str(len(dir_sim))
+    imsize = def_imsize(this_dir_sim, galname)
 
+    ### measure 7m-only rms
+    print("### dirty map of " + title)
+    rms = dirty_map(this_dir_sim,galname,wt,width,start,ra,dec,weighting,robust,dir_mask,imsize)
