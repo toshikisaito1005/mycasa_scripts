6d873b55f33bba9c8643366a5fdd89821a3ddf5a
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-05-25 11:35:00 +0900

    Commit at 2020-05-25 11:35:00

--- mycasa_scripts_active/scripts_phangs06_ssc_leroy20/myim01_old_image.py
+++ mycasa_scripts_active/scripts_phangs06_ssc_leroy20/myim01_old_image.py
@@ -9,6 +9,7 @@ plt.ioff()
 #####
 dir_data = "/Users/saito/data/myproj_active/proj_phangs06_ssc/data_old/"
 gals = ["ngc0628"]
+i=0
 
 #####
 def process_fits(image,txtdata,mode,index=0):
@@ -30,11 +31,9 @@ def process_fits(image,txtdata,mode,index=0):
 	np.savetxt(txtdata, value_masked_1d)
 
 def import_data(imagename,
-		mode,
-		txtname,
-		index=0):
-    """
-    """
+    mode,
+    txtname,
+    index=0):
     process_fits(imagename,txtname,mode,index=index)
     data = np.loadtxt(txtname)
 
@@ -43,40 +42,35 @@ def import_data(imagename,
 
 #####
 galname = gals[i]
-
+#
 imagenames = glob.glob(dir_data + galname + "/*.image")
-
-
-
-txt_ra = imagenames[0]+"_ra.txt"
+#
+txt_ra = imagenames[0] + "_ra.txt"
 os.system("rm -rf " + txt_ra)
-ra_tmp_ = import_data(imagename = imagenames[0],
-                      mode = "coords",
-		      txtname = txt_ra)
-ra_tmp1_ = ra_tmp_ * 180 / np.pi
-txt_dec = imagenames[0]+"_dec.txt"
+ra = import_data(imagename=imagenames[0], mode="coords", txtname=txt_ra)
+ra = ra * 180 / np.pi
+#
+txt_dec = imagenames[0] + "_dec.txt"
 os.system("rm -rf " + txt_dec)
-dec_tmp_ = import_data(imagename = imagenames[0],
-                       mode = "coords",
- 		       txtname = txt_dec,
-		       index = 1)
-dec_tmp1_ = dec_tmp_ * 180 / np.pi
+dec = import_data(imagename=imagenames[0], mode="coords", txtname=txt_dec, index=1)
+dec = dec * 180 / np.pi
+#
 
-for i in range(len(imagenames)):
-    txt_caf = imagenames[i] + ".txt"
-    data_tmp_ = import_data(imagename = imagenames[i],
-                            mode = "data",
-                            txtname = txt_caf)
-    ra_tmp1_ = ra_tmp1_[data_tmp_>0.0001]
-    dec_tmp1_ = dec_tmp1_[data_tmp_>0.0001]
-    data_tmp1_ = data_tmp_[data_tmp_>0.0001]
 
+
+for j in range(len(imagenames)):
+    txtfile = imagenames[i] + ".txt"
+    data = import_data(imagename=imagenames[j], mode="data", txtname=txtfile)
+    #
+    this_ra = ra[data>0.0001]
+    this_dec = dec[data>0.0001]
+    this_data = data[data>0.0001]
+    #
     plt.figure(figsize=(8,8))
-    plt.scatter(ra_tmp1_,dec_tmp1_,
-                color=data_tmp1_,marker=".",cmap="rainbow",lw=1,s=3,alpha=0.5)
+    plt.scatter(this_ra, this_dec, color=this_data, marker=".", cmap="rainbow", lw=1, s=5, alpha=0.5)
     plt.xlim([ra_tmp1_.max(),ra_tmp1_.min()])
     plt.ylim([dec_tmp1_.min(),dec_tmp1_.max()])
-    plt.savefig(imagenames[i].split("/")[-1]+".png",dpi=100)
+    plt.savefig(imagenames[j].split("/")[-1]+".png",dpi=100)
 
 
 os.system("rm -rf *.last")
