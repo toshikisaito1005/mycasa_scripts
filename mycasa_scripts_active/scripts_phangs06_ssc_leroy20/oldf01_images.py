8e0987498951f7ade97acd82579d4dbc8329c36f
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-05-25 12:25:00 +0900

    Commit at 2020-05-25 12:25:00

--- mycasa_scripts_active/scripts_phangs06_ssc_leroy20/oldf01_images.py
+++ mycasa_scripts_active/scripts_phangs06_ssc_leroy20/oldf01_images.py
@@ -26,24 +26,24 @@ ra_center = "01:36:41.8"
 dec_center = "15.47.0.0"
 xlim = [-150, 150]
 ylim = [150, -150]
+clim = [0, 3.93731]
 value = None
+color_contour = "black"
+color_beam = "white"
+colorlog = False
+colorbar = True
+colorscale = "rainbow"
+colorbar_label = "(Jy beam$^{-1}$ km s$^{-1}$)"
+contour = np.array([0.15, 0.35, 0.55, 0.75, 0.95])
+imagename_contour = galname + "/sim_ngc0628_skymodel.smooth.fits"
 
 
 ### model
 #
-imagename_contour = galname + "/sim_ngc0628_skymodel.smooth.fits"
 imagename_color = galname + "/sim_ngc0628_skymodel.smooth.fits"
-imagemax = imstat(dir_data + imagename_contour)["max"]
-#
-contour = np.array([0.15, 0.35, 0.55, 0.75, 0.95])
-title = "7m-only"
-colorscale = "rainbow"
-color_contour = "black"
-color_beam = "black"
-colorlog = False
-colorbar = True
-colorbar_label = "(Jy km s$^{-1}$)"
+title = "Convolved Model"
 output = "../eps/ngc0628_skymodel.png"
+#
 myim.fits2eps(dir_data = dir_data,
               imagename_color = imagename_color,
               imagename_contour = imagename_contour,
@@ -60,24 +60,16 @@ myim.fits2eps(dir_data = dir_data,
               value = value,
               contour = contour,
               xlim = xlim,
-              ylim = ylim)
+              ylim = ylim,
+              clim = clim)
 
 
 ### 7m-only
 #
-imagename_contour = galname + "/sim_ngc0628_skymodel.smooth.fits"
 imagename_color = galname + "/sim_ngc0628_7m_br.smooth.pbcor.clip.fits"
-imagemax = imstat(dir_data + imagename_contour)["max"]
-#
-contour = np.array([0.15, 0.35, 0.55, 0.75, 0.95])
-title = "7m-only"
-colorscale = "rainbow"
-color_contour = "black"
-color_beam = "black"
-colorlog = False
-colorbar = True
-colorbar_label = "(Jy km s$^{-1}$)"
+title = "Convolved 7m-only"
 output = "../eps/ngc0628_7m.png"
+#
 myim.fits2eps(dir_data = dir_data,
               imagename_color = imagename_color,
               imagename_contour = imagename_contour,
@@ -94,7 +86,34 @@ myim.fits2eps(dir_data = dir_data,
               value = value,
               contour = contour,
               xlim = xlim,
-              ylim = ylim)
+              ylim = ylim,
+              clim = clim)
+
+
+### 7m-only
+#
+imagename_color = galname + "/sim_ngc0628_7m_br.smooth.pbcor.clip.fits"
+title = "Convolved 7m-only"
+output = "../eps/ngc0628_7m.png"
+#
+myim.fits2eps(dir_data = dir_data,
+              imagename_color = imagename_color,
+              imagename_contour = imagename_contour,
+              ra_center = ra_center,
+              dec_center = dec_center,
+              title = title,
+              colorbar_label = colorbar_label,
+              output = output,
+              colorscale = colorscale,
+              colorlog = colorlog,
+              color_contour = color_contour,
+              color_beam = color_beam,
+              colorbar = colorbar,
+              value = value,
+              contour = contour,
+              xlim = xlim,
+              ylim = ylim,
+              clim = clim)
 
 
 os.system("rm -rf *.last")
