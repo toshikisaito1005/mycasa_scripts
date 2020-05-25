19ca4e20618d43cd47ba46a11b0f8169132981ad
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-05-25 18:30:00 +0900

    Commit at 2020-05-25 18:30:00

--- mycasa_scripts_active/scripts_phangs06_ssc_leroy20/myim03_clean.py
+++ mycasa_scripts_active/scripts_phangs06_ssc_leroy20/myim03_clean.py
@@ -5,3 +5,13 @@ import datetime
 
 a=datetime.datetime.now()
 
+
+##############################
+### main
+##############################
+# get systemic velocity definition
+mosaic_def = np.loadtxt("mosaic_definitions.txt",dtype="S20",usecols=(0,3))
+mosaic_def = np.c_[[s.split("_")[0] for s in mosaic_def[:,0]],mosaic_def[:,1]]
+
+mosaic_def2 = np.loadtxt("multipart_fields.txt",dtype="S20",usecols=(0,1,2))
+mosaic_def2 = np.r_[np.loadtxt("mosaic_definitions.txt",dtype="S20",usecols=(0,1,2)),mosaic_def2]
