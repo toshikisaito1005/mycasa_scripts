57ded925044bb02315961494d8b38a447b239c35
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-05-25 19:20:01 +0900

    Commit at 2020-05-25 19:20:01

--- mycasa_scripts_active/scripts_phangs06_ssc_leroy20/myim03_clean.py
+++ mycasa_scripts_active/scripts_phangs06_ssc_leroy20/myim03_clean.py
@@ -19,7 +19,7 @@ def get_info(
 	mosaic_def2,
 	robust,
 	):
-	galname = this_dir_sim.split("/")[-1]
+	galname = this_dir_sim.split("/")[-1].split("sim_")[-1]
 	start = "" #mosaic_def[:,1][np.where(mosaic_def[:,0]==galname)[0][0]] + "km/s"
 	ra = mosaic_def2[:,1][np.where(mosaic_def2[:,0]==galname)[0][0]]
 	dec = mosaic_def2[:,2][np.where(mosaic_def2[:,0]==galname)[0][0]]
@@ -39,6 +39,51 @@ def get_info(
 	    wt = "br"
 
 	return galname, start, ra, dec, weighting, wt
+
+def def_imsize(
+	this_dir_sim,
+	galname,
+	):
+	skymodel = glob.glob("../phangs_dr1/"+galname+"*.skymodel")[0]
+	size_ra = imhead(tpname,mode="list")["shape"][0]
+	size_dec = imhead(tpname,mode="list")["shape"][1]
+	size_pixel = max(size_ra,size_dec)
+	pixel_arcsec = abs(imhead(tpname,mode="list")["cdelt1"]) * 180/np.pi*3600
+	size_arcsec = size_pixel * pixel_arcsec
+	imsize  = int(np.round(size_arcsec,-1) * 1.4)
+	if imsize < 256:
+	    imsize = 256
+	else:
+	    if imsize < 288:
+	    imsize = 288
+	else:
+	    if imsize < 320:
+	        imsize = 320
+		    else:
+	        if imsize < 384:
+	            imsize = 384
+	        else:
+		    if imsize < 400:
+		        imsize = 400
+		    else:
+	   	                if imsize < 448:
+		            imsize = 448
+		        else:
+		            if imsize < 512:
+		                imsize = 512
+	                        else:
+		                if imsize < 576:
+			            imsize = 576
+			        else:
+			            if imsize < 648:
+			                imsize = 648
+			            else:
+				        if imsize < 800:
+				            imsize = 800
+				        else:
+				            imsize = 1024
+
+	return imsize
 
 def dirty_map(
 	vis,
@@ -117,7 +162,6 @@ mosaic_def = np.loadtxt("mosaic_definitions.txt",dtype="S20",usecols=(0,3))
 mosaic_def = np.c_[[s.split("_")[0] for s in mosaic_def[:,0]],mosaic_def[:,1]]
 mosaic_def2 = np.loadtxt("multipart_fields.txt",dtype="S20",usecols=(0,1,2))
 mosaic_def2 = np.r_[np.loadtxt("mosaic_definitions.txt",dtype="S20",usecols=(0,1,2)),mosaic_def2]
-
 #
 dir_sim = glob.glob(dir_proj + "*")
 dir_mask = dir_proj + "../v3p3_hybridmask/"
