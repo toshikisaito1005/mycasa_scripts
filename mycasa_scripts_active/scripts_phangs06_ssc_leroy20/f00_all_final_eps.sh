ccfe51c75742efd581c95c3226d77d0aad29c848
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-05-26 16:30:00 +0900

    Commit at 2020-05-26 16:30:00

--- mycasa_scripts_active/scripts_phangs06_ssc_leroy20/f00_all_final_eps.sh
+++ mycasa_scripts_active/scripts_phangs06_ssc_leroy20/f00_all_final_eps.sh
@@ -8,3 +8,6 @@ convert -append -border 0x0 $dir_data"fig_ssc_01a.png" $dir_data"fig_ssc_01b.png
 
 rm -rf $dir_data"ngc0628_skymodel.png" $dir_data"ngc0628_7m.png" $dir_data"fig_ssc_01a.png"
 rm -rf $dir_data"ngc0628_tp2vis.png" $dir_data"ngc0628_tpmodel.png" $dir_data"ngc0628_feather.png" $dir_data"fig_ssc_01b.png"
+
+### figure 0
+convert dir_data"fig_fidelity_vs_circ_diameter.png" eps2:$dir_data"ssc_fidelity_vs_size.eps"
