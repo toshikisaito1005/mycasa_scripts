b864a424c7c3984eeff70c38e5880fdf483e889a
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-06-05 11:10:00 +0200

    Commit at 2020-06-05 11:10:00

--- mycasa_scripts_active/scripts_phangs06_ssc_leroy20/myim01_makemodel.py
+++ mycasa_scripts_active/scripts_phangs06_ssc_leroy20/myim01_makemodel.py
@@ -2,10 +2,70 @@ import os
 import sys
 import glob
 import numpy as np
+from astropy.coordinates import SkyCoord
 
 
 dir_project = "/Users/saito/data/myproj_active/proj_phangs06_ssc/"
 tpbeam = "28.5arcsec"
+
+
+##############################
+### def
+##############################
+def biggersize(
+    imagename,
+    ):
+    # get native grid information
+    num_x_pix = imhead(imagename,mode="list")["shape"][0]
+    num_y_pix = imhead(imagename,mode="list")["shape"][1]
+    pix_radian = imhead(imagename,mode="list")["cdelt2"]
+    obsfreq = imhead(imagename,mode="list")["crval4"]/1e9
+    pix_arcsec = round(pix_radian * 3600 * 180 / np.pi, 3)
+
+    # create tempalte image
+    blc_ra_tmp=imstat(imagename)["blcf"].split(", ")[0]
+    blc_dec_tmp=imstat(imagename)["blcf"].split(", ")[1]
+    blc_ra = blc_ra_tmp.replace(":","h",1).replace(":","m",1)+"s"
+    blc_dec = blc_dec_tmp.replace(".","d",1).replace(".","m",1)+"s"
+    beamsize=round(imhead(imagename,"list")["beammajor"]["value"], 2)
+    pix_size=round(beamsize/4.53, 2)
+    size_x = int(21. * 300. / obsfreq / pix_size)
+    size_y = size_x
+    c = SkyCoord(blc_ra, blc_dec)
+    ra_dgr = str(c.ra.degree)
+    dec_dgr = str(c.dec.degree)
+    direction_ra = str(float(ra_dgr) - num_x_pix*pix_arcsec/3600./2.*5/4.)+"deg"
+    direction_dec = str(float(dec_dgr) + num_y_pix*pix_arcsec/3600./2.)+"deg"
+    direction="J2000 "+direction_ra+" "+direction_dec
+    cl.done()
+    cl.addcomponent(dir=direction,
+                    flux=1.0,
+                    fluxunit="Jy",
+                    freq="230.0GHz",
+                    shape="Gaussian",
+                    majoraxis="0.1arcmin",
+                    minoraxis="0.05arcmin",
+                    positionangle="45.0deg")
+
+    ia.fromshape("template.im",[size_x,size_y,1,1],overwrite=True)
+    cs=ia.coordsys()
+    cs.setunits(["rad","rad","","Hz"])
+    cell_rad=qa.convert(qa.quantity(str(pix_size)+"arcsec"),"rad")["value"]
+    cs.setincrement([-cell_rad,cell_rad],"direction")
+    cs.setreferencevalue([qa.convert(direction_ra,"rad")["value"],
+                          qa.convert(direction_dec,"rad")["value"]],
+                         type="direction")
+    cs.setreferencevalue(str(obsfreq)+"GHz","spectral")
+    cs.setincrement("1GHz","spectral")
+    ia.setcoordsys(cs.torecord())
+    ia.setbrightnessunit("Jy/pixel")
+    ia.modify(cl.torecord(),subtract=False)
+    exportfits(imagename="template.im",
+               fitsimage="template.fits",
+               overwrite=True)
+
+    importfits(fitsimage="template.fits",
+               imagename="template.image")
 
 
 ##############################
@@ -72,7 +132,9 @@ for i in range(len(imagenames)):
     immath(imagename=outfile1, expr="IM0/"+str(beamarea), outfile=outfile2)
     #
     imhead(imagename=outfile2, mode="put", hdkey="bunit", hdvalue="Jy/pixel")
-    imhead(imagename=outfile1,mode="put", hdkey="bunit", hdvalue="Jy/beam")
+    imhead(imagename=outfile1, mode="put", hdkey="bunit", hdvalue="Jy/beam")
+    # make imsize of outfile1 bigger
+
     #
     outfile3 = this_image.replace(".image",".jypb.smooth_tmp_")
     os.system("rm -rf " + outfile3)
