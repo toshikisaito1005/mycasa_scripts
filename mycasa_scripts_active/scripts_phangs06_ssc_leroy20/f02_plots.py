60d837d02f55c0259fd266d20c783871e3ce07f4
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-06-09 17:30:00 +0200

    Commit at 2020-06-09 17:30:00

--- mycasa_scripts_active/scripts_phangs06_ssc_leroy20/f02_plots.py
+++ mycasa_scripts_active/scripts_phangs06_ssc_leroy20/f02_plots.py
@@ -27,6 +27,8 @@ def plotter(
        txtdata,
        output,
        ylim,
+       ylabel,
+       npround=2,
        ):
        data = np.loadtxt(txtdata)
        #
@@ -34,10 +36,10 @@ def plotter(
        plt.rcParams["font.size"] = 16
        plt.subplots_adjust(bottom=0.15, left=0.07, right=0.95, top=0.95)
        #
-       median_7m, disp_7m = np.round(np.median(data[:,1]),1), np.round(np.std(data[:,1]),1)
-       median_feather, disp_feather = np.round(np.median(data[:,2]),1), np.round(np.std(data[:,2]),1)
-       median_tp2vis, disp_tp2vis = np.round(np.median(data[:,3]),1), np.round(np.std(data[:,3]),1)
-       median_tpmodel, disp_tpmodel = np.round(np.median(data[:,4]),1), np.round(np.std(data[:,4]),1)
+       median_7m, disp_7m = np.round(np.median(data[:,1]),npround), np.round(np.std(data[:,1]),npround)
+       median_feather, disp_feather = np.round(np.median(data[:,2]),npround), np.round(np.std(data[:,2]),npround)
+       median_tp2vis, disp_tp2vis = np.round(np.median(data[:,3]),npround), np.round(np.std(data[:,3]),npround)
+       median_tpmodel, disp_tpmodel = np.round(np.median(data[:,4]),npround), np.round(np.std(data[:,4]),npround)
        #
        plt.plot([0,300],[median_7m,median_7m],"--",c=cm.gnuplot(0/3.5),lw=2)
        plt.plot([0,300],[median_feather,median_feather],"--",c=cm.gnuplot(1/3.5),lw=2)
@@ -49,11 +51,13 @@ def plotter(
        label_tp2vis = "tp2vis (" + str(median_tp2vis) + " $\pm$ " + str(disp_tp2vis) + ")"
        label_tpmodel = "tpmodel (" + str(median_tpmodel) + " $\pm$ " + str(disp_tpmodel) + ")"
        #
-       plt.scatter(data[:,0]*2, data[:,1], color=cm.gnuplot(0/3.5), s=50, alpha=0.7, lw=1, label=label_7m)
-       plt.scatter(data[:,0]*2, data[:,2], color=cm.gnuplot(1/3.5), s=50, alpha=0.7, lw=1, label=label_feather)
-       plt.scatter(data[:,0]*2, data[:,3], color=cm.gnuplot(2/3.5), s=50, alpha=0.7, lw=1, label=label_tp2vis)
-       plt.scatter(data[:,0]*2, data[:,4], color=cm.gnuplot(3/3.5), s=50, alpha=0.7, lw=1, label=label_tpmodel)
+       plt.scatter(data[:,0]*2, data[:,1], color=cm.gnuplot(0/3.5), s=40, alpha=0.7, lw=1, label=label_7m)
+       plt.scatter(data[:,0]*2, data[:,2], color=cm.gnuplot(1/3.5), s=40, alpha=0.7, lw=1, label=label_feather)
+       plt.scatter(data[:,0]*2, data[:,3], color=cm.gnuplot(2/3.5), s=40, alpha=0.7, lw=1, label=label_tp2vis)
+       plt.scatter(data[:,0]*2, data[:,4], color=cm.gnuplot(3/3.5), s=40, alpha=0.7, lw=1, label=label_tpmodel)
        #
+       plt.xlabel("CO Size Diameter (arcsec)")
+       plt.ylabel(ylabel)
        plt.xlim([0,300])
        plt.ylim(ylim)
        plt.legend(ncol=2)
@@ -63,5 +67,6 @@ def plotter(
 #####################
 ### Main Procedure
 #####################
-plotter("list_median.txt", dir_data+"fig_fidelity_vs_circ_diameter.png", [0,55])
+plotter("list_median.txt", dir_data+"ssc_fidelity_vs_size.png", [0,55], "Fidelity Median", 1)
+plotter("list_diff_total.txt", dir_data+"ssc_diff_total_vs_size.png", [-0.3,1.7], "Total Flux Difference")
 
