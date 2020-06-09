c1c7e92b2d7b149196921752e9e41bb8592a7b96
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-06-09 15:10:00 +0200

    Commit at 2020-06-09 15:10:00

--- mycasa_scripts_active/scripts_phangs06_ssc_leroy20/f02_plots.py
+++ mycasa_scripts_active/scripts_phangs06_ssc_leroy20/f02_plots.py
@@ -24,6 +24,12 @@ for i in range(len(dir_gals)):
        fidelity_tpmodel = glob.glob(dir_gals[i] + "*tpmodel*.fidelity")[0]
        #
        image_7m = glob.glob(dir_gals[i] + "*7m*.fidelity")[0]
-       fidelity_feather = glob.glob(dir_gals[i] + "*feather*.fidelity")[0]
-       fidelity_tp2vis = glob.glob(dir_gals[i] + "*tp2vis*.fidelity")[0]
-       fidelity_tpmodel = glob.glob(dir_gals[i] + "*tpmodel*.fidelity")[0]
+       image_feather = glob.glob(dir_gals[i] + "*feather*.fidelity")[0]
+       image_tp2vis = glob.glob(dir_gals[i] + "*tp2vis*.fidelity")[0]
+       image_tpmodel = glob.glob(dir_gals[i] + "*tpmodel*.fidelity")[0]
+       image_model = glob.glob(dir_gals[i] + "*skymodel_regrid.smooth")[0]
+       #
+       shape = imhead(fidelity_7m,mode="list")["shape"]
+       box = "0,0,"+str(shape[0]-1)+","+str(shape[1]-1)
+       #
+       fidelity_7m = imval(fidelity_7m,box=box)
