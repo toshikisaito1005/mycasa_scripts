d31a955de428b83537d44aa52e594cf7045c0d04
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-06-09 14:45:00 +0200

    Commit at 2020-06-09 14:45:00

---
+++ mycasa_scripts_active/scripts_phangs06_ssc_leroy20/myim05_fidelity.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+plt.ioff()
+
+
+#####
+dir_data = "/Users/saito/data/myproj_active/proj_phangs06_ssc/sim_phangs/"
+dir_gals = glob.glob(dir_data + "sim_*/")
+dir_gals.sort()
+
+
+imagenames = glob.glob(dir_gals[i] + "*.smooth")
