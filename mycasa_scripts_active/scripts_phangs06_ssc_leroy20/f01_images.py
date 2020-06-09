71b11e331936a99098061c56cd0f2f7b92e4d048
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-06-09 14:30:00 +0200

    Commit at 2020-06-09 14:30:00

--- mycasa_scripts_active/scripts_phangs06_ssc_leroy20/f01_images.py
+++ mycasa_scripts_active/scripts_phangs06_ssc_leroy20/f01_images.py
@@ -45,12 +45,12 @@ colorlog = False
 colorscale = "rainbow"
 colorbar_label = "(Jy beam$^{-1}$ km s$^{-1}$)"
 contour = np.array([0.15, 0.35, 0.55, 0.75, 0.95])
-imagename_contour = "sim_ngc0628_skymodel.smooth.fits"
+imagename_contour = "sim_ngc0628_skymodel_regrid.smooth.fits"
 
 
 ### model
 #
-imagename_color = "sim_ngc0628_skymodel.smooth.fits"
+imagename_color = "sim_ngc0628_skymodel_regrid.smooth.fits"
 title = "Convolved Model"
 output = "../../eps/ngc0628_skymodel.png"
 #
@@ -102,9 +102,9 @@ myim.fits2eps(dir_data = dir_data,
 
 ### CAF
 #
-imagename_color = galname + "/sim_ngc0628_caf_br.smooth.pbcor.clip.fits"
+imagename_color = "sim_ngc0628_feather_br.smooth.fits"
 title = "Convolved Feather"
-output = "../eps/ngc0628_feather.png"
+output = "../../eps/ngc0628_feather.png"
 #
 myim.fits2eps(dir_data = dir_data,
               imagename_color = imagename_color,
@@ -124,36 +124,13 @@ myim.fits2eps(dir_data = dir_data,
               xlim = xlim,
               ylim = ylim,
               clim = clim)
-#
-imagename_color = galname + "/sim_ngc0628_caf_br.smooth.pbcor.difference.fits"
-title = "Feather - Model"
-output = "../eps/ngc0628_diff_feather.png"
-#
-myim.fits2eps(dir_data = dir_data,
-              imagename_color = imagename_color,
-              imagename_contour = imagename_contour,
-              ra_center = ra_center,
-              dec_center = dec_center,
-              title = title,
-              colorbar_label = colorbar_label,
-              output = output,
-              colorscale = colorscale,
-              colorlog = colorlog,
-              color_contour = color_contour,
-              color_beam = color_beam,
-              colorbar = colorbar,
-              value = value,
-              contour = contour,
-              xlim = xlim,
-              ylim = ylim,
-              clim = clim_diff)
 
 
 ### CBF
 #
-imagename_color = galname + "/sim_ngc0628_cbf_br.smooth.pbcor.clip.fits"
+imagename_color = "sim_ngc0628_tp2vis_br.smooth.fits"
 title = "Convolved tp2vis"
-output = "../eps/ngc0628_tp2vis.png"
+output = "../../eps/ngc0628_tp2vis.png"
 #
 myim.fits2eps(dir_data = dir_data,
               imagename_color = imagename_color,
@@ -173,36 +150,13 @@ myim.fits2eps(dir_data = dir_data,
               xlim = xlim,
               ylim = ylim,
               clim = clim)
-#
-imagename_color = galname + "/sim_ngc0628_cbf_br.smooth.pbcor.difference.fits"
-title = "tp2vis - Model"
-output = "../eps/ngc0628_diff_tp2vis.png"
-#
-myim.fits2eps(dir_data = dir_data,
-              imagename_color = imagename_color,
-              imagename_contour = imagename_contour,
-              ra_center = ra_center,
-              dec_center = dec_center,
-              title = title,
-              colorbar_label = colorbar_label,
-              output = output,
-              colorscale = colorscale,
-              colorlog = colorlog,
-              color_contour = color_contour,
-              color_beam = color_beam,
-              colorbar = colorbar,
-              value = value,
-              contour = contour,
-              xlim = xlim,
-              ylim = ylim,
-              clim = clim_diff)
 
 
 ### CDF
 #
-imagename_color = galname + "/sim_ngc0628_cdf_br.smooth.pbcor.clip.fits"
+imagename_color = "sim_ngc0628_tpmodel_br.smooth.fits"
 title = "Convolved TPmodel"
-output = "../eps/ngc0628_tpmodel.png"
+output = "../../eps/ngc0628_tpmodel.png"
 #
 myim.fits2eps(dir_data = dir_data,
               imagename_color = imagename_color,
@@ -222,29 +176,6 @@ myim.fits2eps(dir_data = dir_data,
               xlim = xlim,
               ylim = ylim,
               clim = clim)
-#
-imagename_color = galname + "/sim_ngc0628_cdf_br.smooth.pbcor.difference.fits"
-title = "TPmodel - Model"
-output = "../eps/ngc0628_diff_tpmodel.png"
-#
-myim.fits2eps(dir_data = dir_data,
-              imagename_color = imagename_color,
-              imagename_contour = imagename_contour,
-              ra_center = ra_center,
-              dec_center = dec_center,
-              title = title,
-              colorbar_label = colorbar_label,
-              output = output,
-              colorscale = colorscale,
-              colorlog = colorlog,
-              color_contour = color_contour,
-              color_beam = color_beam,
-              colorbar = colorbar,
-              value = value,
-              contour = contour,
-              xlim = xlim,
-              ylim = ylim,
-              clim = clim_diff)
 
 
 os.system("rm -rf *.last")
