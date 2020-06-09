6f665da37425ce70b170ac6d455c3755c8ca2b21
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-06-09 14:55:00 +0200

    Commit at 2020-06-09 14:55:00

--- mycasa_scripts_active/scripts_phangs06_ssc_leroy20/myim05_fidelity.py
+++ mycasa_scripts_active/scripts_phangs06_ssc_leroy20/myim05_fidelity.py
@@ -13,5 +13,11 @@ dir_gals = glob.glob(dir_data + "sim_*/")
 dir_gals.sort()
 
 
-imagenames = glob.glob(dir_gals[i] + "*_br.smooth")
-skymodel = glob.glob(dir_gals[i] + "*_skymodel_regrid.smooth")[0]
+for i in range(len(dir_gals)):
+    imagenames = glob.glob(dir_gals[i] + "*_br.smooth")
+    skymodel = glob.glob(dir_gals[i] + "*_skymodel_regrid.smooth")[0]
+    for j in range(len(imagenames)):
+        outfile = imagenames[j].replace(".smooth",".fidelity")
+        os.system("rm -rf " + outfile)
+        print("# processing " + outfile.split("/")[-1])
+        immath(imagename=[imagenames[j],skymodel], expr="abs(IM1)/abs(IM1-IM0)", outfile=outfile)
