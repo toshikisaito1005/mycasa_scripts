adce8b737ccbf44b3c68b690bc23af031fbd46c4
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-06-09 15:50:00 +0200

    Commit at 2020-06-09 15:50:00

--- mycasa_scripts_active/scripts_phangs06_ssc_leroy20/myim06b_list_diff_total.py
+++ mycasa_scripts_active/scripts_phangs06_ssc_leroy20/myim06b_list_diff_total.py
@@ -17,10 +17,10 @@ dir_gals = glob.glob(dir_data + "sim_*/")
 dir_gals.sort()
 
 
-list_fidelity_7m = []
-list_fidelity_feather = []
-list_fidelity_tp2vis = []
-list_fidelity_tpmodel = []
+list_7m = []
+list_feather = []
+list_tp2vis = []
+list_tpmodel = []
 for i in range(len(dir_gals)):
        print("# processing " + dir_gals[i].split("/")[-2] + " " + str(i) + "/" + str(len(dir_gals)))
        #
@@ -30,32 +30,35 @@ for i in range(len(dir_gals)):
        image_tpmodel = glob.glob(dir_gals[i] + "*tpmodel*.smooth")[0]
        image_model = glob.glob(dir_gals[i] + "*skymodel_regrid.smooth")[0]
        #
-       shape = imhead(fidelity_7m,mode="list")["shape"]
+       shape = imhead(image_7m,mode="list")["shape"]
        box = "0,0,"+str(shape[0]-1)+","+str(shape[1]-1)
        #
+       image_model = imval(image_model,box=box)["data"].flatten()
+       image_model[np.isnan(image_model)] = 0
+       diff_total_model = np.sum(image_model[image_model>0])
        #
-       fidelity_7m = imval(fidelity_7m,box=box)["data"].flatten()
-       fidelity_7m[np.isnan(fidelity_7m)] = 0
-       median_fidelity_7m = np.sum(fidelity_7m[fidelity_7m>0])
+       image_7m = imval(image_7m,box=box)["data"].flatten()
+       image_7m[np.isnan(image_7m)] = 0
+       diff_total_7m = (np.sum(image_7m[image_7m>0]) - diff_total_model) / diff_total_model
        #
-       fidelity_feather = imval(fidelity_feather,box=box)["data"].flatten()
-       fidelity_feather[np.isnan(fidelity_feather)] = 0
-       median_fidelity_feather = np.sum(fidelity_feather[fidelity_feather>0])
+       image_feather = imval(image_feather,box=box)["data"].flatten()
+       image_feather[np.isnan(image_feather)] = 0
+       diff_total_feather = (np.sum(image_feather[image_feather>0]) - diff_total_model) / diff_total_model
        #
-       fidelity_tp2vis = imval(fidelity_tp2vis,box=box)["data"].flatten()
-       fidelity_tp2vis[np.isnan(fidelity_tp2vis)] = 0
-       median_fidelity_tp2vis = np.sum(fidelity_tp2vis[fidelity_tp2vis>0])
+       image_tp2vis = imval(image_tp2vis,box=box)["data"].flatten()
+       image_tp2vis[np.isnan(image_tp2vis)] = 0
+       diff_total_tp2vis = (np.sum(image_tp2vis[image_tp2vis>0]) - diff_total_model) / diff_total_model
        #
-       fidelity_tpmodel = imval(fidelity_tpmodel,box=box)["data"].flatten()
-       fidelity_tpmodel[np.isnan(fidelity_tpmodel)] = 0
-       median_fidelity_tpmodel = np.sum(fidelity_tpmodel[fidelity_tpmodel>0])
+       image_tpmodel = imval(image_tpmodel,box=box)["data"].flatten()
+       image_tpmodel[np.isnan(image_tpmodel)] = 0
+       diff_total_tpmodel = (np.sum(image_tpmodel[image_tpmodel>0]) - diff_total_model) / diff_total_model
        #
-       list_fidelity_7m.append(median_fidelity_7m)
-       list_fidelity_feather.append(median_fidelity_feather)
-       list_fidelity_tp2vis.append(median_fidelity_tp2vis)
-       list_fidelity_tpmodel.append(median_fidelity_tpmodel)
+       list_7m.append(diff_total_7m)
+       list_feather.append(diff_total_feather)
+       list_tp2vis.append(diff_total_tp2vis)
+       list_tpmodel.append(diff_total_tpmodel)
 
-list_median = np.c_[list_fidelity_7m,list_fidelity_feather,list_fidelity_tp2vis,list_fidelity_tpmodel]
-np.savetxt("list_median.txt",list_median,header="7m feather tp2vis tpmodel",fmt="%.3f")
+list_median = np.c_[list_7m,list_feather,list_tp2vis,list_tpmodel]
+np.savetxt("list_diff_total.txt",list_median,header="7m feather tp2vis tpmodel",fmt="%.3f")
 
 os.system("rm -rf *.last")
