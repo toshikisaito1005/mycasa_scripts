0d0e7a750ada821900f571dd8917767b420f0c6f
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-06-09 17:15:00 +0200

    Commit at 2020-06-09 17:15:00

--- mycasa_scripts_active/scripts_phangs06_ssc_leroy20/f02_plots.py
+++ mycasa_scripts_active/scripts_phangs06_ssc_leroy20/f02_plots.py
@@ -3,6 +3,8 @@ import re
 import sys
 import glob
 import scipy
+import numpy as np
+import matplotlib.cm as cm
 import mycasaimaging_tools as myim
 import matplotlib.pyplot as plt
 plt.ioff()
@@ -21,3 +23,15 @@ if not done:
 #####################
 ### Main Procedure
 #####################
+data = np.loadtxt("list_median.txt")
+
+fig = plt.figure(figsize=(12,4))
+plt.rcParams["font.size"] = 16
+plt.subplots_adjust(bottom=0.15, left=0.07, right=0.95, top=0.95)
+
+median_7m, disp_7m = np.round(np.median(data[:,1]),1), np.round(np.std(data[:,1]),1)
+label_7m = "7m-only (" + str(median_7m) + " $\pm$ " + str(disp_7m) + ")"
+
+plt.scatter(data[:,0],
+            data[:,1],
+            color=cm.gnuplot(0/3.5), s=50, alpha=0.7, lw=1, label=label_7m)
