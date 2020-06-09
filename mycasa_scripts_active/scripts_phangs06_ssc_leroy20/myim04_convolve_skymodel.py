e1b6bc66cf6f32a0b983ac46f77198d4e1e1fb60
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-06-09 13:50:01 +0200

    Commit at 2020-06-09 13:50:01

--- mycasa_scripts_active/scripts_phangs06_ssc_leroy20/myim04_convolve_skymodel.py
+++ mycasa_scripts_active/scripts_phangs06_ssc_leroy20/myim04_convolve_skymodel.py
@@ -2,15 +2,23 @@ import os
 import sys
 import glob
 import datetime
-a=datetime.datetime.now()
 
 
 ##############################
 ### parameters
 ##############################
-dir_proj = "/Users/saito/data/myproj_active/proj_phangs06_ssc/sim_phangs/v3p4_tpeak/"
+dir_proj = "/Users/saito/data/myproj_active/proj_phangs06_ssc/v3p4_tpeak/"
 
 
 ##############################
 ### main
 ##############################
+skymodels = glob.glob(dir_proj + "*.skymodel")
+skymodels.sort()
+
+for i in range(len(skymodels)):
+	galname = skymodels[i].split("/")[-1].split("_")[0]
+	dir_gal = dir_proj + "../sim_phangs/sim_" + galname + "/"
+	# smooth
+	imsmoith(imagename = skymodels[i],
+		)
