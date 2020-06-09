f413285fa33a938119798bd41c2cc4053cbf99c7
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-06-09 17:25:00 +0200

    Commit at 2020-06-09 17:25:00

--- mycasa_scripts_active/scripts_phangs06_ssc_leroy20/f02_plots.py
+++ mycasa_scripts_active/scripts_phangs06_ssc_leroy20/f02_plots.py
@@ -24,30 +24,44 @@ if not done:
 ### def
 #####################
 def plotter(
-       txtdata,):
+       txtdata,
+       output,
+       ylim,
+       ):
+       data = np.loadtxt(txtdata)
+       #
+       fig = plt.figure(figsize=(12,4))
+       plt.rcParams["font.size"] = 16
+       plt.subplots_adjust(bottom=0.15, left=0.07, right=0.95, top=0.95)
+       #
+       median_7m, disp_7m = np.round(np.median(data[:,1]),1), np.round(np.std(data[:,1]),1)
+       median_feather, disp_feather = np.round(np.median(data[:,2]),1), np.round(np.std(data[:,2]),1)
+       median_tp2vis, disp_tp2vis = np.round(np.median(data[:,3]),1), np.round(np.std(data[:,3]),1)
+       median_tpmodel, disp_tpmodel = np.round(np.median(data[:,4]),1), np.round(np.std(data[:,4]),1)
+       #
+       plt.plot([0,300],[median_7m,median_7m],"--",c=cm.gnuplot(0/3.5),lw=2)
+       plt.plot([0,300],[median_feather,median_feather],"--",c=cm.gnuplot(1/3.5),lw=2)
+       plt.plot([0,300],[median_tp2vis,median_tp2vis],"--",c=cm.gnuplot(2/3.5),lw=2)
+       plt.plot([0,300],[median_tpmodel,median_tpmodel],"--",c=cm.gnuplot(3/3.5),lw=2)
+       #
+       label_7m = "7m-only (" + str(median_7m) + " $\pm$ " + str(disp_7m) + ")"
+       label_feather = "feather (" + str(median_feather) + " $\pm$ " + str(disp_feather) + ")"
+       label_tp2vis = "tp2vis (" + str(median_tp2vis) + " $\pm$ " + str(disp_tp2vis) + ")"
+       label_tpmodel = "tpmodel (" + str(median_tpmodel) + " $\pm$ " + str(disp_tpmodel) + ")"
+       #
+       plt.scatter(data[:,0]*2, data[:,1], color=cm.gnuplot(0/3.5), s=50, alpha=0.7, lw=1, label=label_7m)
+       plt.scatter(data[:,0]*2, data[:,2], color=cm.gnuplot(1/3.5), s=50, alpha=0.7, lw=1, label=label_feather)
+       plt.scatter(data[:,0]*2, data[:,3], color=cm.gnuplot(2/3.5), s=50, alpha=0.7, lw=1, label=label_tp2vis)
+       plt.scatter(data[:,0]*2, data[:,4], color=cm.gnuplot(3/3.5), s=50, alpha=0.7, lw=1, label=label_tpmodel)
+       #
+       plt.xlim([0,300])
+       plt.ylim(ylim)
+       plt.legend(ncol=2)
+       plt.savefig(output)
+
 
 #####################
 ### Main Procedure
 #####################
-data = np.loadtxt("list_median.txt")
-#
-fig = plt.figure(figsize=(12,4))
-plt.rcParams["font.size"] = 16
-plt.subplots_adjust(bottom=0.15, left=0.07, right=0.95, top=0.95)
-#
-median_7m, disp_7m = np.round(np.median(data[:,1]),1), np.round(np.std(data[:,1]),1)
-median_feather, disp_feather = np.round(np.median(data[:,2]),1), np.round(np.std(data[:,2]),1)
-median_tp2vis, disp_tp2vis = np.round(np.median(data[:,3]),1), np.round(np.std(data[:,3]),1)
-median_tpmodel, disp_tpmodel = np.round(np.median(data[:,4]),1), np.round(np.std(data[:,4]),1)
-#
-label_7m = "7m-only (" + str(median_7m) + " $\pm$ " + str(disp_7m) + ")"
-label_feather = "feather (" + str(median_feather) + " $\pm$ " + str(disp_feather) + ")"
-label_tp2vis = "tp2vis (" + str(median_tp2vis) + " $\pm$ " + str(disp_tp2vis) + ")"
-label_tpmodel = "tpmodel (" + str(median_tpmodel) + " $\pm$ " + str(disp_tpmodel) + ")"
-#
-plt.scatter(data[:,0], data[:,1], color=cm.gnuplot(0/3.5), s=50, alpha=0.7, lw=1, label=label_7m)
-plt.scatter(data[:,0], data[:,2], color=cm.gnuplot(1/3.5), s=50, alpha=0.7, lw=1, label=label_feather)
-plt.scatter(data[:,0], data[:,3], color=cm.gnuplot(2/3.5), s=50, alpha=0.7, lw=1, label=label_tp2vis)
-plt.scatter(data[:,0], data[:,4], color=cm.gnuplot(3/3.5), s=50, alpha=0.7, lw=1, label=label_tpmodel)
-#
-plt.savefig(dir_data + "fig_fidelity_vs_circ_diameter.png")
+plotter("list_median.txt", dir_data+"fig_fidelity_vs_circ_diameter.png", [0,55])
+
