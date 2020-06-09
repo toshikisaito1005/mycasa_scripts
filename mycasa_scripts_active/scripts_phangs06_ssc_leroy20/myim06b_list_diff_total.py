77ae7fd478036ec82b33f5c289ca25c1f31cfbe2
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-06-09 15:45:00 +0200

    Commit at 2020-06-09 15:45:00

--- mycasa_scripts_active/scripts_phangs06_ssc_leroy20/myim06b_list_diff_total.py
+++ mycasa_scripts_active/scripts_phangs06_ssc_leroy20/myim06b_list_diff_total.py
@@ -24,40 +24,38 @@ list_fidelity_tpmodel = []
 for i in range(len(dir_gals)):
        print("# processing " + dir_gals[i].split("/")[-2] + " " + str(i) + "/" + str(len(dir_gals)))
        #
-       fidelity_7m = glob.glob(dir_gals[i] + "*7m*.fidelity")[0]
-       fidelity_feather = glob.glob(dir_gals[i] + "*feather*.fidelity")[0]
-       fidelity_tp2vis = glob.glob(dir_gals[i] + "*tp2vis*.fidelity")[0]
-       fidelity_tpmodel = glob.glob(dir_gals[i] + "*tpmodel*.fidelity")[0]
-       #
-       image_7m = glob.glob(dir_gals[i] + "*7m*.fidelity")[0]
-       image_feather = glob.glob(dir_gals[i] + "*feather*.fidelity")[0]
-       image_tp2vis = glob.glob(dir_gals[i] + "*tp2vis*.fidelity")[0]
-       image_tpmodel = glob.glob(dir_gals[i] + "*tpmodel*.fidelity")[0]
+       image_7m = glob.glob(dir_gals[i] + "*7m*.smooth")[0]
+       image_feather = glob.glob(dir_gals[i] + "*feather*.smooth")[0]
+       image_tp2vis = glob.glob(dir_gals[i] + "*tp2vis*.smooth")[0]
+       image_tpmodel = glob.glob(dir_gals[i] + "*tpmodel*.smooth")[0]
        image_model = glob.glob(dir_gals[i] + "*skymodel_regrid.smooth")[0]
        #
        shape = imhead(fidelity_7m,mode="list")["shape"]
        box = "0,0,"+str(shape[0]-1)+","+str(shape[1]-1)
        #
+       #
        fidelity_7m = imval(fidelity_7m,box=box)["data"].flatten()
        fidelity_7m[np.isnan(fidelity_7m)] = 0
-       median_fidelity_7m = np.median(fidelity_7m[fidelity_7m>0])
+       median_fidelity_7m = np.sum(fidelity_7m[fidelity_7m>0])
        #
        fidelity_feather = imval(fidelity_feather,box=box)["data"].flatten()
        fidelity_feather[np.isnan(fidelity_feather)] = 0
-       median_fidelity_feather = np.median(fidelity_feather[fidelity_feather>0])
+       median_fidelity_feather = np.sum(fidelity_feather[fidelity_feather>0])
        #
        fidelity_tp2vis = imval(fidelity_tp2vis,box=box)["data"].flatten()
        fidelity_tp2vis[np.isnan(fidelity_tp2vis)] = 0
-       median_fidelity_tp2vis = np.median(fidelity_tp2vis[fidelity_tp2vis>0])
+       median_fidelity_tp2vis = np.sum(fidelity_tp2vis[fidelity_tp2vis>0])
        #
        fidelity_tpmodel = imval(fidelity_tpmodel,box=box)["data"].flatten()
        fidelity_tpmodel[np.isnan(fidelity_tpmodel)] = 0
-       median_fidelity_tpmodel = np.median(fidelity_tpmodel[fidelity_tpmodel>0])
+       median_fidelity_tpmodel = np.sum(fidelity_tpmodel[fidelity_tpmodel>0])
        #
        list_fidelity_7m.append(median_fidelity_7m)
        list_fidelity_feather.append(median_fidelity_feather)
        list_fidelity_tp2vis.append(median_fidelity_tp2vis)
        list_fidelity_tpmodel.append(median_fidelity_tpmodel)
 
+list_median = np.c_[list_fidelity_7m,list_fidelity_feather,list_fidelity_tp2vis,list_fidelity_tpmodel]
+np.savetxt("list_median.txt",list_median,header="7m feather tp2vis tpmodel",fmt="%.3f")
 
 os.system("rm -rf *.last")
