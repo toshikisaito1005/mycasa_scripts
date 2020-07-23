d6a78fb78da7fd1295e279cb51e99aa60e86dc23
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-07-24 00:05:00 +0200

    Commit at 2020-07-24 00:05:00

--- mycasa_scripts_active/scripts_ts10_phangs_ulirg/f00_all_final_eps.sh
+++ mycasa_scripts_active/scripts_ts10_phangs_ulirg/f00_all_final_eps.sh
@@ -1,5 +1,10 @@
 dir_data="/Users/saito/data/myproj_active/proj_ts10_phangs_ulirgs/eps/"
 
-### figure 1
-convert -crop 700x560+50+30 $dir_data"eso267_m0.png" $dir_data"eso267_m0_clip.png"
+galname1=(eso267 eso297g011 eso297g012 eso507 eso557 ic4518e ic4518w ic5179 iras06592)
+galname2=()
 
+### figure 1
+for var in ${galname[@]}
+do
+convert -crop 700x560+50+30 $dir_data$var"_m0.png" $dir_data$var"_m0_clip.png"
+done
