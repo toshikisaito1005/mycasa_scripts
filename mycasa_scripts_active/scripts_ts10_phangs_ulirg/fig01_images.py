078a14c484360a4f46f9dfa6ab31364d52a39279
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-07-23 22:20:00 +0200

    Commit at 2020-07-23 22:20:00

--- mycasa_scripts_active/scripts_ts10_phangs_ulirg/fig01_images.py
+++ mycasa_scripts_active/scripts_ts10_phangs_ulirg/fig01_images.py
@@ -30,8 +30,8 @@ if not done:
 # eso267
 ra_center = "12:14:12.923"
 dec_center = "-47.13.43.85"
-xlim = [-9, 9]
-ylim = [9, -9]
+xlim = [-9.5, 9.5]
+ylim = [9.5, -9.5]
 imagename_contour = "data/eso267_12m_co21_pbcorr_trimmed_k_res150pc_strict_mom0.fits"
 imagename_color = imagename_contour
 title = "ESO 267-G030"
@@ -57,8 +57,8 @@ myim.fits2eps(dir_data = dir_data,
 # eso297g011
 ra_center = "01:36:23.415"
 dec_center = "-37.19.17.6"
-xlim = [-10, 10]
-ylim = [10, -10]
+xlim = [-9.5, 9.5]
+ylim = [9.5, -9.5]
 value = None
 imagename_contour = "data/eso297g011_12m_co21_pbcorr_trimmed_k_res150pc_strict_mom0.fits"
 imagename_color = imagename_contour
@@ -85,8 +85,8 @@ myim.fits2eps(dir_data = dir_data,
 # eso297g012
 ra_center = "01:36:24.185"
 dec_center = "-37.20.25.45"
-xlim = [-5, 5]
-ylim = [5, -5]
+xlim = [-4.7, 4.7]
+ylim = [4.7, -4.7]
 value = None
 imagename_contour = "data/eso297g012_12m_co21_pbcorr_trimmed_k_res150pc_strict_mom0.fits"
 imagename_color = imagename_contour
@@ -110,16 +110,44 @@ myim.fits2eps(dir_data = dir_data,
               xlim = xlim,
               ylim = ylim)
 
-# eso297g012
+# eso507
 ra_center = "13:02:52.337"
 dec_center = "-23.55.18.5"
-xlim = [-6, 6]
-ylim = [6, -6]
+xlim = [-5.1, 5.1]
+ylim = [5.1, -5.1]
 value = None
 imagename_contour = "data/eso507_12m_co21_pbcorr_trimmed_k_res150pc_strict_mom0.fits"
 imagename_color = imagename_contour
 title = "ESO 507-G070"
 output = "eps/eso507_m0.png"
+myim.fits2eps(dir_data = dir_data,
+              imagename_color = imagename_color,
+              imagename_contour = imagename_contour,
+              ra_center = ra_center,
+              dec_center = dec_center,
+              title = title,
+              colorbar_label = colorbar_label,
+              output = output,
+              colorscale = colorscale,
+              colorlog = colorlog,
+              color_contour = color_contour,
+              color_beam = color_beam,
+              colorbar = colorbar,
+              value = value,
+              contour = contour,
+              xlim = xlim,
+              ylim = ylim)
+
+# eso557
+ra_center = "06:31:47.26"
+dec_center = "-17.37.15.783"
+xlim = [-6, 6]
+ylim = [6, -6]
+value = None
+imagename_contour = "data/eso557_12m+7m_co21_pbcorr_trimmed_k_res150pc_strict_mom0.fits"
+imagename_color = imagename_contour
+title = "ESO 557-G002"
+output = "eps/eso557_m0.png"
 myim.fits2eps(dir_data = dir_data,
               imagename_color = imagename_color,
               imagename_contour = imagename_contour,
