ef2b89684af6aafac86b228f85f987d7e5b58721
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-07-24 00:10:00 +0200

    Commit at 2020-07-24 00:10:00

--- mycasa_scripts_active/scripts_ts10_phangs_ulirg/f00_all_final_eps.sh
+++ mycasa_scripts_active/scripts_ts10_phangs_ulirg/f00_all_final_eps.sh
@@ -1,10 +1,10 @@
 dir_data="/Users/saito/data/myproj_active/proj_ts10_phangs_ulirgs/eps/"
 
 galname1=(eso267 eso297g011 eso297g012 eso507 eso557 ic4518e ic4518w ic5179 iras06592)
-galname2=()
+galname2=(irasf10409 irasf17138 mcg02 ngc1614 ngc2369 ngc3110 ngc3256 ngc5257 ngc6240)
 
 ### figure 1
-for var in ${galname[@]}
+for var in ${galname1[@]} ${galname2[@]}
 do
-convert -crop 700x560+50+30 $dir_data$var"_m0.png" $dir_data$var"_m0_clip.png"
+convert -crop 710x560+40+30 $dir_data$var"_m0.png" $dir_data$var"_m0_clip.png"
 done
