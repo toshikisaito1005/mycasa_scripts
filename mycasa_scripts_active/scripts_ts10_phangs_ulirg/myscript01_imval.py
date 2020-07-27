0817b48e995f63103f951c20daeaf811096654a1
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-07-27 23:15:00 +0200

    Commit at 2020-07-27 23:15:00

---
+++ mycasa_scripts_active/scripts_ts10_phangs_ulirg/myscript01_imval.py
@@ -0,0 +1,16 @@
+import os
+import re
+import sys
+import glob
+import scipy
+import numpy as np
+
+#####################
+### Parameter
+#####################
+dir_proj = "/Users/saito/data/myproj_active/proj_ts10_phangs_ulirgs/data/"
+
+
+#####################
+### Main Procedure
+#####################
