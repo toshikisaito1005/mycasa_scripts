3fd89345fced96735d5caa0a4bf6c4d3020a2d95
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-07-27 23:20:00 +0200

    Commit at 2020-07-27 23:20:00

--- mycasa_scripts_active/scripts_ts10_phangs_ulirg/myscript01_imval.py
+++ mycasa_scripts_active/scripts_ts10_phangs_ulirg/myscript01_imval.py
@@ -9,8 +9,12 @@ import numpy as np
 ### Parameter
 #####################
 dir_proj = "/Users/saito/data/myproj_active/proj_ts10_phangs_ulirgs/data/"
-
+galaxy = ["eso267"]
 
 #####################
 ### Main Procedure
 #####################
+for i in range(len(galaxy)):
+	this_galaxy = galaxy[i]
+	this_mom0 = glob.glob(dir_proj + this_galaxy + "*_mom0.fits")[0]
+	#
