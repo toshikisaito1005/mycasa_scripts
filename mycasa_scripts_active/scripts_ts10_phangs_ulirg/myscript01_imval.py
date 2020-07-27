6fe2c0ccb1b1e156d7d0de14943bb6b4d41765e8
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-07-28 00:35:00 +0200

    Commit at 2020-07-28 00:35:00

--- mycasa_scripts_active/scripts_ts10_phangs_ulirg/myscript01_imval.py
+++ mycasa_scripts_active/scripts_ts10_phangs_ulirg/myscript01_imval.py
@@ -20,6 +20,7 @@ galaxy = ["eso267"]
 for i in range(len(galaxy)):
 	this_galaxy = galaxy[i]
 	this_mom0 = glob.glob(dir_proj + this_galaxy + "*_mom0.fits")[0]
+	this_ew = glob.glob(dir_proj + this_galaxy + "*_ew.fits")[0]
 	# get box
 	this_header = imhead(this_mom0,mode="list")
 	shape = this_header["shape"]
@@ -29,13 +30,22 @@ for i in range(len(galaxy)):
 	this_pixelsize = abs(this_header["cdelt1"]) * 3600 * 180 / np.pi # arcsec
 	this_beamsize_per_pix = this_beamsize / this_pixelsize
 	gridsize = (int(shape[0] / this_beamsize_per_pix), int(shape[1] / this_beamsize_per_pix))
-	# imval
+	# imval mom0
 	this_data = imval(this_mom0, box=box)
 	x = this_data["coords"][:,:,0].flatten()
 	y = this_data["coords"][:,:,1].flatten()
-	z = this_data["data"].flatten()
-	# plot
+	z_m0 = this_data["data"].flatten()
+	# imval ew
+	this_data = imval(this_ew, box=box)
+	z_ew = this_data["data"].flatten()
+	# hexbin
 	fig, ax = plt.subplots(1, 1)
-	hexdata = ax.hexbin(x, y, C=z, gridsize=gridsize)
-	hexdata = hexdata.get_array()
+	hex_m0 = ax.hexbin(x, y, C=z_m0, gridsize=gridsize)
+	hex_m0 = hex_m0.get_array()
+	hex_ew = ax.hexbin(x, y, C=z_ew, gridsize=gridsize)
+	hex_ew = hex_m0.get_array()
 	# plt.savefig(dir_eps+"test.png",dpi=200)
+	#
+	fig, ax = plt.subplots(1, 1)
+	ax.scatter(hex_m0,hex_ew)
+	plt.savefig(dir_eps+"test.png",dpi=200)
