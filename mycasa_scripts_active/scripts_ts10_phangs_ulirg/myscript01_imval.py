b9ef719e3c8132945a7238667e85667b1939b8c8
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-07-27 23:25:00 +0200

    Commit at 2020-07-27 23:25:00

--- mycasa_scripts_active/scripts_ts10_phangs_ulirg/myscript01_imval.py
+++ mycasa_scripts_active/scripts_ts10_phangs_ulirg/myscript01_imval.py
@@ -17,4 +17,8 @@ galaxy = ["eso267"]
 for i in range(len(galaxy)):
 	this_galaxy = galaxy[i]
 	this_mom0 = glob.glob(dir_proj + this_galaxy + "*_mom0.fits")[0]
-	#
+	# get box
+	shape = imhead(this_mom0,mode="list")["shape"]
+	box = "0,0," + str(shape[0]) + "," + str(shape[1])
+	# get beam and pixel size
+	
