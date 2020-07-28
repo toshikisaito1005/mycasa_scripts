f2f315c89d3228db9e13a7aa9bf2ce494e7b5ecb
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-07-28 23:20:00 +0200

    Commit at 2020-07-28 23:20:00

--- mycasa_scripts_active/scripts_ts10_phangs_ulirg/fig02_scatter.py
+++ mycasa_scripts_active/scripts_ts10_phangs_ulirg/fig02_scatter.py
@@ -51,25 +51,28 @@ print("# get phangs data")
 
 ### plot
 print("# plot")
-figure = plt.figure(figsize=(10,9))
+figure = plt.figure(figsize=(10,10))
 gs = gridspec.GridSpec(nrows=9, ncols=9)
 ax1 = plt.subplot(gs[0:7,0:7])
 ax2 = plt.subplot(gs[0:7,7:9])
 ax3 = plt.subplot(gs[7:9,0:7])
 plt.rcParams["font.size"] = 20
 plt.rcParams["legend.fontsize"] = 18
-plt.subplots_adjust(bottom=0.15, left=0.15, right=0.95, top=0.95) 
+plt.subplots_adjust(bottom=0.15, left=0.15, right=0.90, top=0.85) 
 # plot
-ax1.scatter(phangs_m0*4.3, phangs_ew, c="skyblue", s=40, alpha=0.4, linewidths=0)
-ax1.scatter(lirg_m0*0.8, lirg_ew, c="pink", s=40, alpha=0.4, linewidths=0)
+ax1.scatter(phangs_m0*4.3, phangs_ew, c="skyblue", s=40, linewidths=0)
+ax1.scatter(lirg_m0*0.8, lirg_ew, c="pink", s=40, linewidths=0)
 # ax1
 ax1.set_xlim([10**-1,10**4.5])
 ax1.set_ylim([10**-0.1,10**2.4])
 ax1.grid()
+ax1.tick_params(labelbottom=False)
 ax1.set_xscale("log")
 ax1.set_yscale("log")
-ax1.set_xlabel(r"$\Sigma_{\mathsf{mol,150pc}}$ ($M_{\odot}$ pc$^{-2}$)")
 ax1.set_ylabel(r"$\sigma_{\mathsf{mol,150pc}}$ (km s$^{-1}$)")
+# ax2
+# ax3
+ax3.set_xlabel(r"$\Sigma_{\mathsf{mol,150pc}}$ ($M_{\odot}$ pc$^{-2}$)")
 #
 plt.legend(ncol=4, loc="upper left")
 plt.savefig(dir_eps+"plot_scatter_all.png",dpi=200)
