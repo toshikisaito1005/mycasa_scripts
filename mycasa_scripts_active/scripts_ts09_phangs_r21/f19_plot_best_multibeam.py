99c31c7a92f27209bed145cdc90cc249496abaa3
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-07-28 19:30:00 +0200

    Commit at 2020-07-28 19:30:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f19_plot_best_multibeam.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f19_plot_best_multibeam.py
@@ -26,6 +26,34 @@ beams = ["04p0","08p0","12p0","16p0","20p0"]
 #####################
 ### functions
 #####################
+def weighted_percentile(
+    data,
+    percentile,
+    weights=None,
+    ):
+    """
+    Args:
+        data (list or numpy.array): data
+        weights (list or numpy.array): weights
+    """
+    if weights==None:
+        w_median = np.percentile(data,percentile*100)
+    else:
+        data, weights = np.array(data).squeeze(), np.array(weights).squeeze()
+        s_data, s_weights = map(np.array, zip(*sorted(zip(data, weights))))
+        midpoint = percentile * sum(s_weights)
+        if any(weights > midpoint):
+            w_median = (data[weights == np.max(weights)])[0]
+        else:
+            cs_weights = np.cumsum(s_weights)
+            idx = np.where(cs_weights <= midpoint)[0][-1]
+            if cs_weights[idx] == midpoint:
+                w_median = np.mean(s_data[idx:idx+2])
+            else:
+                w_median = s_data[idx+1]
+
+    return w_median
+
 def get_binned_dist(x,y,binrange):
 	"""
 	"""
@@ -380,9 +408,6 @@ for i in range(len(beams)):
 	list_log_r21_mom0_k_model_scatter.append(log_r21_mom0_k_model_scatter.tolist())
 	list_log_r21_mom0_k_model_scatter_noise.append(log_r21_mom0_k_model_scatter_noise.tolist())
 
-
-list_median_cut = [10**np.median(s) for s in list_log_r21_mom0_k_model_scatter_noise]
-list_width_cut = [10**np.percentile(s,84)-10**np.percentile(s,16) for s in list_log_r21_mom0_k_model_scatter_noise]
 
 list_log_r21_mom0_k_model_scatter_noise_nocut = []
 for i in range(len(list_log_co21_mom0_k_model_scatter_noise)):
