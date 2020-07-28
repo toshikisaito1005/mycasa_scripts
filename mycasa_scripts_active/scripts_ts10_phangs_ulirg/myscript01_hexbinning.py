d6424e28486ff40113b8d84dc1a06077d44ab358
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-07-28 22:45:00 +0200

    Commit at 2020-07-28 22:45:00

--- mycasa_scripts_active/scripts_ts10_phangs_ulirg/myscript01_hexbinning.py
+++ mycasa_scripts_active/scripts_ts10_phangs_ulirg/myscript01_hexbinning.py
@@ -57,8 +57,8 @@ for i in range(len(galaxy)):
 for i in range(len(phangs)):
 	this_galaxy = phangs[i]
 	print("# working on " + this_galaxy)
-	this_mom0 = glob.glob(dir_proj + this_galaxy + "*_mom0.fits")[0]
-	this_ew = glob.glob(dir_proj + this_galaxy + "*_ew.fits")[0]
+	this_mom0 = glob.glob(dir_proj + "../data_phangs/" + this_galaxy + "*_mom0*.fits")[0]
+	this_ew = glob.glob(dir_proj + "../data_phangs/" + this_galaxy + "*_ew*.fits")[0]
 	# get box
 	this_header = imhead(this_mom0,mode="list")
 	shape = this_header["shape"]
@@ -73,16 +73,17 @@ for i in range(len(phangs)):
 	x = this_data["coords"][:,:,0].flatten()
 	y = this_data["coords"][:,:,1].flatten()
 	z_m0 = this_data["data"].flatten()
+	z_m0[np.isnan(z_m0)] = 0
 	# imval ew
 	this_data = imval(this_ew, box=box)
 	z_ew = this_data["data"].flatten()
+	z_ew[np.isnan(z_ew)] = 0
 	# hexbin
 	fig, ax = plt.subplots(1, 1)
 	hex_m0 = ax.hexbin(x, y, C=z_m0, gridsize=gridsize)
 	hex_m0 = hex_m0.get_array()
 	hex_ew = ax.hexbin(x, y, C=z_ew, gridsize=gridsize)
 	hex_ew = hex_ew.get_array()
-	plt.savefig(dir_eps+"hex_"+this_galaxy+".png",dpi=200)
 	# output
 	np.savetxt(dir_eps+"scatter_"+this_galaxy+".txt",np.c_[hex_m0,hex_ew])
 
