41c94f80a7d9cc2b73db06a862312fe419168326
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-07-29 14:50:00 +0200

    Commit at 2020-07-29 14:50:00

--- mycasa_scripts_active/scripts_ts10_phangs_ulirg/myscript01_hexbinning.py
+++ mycasa_scripts_active/scripts_ts10_phangs_ulirg/myscript01_hexbinning.py
@@ -13,14 +13,16 @@ import matplotlib.patches as mpatches
 #####################
 dir_proj = "/Users/saito/data/myproj_active/proj_ts10_phangs_ulirgs/data/"
 dir_eps = "/Users/saito/data/myproj_active/proj_ts10_phangs_ulirgs/eps/"
-galaxy = [s.split("/")[-1].split("_12m")[0] for s in glob.glob(dir_proj + "*mom0*")]
 phangs = [s.split("/")[-1].split("_12m")[0] for s in glob.glob(dir_proj + "../data_phangs/*mom0*")]
-
+#galaxy = [s.split("/")[-1].split("_12m")[0] for s in glob.glob(dir_proj + "*mom0*")]
+galaxy = ['eso267','eso297g011','eso297g012','eso319','eso507','eso557','ic4518e','ic4518w','ic5179','iras06592','irasf10409','irasf17138','mcg02-33-098','ngc1614','ngc2369','ngc3110','ngc3256','ngc5257','ngc6240']
+center = 
 
 #####################
 ### Main Procedure
 #####################
-for i in range(len(galaxy)):
+#for i in range(len(galaxy)):
+for i in [0]:
 	this_galaxy = galaxy[i]
 	print("# working on " + this_galaxy)
 	this_mom0 = glob.glob(dir_proj + this_galaxy + "*_mom0.fits")[0]
@@ -48,7 +50,11 @@ for i in range(len(galaxy)):
 	hex_m0 = hex_m0.get_array()
 	hex_ew = ax.hexbin(x, y, C=z_ew, gridsize=gridsize)
 	hex_ew = hex_ew.get_array()
-	plt.savefig(dir_eps+"hex_"+this_galaxy+".png",dpi=200)
+	hex_x = ax.hexbin(x, y, C=x, gridsize=gridsize)
+	hex_x = hex_x.get_array()
+	hex_y = ax.hexbin(x, y, C=y, gridsize=gridsize)
+	hex_y = hex_y.get_array()
+	#plt.savefig(dir_eps+"hex_"+this_galaxy+".png",dpi=200)
 	# output
 	np.savetxt(dir_eps+"scatter_"+this_galaxy+".txt",np.c_[hex_m0,hex_ew])
 
