44708825ca96f20e2b6f4370146d31b67a0545ef
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-07-29 16:25:01 +0200

    Commit at 2020-07-29 16:25:00

--- mycasa_scripts_active/scripts_ts10_phangs_ulirg/f00_all_final_eps.sh
+++ mycasa_scripts_active/scripts_ts10_phangs_ulirg/f00_all_final_eps.sh
@@ -1,4 +1,7 @@
 dir_data="/Users/saito/data/myproj_active/proj_ts10_phangs_ulirgs/eps/"
+dir_product="/Users/saito/data/myproj_active/proj_ts10_phangs_ulirgs/products/"
+rm -rf $dir_product
+mkdir $dir_product
 
 galname1=(eso267 eso297g011 eso297g012 eso507 eso557 ic4518e ic4518w ic5179 iras06592)
 galname2=(irasf10409 irasf17138 mcg02 ngc1614 ngc2369 ngc3110 ngc3256 ngc5257 ngc6240)
@@ -13,4 +16,4 @@ done
 ### figure 2
 convert -crop 1800x1570+200+280 $dir_data"plot_scatter_all.png" $dir_data"plot_scatter_all.png"
 convert -crop 1800x1570+200+280 $dir_data"plot_scatter_env.png" $dir_data"plot_scatter_env.png"
-convert +append -border 0x0 $dir_data"plot_scatter_all.png" $dir_data"plot_scatter_env.png" $dir_data"plot_scatter.png"
+convert +append -border 0x0 $dir_data"plot_scatter_all.png" $dir_data"plot_scatter_env.png" $dir_product"plot_scatter.png"
