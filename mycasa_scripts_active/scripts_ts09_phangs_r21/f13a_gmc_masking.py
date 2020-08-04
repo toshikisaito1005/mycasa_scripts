63adcdf6a6714a8d978b0159e4804e3d2b656192
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-08-04 15:45:00 +0200

    Commit at 2020-08-04 15:45:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f13a_gmc_masking.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f13a_gmc_masking.py
@@ -21,6 +21,7 @@ nbins = 40
 def_nucleus = [50*44./1.0,50*52./1.3,30*103/1.4]
 xlim = [0,2.2]
 beamsizes = [4.0,8.0,4.0]
+galaxy = ["NGC 0628", "NGC 3627", "NGC 4321"]
 
 
 #####################
@@ -247,7 +248,7 @@ for i in range(2):
     ax1.scatter(co21_masks_all[i], r21_masks_all[i], c="grey", alpha=0.2, linewidths=0, s=5, zorder=1)
     H, xedges, yedges = np.histogram2d(r21_masks_all[i],co21_masks_all[i],bins=bins_contour,range=(ylim,xlim))
     extent = [yedges[0],yedges[-1],xedges[0],xedges[-1]]
-    ax1.contour(H/H.max()*100,levels=levels,extent=extent,colors=[colors[i]],zorder=1e9,linewidths=2,alpha=1.0)
+    ax1.contour(H/H.max()*100,levels=levels,extent=extent,colors=[colors[i]],zorder=1e9,linewidths=2,alpha=1.0, label=galaxy[0])
 
 #
 xlim = [-0.3,2.7]
@@ -260,7 +261,7 @@ for i in range(2):
     ax2.scatter(co21_masks_all[i], r21_masks_all[i], c="grey", alpha=0.2, linewidths=0, s=5, zorder=1)
     H, xedges, yedges = np.histogram2d(r21_masks_all[i],co21_masks_all[i],bins=bins_contour,range=(ylim,xlim))
     extent = [yedges[0],yedges[-1],xedges[0],xedges[-1]]
-    ax2.contour(H/H.max()*100,levels=levels,extent=extent,colors=[colors[i]],zorder=1e9,linewidths=2,alpha=1.0)
+    ax2.contour(H/H.max()*100,levels=levels,extent=extent,colors=[colors[i]],zorder=1e9,linewidths=2,alpha=1.0, label=galaxy[1])
 
 #
 xlim = [-0.3,2.7]
@@ -273,9 +274,11 @@ for i in range(2):
     ax3.scatter(co21_masks_all[i], r21_masks_all[i], c="grey", alpha=0.2, linewidths=0, s=5, zorder=1)
     H, xedges, yedges = np.histogram2d(r21_masks_all[i],co21_masks_all[i],bins=bins_contour,range=(ylim,xlim))
     extent = [yedges[0],yedges[-1],xedges[0],xedges[-1]]
-    ax3.contour(H/H.max()*100,levels=levels,extent=extent,colors=[colors[i]],zorder=1e9,linewidths=2,alpha=1.0)
+    ax3.contour(H/H.max()*100,levels=levels,extent=extent,colors=[colors[i]],zorder=1e9,linewidths=2,alpha=1.0, label=galaxy[2])
 
 # save
+ax1.legend()
+plt.legend(loc="upper left")
 plt.savefig(dir_product+"scatter_mask_gmc.png",dpi=200)
 
 os.system("rm -rf *.last")
