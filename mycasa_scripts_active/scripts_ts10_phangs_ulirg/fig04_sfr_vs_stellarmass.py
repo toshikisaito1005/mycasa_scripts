028d20a7bc69b0e3d3f61fb9e953744d8348d4aa
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-08-10 15:20:00 +0200

    Commit at 2020-08-10 15:20:00

--- mycasa_scripts_active/scripts_ts10_phangs_ulirg/fig04_sfr_vs_stellarmass.py
+++ mycasa_scripts_active/scripts_ts10_phangs_ulirg/fig04_sfr_vs_stellarmass.py
@@ -1,6 +1,7 @@
 from astropy.io import ascii
 from astropy.table import QTable
 from astroquery.ned import Ned
+import astropy.units as u
 
 
 #####################
@@ -15,4 +16,8 @@ dir_data = "/Users/saito/data/myproj_active/proj_ts10_phangs_ulirgs/data_other/"
 ### Main Procedure
 #####################
 data = ascii.read(dir_data + "apjaaf21at1_mrt.txt")
-print(data["Name"])
+galnames = data["Name"]
+
+for i in range(len(galnames)):
+	result_table = Ned.query_region(galnames[i], radius=30 * u.arcsec)
+	print(result_table["Object Name"])
