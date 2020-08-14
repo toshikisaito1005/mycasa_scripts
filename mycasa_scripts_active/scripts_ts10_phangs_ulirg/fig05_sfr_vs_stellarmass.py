a863c0d308b5f38c63f8efd2909c3d01c4330d43
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-08-14 19:10:00 +0200

    Commit at 2020-08-14 19:10:00

--- mycasa_scripts_active/scripts_ts10_phangs_ulirg/fig05_sfr_vs_stellarmass.py
+++ mycasa_scripts_active/scripts_ts10_phangs_ulirg/fig05_sfr_vs_stellarmass.py
@@ -41,11 +41,13 @@ plt.rcParams["font.size"] = 10
 plt.rcParams["legend.fontsize"] = 10
 plt.subplots_adjust(bottom=0.15, left=0.15, right=0.95, top=0.95)
 #
-ax1.scatter(lirg_logMstar, lirg_logSFR, c="indianred", s=30, marker="s", linewidths=0, zorder=1e9)
-ax1.scatter(phangs_logMstar, phangs_logSFR, c="skyblue", s=20, marker="o", linewidths=0, zorder=1e9)
+ax1.scatter(lirg_logMstar, lirg_logSFR, c="indianred", s=20, marker="s", lw=1, zorder=1e9, label="(U)LIRGs")
+ax1.scatter(phangs_logMstar, phangs_logSFR, c="skyblue", s=10, marker="o", lw=1, zorder=1e9, label="PHANGS")
 #
 ax1.plot([10**9,10**11.7], [0.14125375446227556,9.6827785626124676], "--", lw=2, c="grey")
+ax1.text(10**np.log10(10**11.2),2,"z=0 MS")
 ax1.plot([10**9,10**11.7], [0.14125375446227556*10,9.6827785626124676*10], "--", lw=2, c="grey")
+
 #
 ax1.set_xlim([10**9,10**11.7])
 ax1.set_ylim([10**-1.8,10**2.2])
@@ -55,5 +57,6 @@ plt.xticks([10**9,10**10,10**11],[9,10,11])
 plt.yticks([10**-1,10**0,10**1,10**2],[-1,0,1,2])
 plt.xlabel(r"log $M_{\star}$ ($M_{\odot}$)")
 plt.ylabel(r"log SFR ($M_{\odot}$ yr$^{-1}$)")
+plt.legend(loc="upper left")
 plt.savefig(dir_eps+"plot_sfr_vs_mstar.png",dpi=200)
 
