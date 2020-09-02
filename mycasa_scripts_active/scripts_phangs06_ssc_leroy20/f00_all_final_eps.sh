47a4578697d9e7fb893b0137d1a919acb4221223
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-09-02 18:05:01 +0200

    Commit at 2020-09-02 18:05:01

--- mycasa_scripts_active/scripts_phangs06_ssc_leroy20/f00_all_final_eps.sh
+++ mycasa_scripts_active/scripts_phangs06_ssc_leroy20/f00_all_final_eps.sh
@@ -2,6 +2,8 @@ dir_data="/Users/saito/data/myproj_active/proj_phangs06_ssc/eps/"
 
 
 ### figure
+convert -crop 300x345+25+0 $dir_data"ngc0628_skymodel.png" $dir_data"ngc0628_skymodelb.png"
+
 convert +append -border 0x0 $dir_data"ngc0628_skymodel.png" $dir_data"ngc0628_7m.png" $dir_data"fig_ssc_01a.png"
 convert +append -border 0x0 $dir_data"ngc0628_tp2vis.png" $dir_data"ngc0628_tpmodel.png" $dir_data"ngc0628_feather.png" $dir_data"fig_ssc_01b.png"
 convert -append -border 0x0 $dir_data"fig_ssc_01a.png" $dir_data"fig_ssc_01b.png" $dir_data"ssc_image_ngc0628.png"
--- mycasa_scripts_active/scripts_phangs06_ssc_leroy20/f01_images.py
+++ mycasa_scripts_active/scripts_phangs06_ssc_leroy20/f01_images.py
@@ -10,7 +10,7 @@ plt.ioff()
 reload(myim)
 
 
-galname = "ngc4303" # "ngc0628" "ngc1097"
+galname = "ngc1097" # "ngc0628" "ngc1097"
 dir_data = "/Users/saito/data/myproj_active/proj_phangs06_ssc/sim_phangs/sim_" + galname + "/"
 
 
@@ -31,33 +31,31 @@ for i in range(len(imagenames)):
 ### Main Procedure
 #####################
 ### common
+if galname=="ngc0628":
+       # ngc0628
+       ra_center = "01:36:41.8"
+       dec_center = "15.47.0.0"
+       clim = [0, 3.93731]
+       contour = np.array([0.15, 0.35, 0.55, 0.75, 0.95])
+       xlim = [-150, 150]
+       ylim = [150, -150]
+elif galname=="ngc1097":
+       # ngc1097
+       ra_center = "02:46:19.088"
+       dec_center = "-30.16.30.099"
+       clim = [0, 10]
+       contour = np.array([0.02,0.04,0.08,0.16,0.32,0.64,0.96])
+       xlim = [-135, 135]
+       ylim = [135, -135]
+elif galname=="ngc4303":
+       # ngc4303
+       ra_center = "12:21:54.934"
+       dec_center = "4.28.24.497"
+       clim = [0, 5.0]
+       contour = np.array([0.02,0.04,0.08,0.16,0.32,0.64,0.96])
+       xlim = [-84, 84]
+       ylim = [85, -83]
 
-# ngc0628
-ra_center = "01:36:41.8"
-dec_center = "15.47.0.0"
-clim = [0, 3.93731]
-contour = np.array([0.15, 0.35, 0.55, 0.75, 0.95])
-xlim = [-150, 150]
-ylim = [150, -150]
-
-"""
-# ngc1097
-ra_center = "02:46:19.088"
-dec_center = "-30.16.30.099"
-clim = [0, 10]
-contour = np.array([0.02,0.04,0.08,0.16,0.32,0.64,0.96])
-xlim = [-135, 135]
-ylim = [135, -135]
-"""
-"""
-# ngc4303
-ra_center = "12:21:54.934"
-dec_center = "4.28.24.497"
-clim = [0, 5.0]
-contour = np.array([0.02,0.04,0.08,0.16,0.32,0.64,0.96])
-xlim = [-84, 84]
-ylim = [85, -83]
-"""
 ###
 value = None
 color_contour = "black"
