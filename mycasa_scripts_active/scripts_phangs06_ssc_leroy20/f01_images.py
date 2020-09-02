4f2cc2fd7b0059c547231345cd4af3bf9af4e3c7
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-09-02 17:55:00 +0200

    Commit at 2020-09-02 17:55:00

--- mycasa_scripts_active/scripts_phangs06_ssc_leroy20/f01_images.py
+++ mycasa_scripts_active/scripts_phangs06_ssc_leroy20/f01_images.py
@@ -31,7 +31,7 @@ for i in range(len(imagenames)):
 ### Main Procedure
 #####################
 ### common
-"""
+
 # ngc0628
 ra_center = "01:36:41.8"
 dec_center = "15.47.0.0"
@@ -39,7 +39,7 @@ clim = [0, 3.93731]
 contour = np.array([0.15, 0.35, 0.55, 0.75, 0.95])
 xlim = [-150, 150]
 ylim = [150, -150]
-"""
+
 """
 # ngc1097
 ra_center = "02:46:19.088"
@@ -49,7 +49,7 @@ contour = np.array([0.02,0.04,0.08,0.16,0.32,0.64,0.96])
 xlim = [-135, 135]
 ylim = [135, -135]
 """
-
+"""
 # ngc4303
 ra_center = "12:21:54.934"
 dec_center = "4.28.24.497"
@@ -57,7 +57,7 @@ clim = [0, 5.0]
 contour = np.array([0.02,0.04,0.08,0.16,0.32,0.64,0.96])
 xlim = [-84, 84]
 ylim = [85, -83]
-
+"""
 ###
 value = None
 color_contour = "black"
