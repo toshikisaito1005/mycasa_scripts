a498dfdd9b056d94b80cf82d62c03bd7fc37925e
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-09-07 11:50:00 +0200

    Commit at 2020-09-07 11:50:00

--- mycasa_scripts_active/scripts_observations/azel.py
+++ mycasa_scripts_active/scripts_observations/azel.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import astropy.units as u
 from astropy.time import Time
 from astropy.coordinates import SkyCoord, EarthLocation, AltAz
+from astropy.coordinates import get_moon
 plt.ioff()
 
 
@@ -18,8 +19,32 @@ arp220 = SkyCoord.from_name('Arp220')
 #
 midnight = Time('2021-02-01 00:00:00') - utcoffset
 delta_midnight = np.linspace(-2, 10, 100)*u.hour
-frame_feb = AltAz(obstime=midnight+delta_midnight, location=subaru)
-altazs_arp220_feb = arp220.transform_to(frame_feb)
-altazs_arp220_feb = altazs_arp220_feb.secz
+#frame_feb = AltAz(obstime=midnight+delta_midnight, location=subaru)
+#altazs_arp220_feb = arp220.transform_to(frame_feb)
+#altazs_arp220_feb = altazs_arp220_feb.secz
+
+# set Moon
+times_feb = midnight + delta_midnight
+times_feb = get_moon(times_feb)
+moonaltazs_feb = times_feb.transform_to(times_feb)
+
+arp220_altazs_feb = arp220.transform_to(times_feb)
 
 # plot
+plt.figure()
+plt.plot(delta_midnight, moonaltazs_feb.alt, color=[0.75]*3, ls='--', label='Moon')
+plt.scatter(delta_midnight, arp220_altazs_feb.alt,
+            c=arp220_altazs_feb.az, label='Arp220', lw=0, s=8,
+            cmap='viridis')
+plt.fill_between(delta_midnight, 0*u.deg, 90*u.deg,
+                 sunaltazs_July12_to_13.alt < -0*u.deg, color='0.5', zorder=0)
+plt.fill_between(delta_midnight, 0*u.deg, 90*u.deg,
+                 sunaltazs_July12_to_13.alt < -18*u.deg, color='k', zorder=0)
+plt.colorbar().set_label('Azimuth [deg]')
+plt.legend(loc='upper left')
+plt.xlim(-12*u.hour, 12*u.hour)
+plt.xticks((np.arange(13)*2-12)*u.hour)
+plt.ylim(0*u.deg, 90*u.deg)
+plt.xlabel('Hours from EDT Midnight')
+plt.ylabel('Altitude [deg]')
+plt.show()
