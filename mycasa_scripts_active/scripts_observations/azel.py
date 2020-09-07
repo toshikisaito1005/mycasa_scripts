246dbb90acd2e6574942ba3eca56bf1378f5e4a2
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-09-07 11:40:01 +0200

    Commit at 2020-09-07 11:40:01

--- mycasa_scripts_active/scripts_observations/azel.py
+++ mycasa_scripts_active/scripts_observations/azel.py
@@ -4,8 +4,15 @@ import astropy.units as u
 from astropy.time import Time
 from astropy.coordinates import SkyCoord, EarthLocation, AltAz
 
-Arp220 = SkyCoord.from_name('Arp220')
+# set Subaru information
+subaru = EarthLocation.from_geodetic(-155.4761*u.deg, 19.825*u.deg,4139*u.m)
+utcoffset = -10*u.hour  # Hawaii
+time = Time('2021-02-01 23:00:00') - utcoffset
 
-bear_mountain = EarthLocation(lat=41.3*u.deg, lon=-74*u.deg, height=390*u.m)
-utcoffset = -4*u.hour  # Eastern Daylight Time
-time = Time('2012-7-12 23:00:00') - utcoffset
+# set target information
+arp220 = SkyCoord.from_name('Arp220')
+
+#altaz_arp220 = arp220.transform_to(AltAz(obstime=time,location=subaru))
+#print("Arp220's Altitude = {0.alt:.2}".format(altaz_arp220))
+
+midnight = Time('2021-02-01 00:00:00') - utcoffset
