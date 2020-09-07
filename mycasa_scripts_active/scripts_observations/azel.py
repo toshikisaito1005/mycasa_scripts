449e51afd74e30c3870089464717e076d6a1e0c1
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-09-07 12:05:00 +0200

    Commit at 2020-09-07 12:05:00

--- mycasa_scripts_active/scripts_observations/azel.py
+++ mycasa_scripts_active/scripts_observations/azel.py
@@ -20,18 +20,16 @@ arp220 = SkyCoord.from_name('Arp220')
 #
 midnight = Time('2021-02-01 00:00:00') - utcoffset
 delta_midnight = np.linspace(-12, 12, 1000)*u.hour
-#frame_feb = AltAz(obstime=midnight+delta_midnight, location=subaru)
-#altazs_arp220_feb = arp220.transform_to(frame_feb)
-#altazs_arp220_feb = altazs_arp220_feb.secz
 
 # set Sun
 times_feb = midnight + delta_midnight
 frame_feb = AltAz(obstime=times_feb, location=subaru)
 sunaltazs_feb = get_sun(times_feb).transform_to(frame_feb)
 # set Moon
-moonaltazs_feb = get_moon(times_feb).transform_to(frame_feb) # times_feb.transform_to(times_feb)
+moon_feb = get_moon(times_feb)
+moonaltazs_feb = moon_feb.transform_to(frame_feb)
 #
-arp220_altazs_feb = arp220.transform_to(times_feb)
+arp220_altazs_feb = arp220.transform_to(frame_feb)
 
 # plot
 plt.figure()
@@ -40,15 +38,15 @@ plt.plot(delta_midnight, moonaltazs_feb.alt, color=[0.75]*3, ls='--', label='Moo
 plt.scatter(delta_midnight, arp220_altazs_feb.alt,
             c=arp220_altazs_feb.az, label='Arp220', lw=0, s=8,
             cmap='viridis')
-plt.fill_between(delta_midnight, 0*u.deg, 90*u.deg,
-                 sunaltazs_feb.alt < -0*u.deg, color='0.5', zorder=0)
-plt.fill_between(delta_midnight, 0*u.deg, 90*u.deg,
-                 sunaltazs_feb.alt < -18*u.deg, color='k', zorder=0)
+#plt.fill_between(delta_midnight, 0*u.deg, 90*u.deg,
+#                 sunaltazs_feb.alt < -0*u.deg, color='0.5', zorder=0)
+#plt.fill_between(delta_midnight, 0*u.deg, 90*u.deg,
+#                 sunaltazs_feb.alt < -18*u.deg, color='k', zorder=0)
 plt.colorbar().set_label('Azimuth [deg]')
 plt.legend(loc='upper left')
-plt.xlim(-12*u.hour, 12*u.hour)
-plt.xticks((np.arange(13)*2-12)*u.hour)
-plt.ylim(0*u.deg, 90*u.deg)
+plt.xlim([-12, 12])
+plt.xticks((np.arange(13)*2-12))
+plt.ylim([0, 90])
 plt.xlabel('Hours from Hawaii Midnight')
 plt.ylabel('Altitude [deg]')
 plt.savefig("test.png")
