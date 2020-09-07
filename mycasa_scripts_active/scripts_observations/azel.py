74ab45d574de84f3471faa4278e873469cbded84
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-09-07 11:45:00 +0200

    Commit at 2020-09-07 11:45:00

--- mycasa_scripts_active/scripts_observations/azel.py
+++ mycasa_scripts_active/scripts_observations/azel.py
@@ -3,16 +3,23 @@ import matplotlib.pyplot as plt
 import astropy.units as u
 from astropy.time import Time
 from astropy.coordinates import SkyCoord, EarthLocation, AltAz
+plt.ioff()
+
 
 # set Subaru information
 subaru = EarthLocation.from_geodetic(-155.4761*u.deg, 19.825*u.deg,4139*u.m)
 utcoffset = -10*u.hour  # Hawaii
 time = Time('2021-02-01 23:00:00') - utcoffset
 
+
 # set target information
 arp220 = SkyCoord.from_name('Arp220')
 
-#altaz_arp220 = arp220.transform_to(AltAz(obstime=time,location=subaru))
-#print("Arp220's Altitude = {0.alt:.2}".format(altaz_arp220))
-
+#
 midnight = Time('2021-02-01 00:00:00') - utcoffset
+delta_midnight = np.linspace(-2, 10, 100)*u.hour
+frame_feb = AltAz(obstime=midnight+delta_midnight, location=subaru)
+altazs_arp220_feb = arp220.transform_to(frame_feb)
+altazs_arp220_feb = altazs_arp220_feb.secz
+
+# plot
