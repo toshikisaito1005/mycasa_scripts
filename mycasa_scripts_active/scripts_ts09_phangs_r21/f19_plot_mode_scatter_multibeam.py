cb40bffc0e412e0db9c48dbe7f539350094d7dc5
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-09-21 15:25:00 +0200

    Commit at 2020-09-21 15:25:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f19_plot_mode_scatter_multibeam.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f19_plot_mode_scatter_multibeam.py
@@ -17,17 +17,34 @@ dir_proj = "/Users/saito/data/myproj_active/proj_ts09_phangs_r21/"
 galname = "ngc0628"
 freqco10 = 115.27120
 freqco21 = 230.53800
-nbins = [40, 30, 20, 10, 10]
 percentile = 84
 
+nbins_n0628 = [40, 30, 20, 10, 10]
 beams = ["04p0","08p0","12p0","16p0","20p0"]
 
 
 #####################
 ### functions
 #####################
+def get_best_params(
+	txtfile,
+	):
+	"""
+	"""
+	data = np.loadtxt(txtfile)
+	best = np..median(data, axis=0)
 
 
 #####################
-### functions
+### main
 #####################
+##
+txt_n0628_co10 = glob.glob(dir_proj + "eps/bootstrap_co10_models_ngc0628_??p0.txt")
+txt_n0628_co21 = glob.glob(dir_proj + "eps/bootstrap_co21_models_ngc0628_??p0.txt")
+txt_n3627_co10 = glob.glob(dir_proj + "eps/bootstrap_co10_models_ngc3627_??p0.txt")
+txt_n3627_co21 = glob.glob(dir_proj + "eps/bootstrap_co21_models_ngc3627_??p0.txt")
+txt_n4321_co10 = glob.glob(dir_proj + "eps/bootstrap_co10_models_ngc4321_??p0.txt")
+txt_n4321_co21 = glob.glob(dir_proj + "eps/bootstrap_co21_models_ngc4321_??p0.txt")
+
+##
+for i in range(len(nbins_n0628)):
