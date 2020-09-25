507cefe7d906149e677d7618f41bb80a90b87682
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-09-25 20:35:01 +0200

    Commit at 2020-09-25 20:35:01

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f10b_disp.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f10b_disp.py
@@ -198,19 +198,19 @@ for i in range(len(gals)):
 		label2=None
 		label3=None
 	#
-	norm_disp = (np.log10(np.array(statslist_r21)[:,0])-np.log10(np.array(statslist_r21)[:,4])) / (np.log10(np.array(statslist_r21)[:,0][0])-np.log10(np.array(statslist_r21)[:,4][0]))
+	norm_disp = ((np.array(statslist_r21)[:,0])-(np.array(statslist_r21)[:,4])) / ((np.array(statslist_r21)[:,0][0])-(np.array(statslist_r21)[:,4][0]))
 	bars = ax.plot(list_beam, norm_disp, "o-", color=color, alpha=0.6, lw=2, label=label1)
 	[bar.set_alpha(0.6) for bar in bars]
 	#
-	norm_disp = (np.log10(np.array(statslist_r21_wco10)[:,0])-np.log10(np.array(statslist_r21_wco10)[:,4])) / (np.log10(np.array(statslist_r21_wco10)[:,0][0])-np.log10(np.array(statslist_r21_wco10)[:,4][0]))
+	norm_disp = ((np.array(statslist_r21_wco10)[:,0])-(np.array(statslist_r21_wco10)[:,4])) / ((np.array(statslist_r21_wco10)[:,0][0])-(np.array(statslist_r21_wco10)[:,4][0]))
 	bars = ax.plot(list_beam, norm_disp, "o-", color=color, alpha=0.6, lw=2, label=label2)
 	[bar.set_alpha(0.4) for bar in bars]
 	#
-	norm_disp = (np.log10(np.array(statslist_r21_wco21)[:,0])-np.log10(np.array(statslist_r21_wco21)[:,4])) / (np.log10(np.array(statslist_r21_wco21)[:,0][0])-np.log10(np.array(statslist_r21_wco21)[:,4][0]))
+	norm_disp = ((np.array(statslist_r21_wco21)[:,0])-(np.array(statslist_r21_wco21)[:,4])) / ((np.array(statslist_r21_wco21)[:,0][0])-(np.array(statslist_r21_wco21)[:,4][0]))
 	bars = ax.plot(list_beam, norm_disp, "o-", color=color, alpha=0.6, lw=2, label=label3)
 	[bar.set_alpha(0.2) for bar in bars]
 
-	ax.set_ylim([0.3,1.2])
+	ax.set_ylim([0.3,1.4])
 	if i==1:
 		ax.set_xlim([6.0,35.0])
 	else:
