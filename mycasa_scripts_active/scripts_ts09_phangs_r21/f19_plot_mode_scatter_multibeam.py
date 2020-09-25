80743cb775646e8cca1ef78b9d0a084a5e5b5ad9
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-09-25 18:45:00 +0200

    Commit at 2020-09-25 18:45:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f19_plot_mode_scatter_multibeam.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f19_plot_mode_scatter_multibeam.py
@@ -330,6 +330,8 @@ for i in range(len(nbins_n0628)):
 	#
 	log_co10_mom0_k, log_co10_noise_k, log_co21_mom0_k, log_co21_noise_k = getdata(co10_mom0, co10_noise, co21_mom0, co21_noise, freqco10, freqco21, pa, inc, cnt_ra, cnt_dec, scale, def_nucleus)
 	xbins_co10, xbins_co21 = plotter_noise(dir_proj, log_co10_mom0_k, log_co10_noise_k, log_co21_mom0_k, log_co21_noise_k, nbins_n0628[i], percentile, galname)
+	#
+	for j in range(100):
 	log_co10_mom0_k_model, log_co10_mom0_k_model_scatter, log_co10_mom0_k_model_scatter_noise, log_co21_mom0_k_model, log_co21_mom0_k_model_scatter, log_co21_mom0_k_model_scatter_noise = \
 		create_best_models(log_co10_mom0_k, log_co21_mom0_k, log_co10_noise_k, log_co21_noise_k, xbins_co10, xbins_co21, n0628_co10_best_params, n0628_co21_best_params)
 	r21 = 10**log_co21_mom0_k_model_scatter/10**log_co10_mom0_k_model_scatter
