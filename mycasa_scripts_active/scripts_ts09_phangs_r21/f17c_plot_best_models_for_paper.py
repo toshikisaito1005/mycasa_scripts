f24a531440a3753cd2ddcd153082d43840c2c7a4
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-09-28 16:25:00 +0200

    Commit at 2020-09-28 16:25:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/f17c_plot_best_models_for_paper.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/f17c_plot_best_models_for_paper.py
@@ -19,17 +19,14 @@ dir_proj = "/Users/saito/data/myproj_active/proj_ts09_phangs_r21/"
 #####################
 ### main
 #####################
-#np.savetxt(dir_proj + "eps/"+galname+"_model_"+beamstr+"_scatter.txt", np.c_[log_co10_mom0_k_model_scatter, log_co21_mom0_k_model_scatter])
-#np.savetxt(dir_proj + "eps/"+galname+"_model_"+beamstr+"_scatter_noise.txt", np.c_[log_co10_mom0_k_model_scatter_noise, log_co21_mom0_k_model_scatter_noise])
-np.savetxt(dir_proj + "eps/"+galname+"_model_"+beamstr+"_scatter_cut.txt", np.c_[log_co10_mom0_k_model_scatter_cut, log_co21_mom0_k_model_scatter_cut])
-#np.savetxt(dir_proj + "eps/"+galname+"_model_"+beamstr+"_scatter_noise_cut.txt", np.c_[log_co10_mom0_k_model_scatter_noise_cut, log_co21_mom0_k_model_scatter_noise_cut])
-
-### plot co21 vs r21
-r21 = np.log10(10**log_co21_mom0_k/10**log_co10_mom0_k)
-r21_model = np.log10(10**log_co21_mom0_k_model/10**log_co10_mom0_k_model)
-r21_model_scatter = np.log10(10**log_co21_mom0_k_model_scatter_cut/10**log_co10_mom0_k_model_scatter_cut)
-r21_model_scatter_noise = np.log10(10**log_co21_mom0_k_model_scatter_noise_cut/10**log_co10_mom0_k_model_scatter_noise_cut)
+###
+data_high = np.loadtxt(dir_proj + "eps/ngc0628_model_04p0_scatter_cut.txt")
+data_low = np.loadtxt(dir_proj + "eps/ngc0628_model_20p0_scatter_cut.txt")
+xlim = [-0.5,1.8] # [-0.6,1.8], [-0.1,2.3], [-0.5,1.8]
+r21_high = np.log10(10**data_high[:,0]/10**data_high[:,1])
+r21_low = np.log10(10**data_low[:,0]/10**data_low[:,1])
 #
+### plot co21 vs r21
 figure = plt.figure(figsize=(10,10))
 gs = gridspec.GridSpec(nrows=8, ncols=8)
 plt.subplots_adjust(bottom=0.10, left=0.15, right=0.98, top=0.95)
@@ -40,9 +37,6 @@ ax1.set_ylabel("log $R_{21}$")
 plt.rcParams["font.size"] = 20
 plt.rcParams["legend.fontsize"] = 18
 #
-binx, mean, std = get_binned_dist(log_co21_mom0_k_model_scatter_noise_cut, r21_model_scatter_noise, range_co21_input)
-ax1.errorbar(binx, mean, yerr = std, color = "dimgrey", ecolor = "dimgrey", lw=4)
-#
 # ax1
 #ax1.plot(log_co21_mom0_k_model, r21_model, "o", color="black", alpha=1.0, markersize=5, markeredgewidth=0, zorder=-1e18)
 ax1.plot(log_co21_mom0_k_model_scatter_cut, r21_model_scatter, "o", color="blue", alpha=0.5, markersize=5, markeredgewidth=0, zorder=-1e20)
@@ -51,5 +45,5 @@ ax1.plot(log_co21_mom0_k_model_scatter_cut, r21_model_scatter, "o", color="blue"
 ax1.set_xlim(xlim)
 ax1.set_ylim([-1.2,0.5])
 #
-plt.savefig(dir_proj + "eps/fig_obs_vs_model_r21_"+galname+"_"+beamstr+".png",dpi=200)
+plt.savefig(dir_proj + "eps/fig_obs_vs_model_r21_ngc0628.png",dpi=200)
 #
