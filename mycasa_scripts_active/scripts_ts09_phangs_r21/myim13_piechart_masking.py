8ccd92d3334321e7fa09a0380f462e2d1b21f208
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-09-30 15:00:00 +0200

    Commit at 2020-09-30 15:00:00

--- mycasa_scripts_active/scripts_ts09_phangs_r21/myim13_piechart_masking.py
+++ mycasa_scripts_active/scripts_ts09_phangs_r21/myim13_piechart_masking.py
@@ -12,15 +12,27 @@ from astropy.coordinates import SkyCoord
 dir_fits = "/Users/saito/data/myproj_active/proj_ts09_phangs_r21/data_other/mask_piechart/"
 dir_product = "/Users/saito/data/myproj_active/proj_ts09_phangs_r21/eps/"
 spiral_fits = ["ngc0628_HASUB_astro_sqr_HII.SNHoopes_commonfov_04arcsec_clipped.image",
-				       "ngc3627_HASUB_astro_sqr_HII.SNHoopes_commonfov_08arcsec_clipped.image",
-				       "ngc4321_HASUB_astro_sqr_HII.SNHoopes_commonfov_04arcsec_clipped.image"]
+               "ngc3627_HASUB_astro_sqr_HII.SNHoopes_commonfov_08arcsec_clipped.image",
+               "ngc4321_HASUB_astro_sqr_HII.SNHoopes_commonfov_04arcsec_clipped.image",
+               "ngc0628_HASUB_astro_sqr_HII.SNHoopes_commonfov_20arcsec_clipped.image",
+               "ngc3627_HASUB_astro_sqr_HII.SNHoopes_commonfov_24arcsec_clipped.image",
+               "ngc4321_HASUB_astro_sqr_HII.SNHoopes_commonfov_20arcsec_clipped.image"]
 mom0_fits = ["../../ngc0628_r21/r21_04p0.moment0",
-			 "../../ngc3627_r21/r21_08p0.moment0",
-			 "../../ngc4321_r21/r21_04p0.moment0"]
+       "../../ngc3627_r21/r21_08p0.moment0",
+       "../../ngc4321_r21/r21_04p0.moment0",
+       "../../ngc0628_r21/r21_20p0.moment0",
+       "../../ngc3627_r21/r21_24p0.moment0",
+       "../../ngc4321_r21/r21_20p0.moment0"]
 output = ["../../ngc0628_r21/piechart_04p0.mask.fits",
-		  "../../ngc3627_r21/piechart_08p0.mask.fits",
-		  "../../ngc4321_r21/piechart_04p0.mask.fits"]
+      "../../ngc3627_r21/piechart_08p0.mask.fits",
+      "../../ngc4321_r21/piechart_04p0.mask.fits",
+      "../../ngc0628_r21/piechart_20p0.mask.fits",
+      "../../ngc3627_r21/piechart_24p0.mask.fits",
+      "../../ngc4321_r21/piechart_20p0.mask.fits"]
 expr = ["iif(IM0>0,1,0)",
+        "iif(IM0>0,1,0)",
+        "iif(IM0>0,1,0)",
+        "iif(IM0>0,1,0)",
         "iif(IM0>0,1,0)",
         "iif(IM0>0,1,0)"]
 
