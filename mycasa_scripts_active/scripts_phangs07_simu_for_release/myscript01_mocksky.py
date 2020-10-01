3a8248298309fe4500aa6a7dff52128dccec72a3
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-10-01 16:20:00 +0200

    Commit at 2020-10-01 16:20:00

--- mycasa_scripts_active/scripts_phangs07_simu_for_release/myscript01_mocksky.py
+++ mycasa_scripts_active/scripts_phangs07_simu_for_release/myscript01_mocksky.py
@@ -23,57 +23,40 @@ os.system("mkdir " + this_project)
 ###
 #
 imsize = 512
-direction_center = "ICRS 12h21m54.947s 4d28m15.258s"
-
-direction_left = "ICRS 12h21m54.947s 4d28m15.258s"
-direction_right = "ICRS 12h21m54.947s 4d28m15.258s"
-
-
 imagename = this_project + "simulated_sky.image"
+direction_center = "ICRS 12h21m54.947s 4d28m15.258s"
+direction_left   = "ICRS 12h21m55.500s 4d28m15.258s"
+direction_right  = "ICRS 12h21m54.800s 4d28m15.258s"
 #
 os.system("rm -rf " + imagename)
 os.system("rm -rf " + imagename.replace(".image",".fits"))
 #
 cl.done()
-cl.addcomponent(dir = direction,
-				flux = 0.1 / 67. * float(imsize**2),
+cl.addcomponent(dir = direction_left,
+				flux = 0.5,
 				fluxunit = "Jy",
 				freq = "230.53800GHz",
-				shape = "Gaussian", 
-				        majoraxis = "5arcsec",
-				minoraxis = "5arcsec",
-				positionangle = "45.0deg")
-
-
-cl.addcomponent(dir = direction,
-                flux = 0.5 / 67. * float(imsize**2),
-	    fluxunit = "Jy",
-	    freq = "230.53800GHz",
-	    shape = "Gaussian", 
-                majoraxis = "30arcsec",
-	    minoraxis = "10arcsec",
-	    positionangle = "45.0deg")
-
-cl.addcomponent(dir = direction,
-                flux = 2.5 / 67. * float(imsize**2),
-	    fluxunit = "Jy",
-	    freq = "230.53800GHz",
-	    shape = "Gaussian", 
-                majoraxis = "120arcsec",
-	    minoraxis = "40arcsec",
-	    positionangle = "45.0deg")
-
-
+				shape = "point")
+cl.addcomponent(dir = direction_center,
+				flux = 0.05,
+				fluxunit = "Jy",
+				freq = "230.53800GHz",
+				shape = "point")
+cl.addcomponent(dir = direction_right,
+				flux = 0.05,
+				fluxunit = "Jy",
+				freq = "230.53800GHz",
+				shape = "point")
 ia.fromshape(imagename,[imsize,imsize,1,1],overwrite=True)
 cs=ia.coordsys()
 cs.setunits(['rad','rad','','Hz'])
-cell_rad=qa.convert(qa.quantity("1.0arcsec"),"rad")['value']
+cell_rad=qa.convert(qa.quantity("0.25arcsec"),"rad")['value']
 cs.setincrement([-cell_rad,cell_rad],'direction')
 cs.setreferencevalue([qa.convert("185.47894583deg",'rad')['value'],
                       qa.convert("4.47090500deg",'rad')['value']],
                      type="direction")
 cs.setreferencevalue("230GHz",'spectral')
-cs.setincrement('1GHz','spectral')
+cs.setincrement('0.5GHz','spectral')
 ia.setcoordsys(cs.torecord())
 ia.setbrightnessunit("Jy/pixel")
 ia.modify(cl.torecord(),subtract=False)
