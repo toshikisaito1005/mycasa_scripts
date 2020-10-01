3661aca4b66fe5d8d15d765ef5e73d19e289ba8a
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-10-01 16:05:00 +0200

    Commit at 2020-10-01 16:05:00

--- mycasa_scripts_active/scripts_phangs06_ssc_leroy20/myim01_makemodel.py
+++ mycasa_scripts_active/scripts_phangs06_ssc_leroy20/myim01_makemodel.py
@@ -5,7 +5,7 @@ import numpy as np
 from astropy.coordinates import SkyCoord
 
 
-dir_project = "/Users/saito/data/myproj_active/proj_phangs06_ssc/"
+dir_project = "/Users/saito/data/myproj_active/proj_phangs07_simu_for_release/"
 tpbeam = "28.6arcsec"
 
 
---
+++ mycasa_scripts_active/scripts_phangs07_simu_for_release/myscript01_mocksky.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import glob
+import numpy as np
+from astropy.coordinates import SkyCoord
+
+
+dir_project = "/Users/saito/data/myproj_active/proj_phangs07_simu_for_release/"
+tpbeam = "28.6arcsec"
+
+
+##############################
+### main
+##############################
