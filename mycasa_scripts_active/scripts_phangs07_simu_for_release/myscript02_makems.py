8cd63406f2a602a55aea26ff3bf35613a3b04ee5
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-10-01 22:00:00 +0200

    Commit at 2020-10-01 22:00:00

--- mycasa_scripts_active/scripts_phangs07_simu_for_release/myscript02_makems.py
+++ mycasa_scripts_active/scripts_phangs07_simu_for_release/myscript02_makems.py
@@ -5,7 +5,6 @@ import pyfits
 import shutil
 
 dir_project = "/Users/saito/data/myproj_active/proj_phangs07_simu_for_release/"
-image_mocksky = "simulated_sky.fits"
 
 ##############################
 ### def
--- mycasa_scripts_active/scripts_phangs07_simu_for_release/run_scripts.py
+++ mycasa_scripts_active/scripts_phangs07_simu_for_release/run_scripts.py
@@ -5,8 +5,10 @@
 this_proj = "sim01"
 imsize = 512
 this_fov = 120
+mapsize=["",""]
 #
 execfile("myscript01_mocksky.py")
+image_mocksky = "simulated_sky.fits"
 execfile("myscript02_makems.py")
 execfile("myscript03_dirtymap_mfs.py")
 """
@@ -17,18 +19,31 @@ execfile("myscript03_dirtymap_mfs.py")
 this_proj = "sim02"
 imsize = 45
 this_fov = 30
+mapsize=["",""]
 #
 execfile("myscript01_mocksky.py")
+image_mocksky = "simulated_sky.fits"
 execfile("myscript02_makems.py")
 execfile("myscript03_dirtymap_mfs.py")
 """
 
-#"""
+"""
+# done
 ### two point sources, cube, single pointing
 this_proj = "sim03"
 imsize = 45
 this_fov = 30
+mapsize=["",""]
+#
 #execfile("myscript01_mocksky.py")
+image_mocksky = "simulated_sky.fits"
 #execfile("myscript02_makems.py")
 execfile("myscript04_dirtymap_cube.py")
-#"""
+"""
+
+this_proj = "sim04"
+imsize = 512
+this_fov = 120
+mapsize=["40arcsec","40arcsec"]
+image_mocksky = "ngc1097_12m+7m+tp_co21_pbcorr_trimmed_k.fits"
+execfile("myscript02_makems.py")
