9d91bad9c9f8adb315a013f5b0654571cd578fbb
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-10-01 19:00:00 +0200

    Commit at 2020-10-01 19:00:00

--- mycasa_scripts_active/scripts_phangs07_simu_for_release/myscript03_dirtymap.py
+++ mycasa_scripts_active/scripts_phangs07_simu_for_release/myscript03_dirtymap.py
@@ -50,7 +50,7 @@ def dirty_continuum(
 		deconvolver = "mtmfs",
 		usemask     = "pb",
 		pbmask      = 0.8,
-		restoration = False,
+		restoration = True,
 		startmodel  = "",
 		pblimit     = 0.5,
 		)
@@ -59,13 +59,9 @@ def dirty_continuum(
 ##############################
 ### main
 ##############################
-# concat
-print("### concat")
+#
 vis_12m = glob.glob(dir_project + this_proj + "/" + this_proj + "_12m.ms")[0]
 vis_7m = glob.glob(dir_project + this_proj + "/" + this_proj + "_7m.ms")[0]
-vis_12m7m = vis_12m.replace("12m","12m+7m")
-os.system("rm -rf " + vis_12m7m)
-concat(vis=[vis_12m,vis_7m], concatvis=vis_12m7m)
 
 # 7m-only dirty map
 print("### dirty map creation: 7m")
@@ -73,7 +69,7 @@ imagename = "image_sim01_7m"
 os.system("rm -rf " + imagename + "*")
 os.system("rm -rf " + dir_project + this_proj + "/" + imagename + "*")
 dirty_continuum(vis_7m, imagename, fov=120, cell="1.0arcsec", phasecenter=phasecenter)
-os.system("mv " + imagename + " " + dir_project + this_proj)
+os.system("mv " + imagename + "* " + dir_project + this_proj)
 
 # 12m-only dirty map
 print("### dirty map creation: 12m")
@@ -81,7 +77,13 @@ imagename = "image_sim01_12m"
 os.system("rm -rf " + imagename + "*")
 os.system("rm -rf " + dir_project + this_proj + "/" + imagename + "*")
 dirty_continuum(vis_12m, imagename, fov=120, cell="0.4arcsec", phasecenter=phasecenter)
-os.system("mv " + imagename + " " + dir_project + this_proj)
+os.system("mv " + imagename + "* " + dir_project + this_proj)
+
+# concat
+print("### concat")
+vis_12m7m = vis_12m.replace("12m","12m+7m")
+os.system("rm -rf " + vis_12m7m)
+concat(vis=[vis_12m,vis_7m], concatvis=vis_12m7m)
 
 # 12m+7m dirty map
 print("### dirty map creation: 12m+7m")
@@ -89,7 +91,7 @@ imagename = "image_sim01_12+7m"
 os.system("rm -rf " + imagename + "*")
 os.system("rm -rf " + dir_project + this_proj + "/" + imagename + "*")
 dirty_continuum(vis_12m7m, imagename, fov=120, cell="0.4arcsec", phasecenter=phasecenter)
-os.system("mv " + imagename + " " + dir_project + this_proj)
+os.system("mv " + imagename + "* " + dir_project + this_proj)
 
 #
 os.system("rm -rf *.last")
