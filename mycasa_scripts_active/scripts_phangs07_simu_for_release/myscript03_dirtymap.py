e24f8f357c5a0794fb27d80a0c0158bb0fcd4853
toshikisaito1005 <toshikisaito1005@gmail.com>
2020-10-01 17:50:00 +0200

    Commit at 2020-10-01 17:50:00

--- mycasa_scripts_active/scripts_phangs07_simu_for_release/myscript03_dirtymap.py
+++ mycasa_scripts_active/scripts_phangs07_simu_for_release/myscript03_dirtymap.py
@@ -26,6 +26,7 @@ def dirty_continuum(
 	# calc imsize
 	imsize_tmp = (fov + 46.85) / cell
 	imsize = 2**int(math.ceil(np.log2(imsize_tmp)))
+	print("# imsize = " + str(imsize))
 	#
 	default("tclean")
 	tclean(
@@ -55,20 +56,33 @@ def dirty_continuum(
 ### main
 ##############################
 # concat
-vis_12m = glob.glob(dir_project + this_proj + "/*12m*.ms")[0]
-vis_7m = glob.glob(dir_project + this_proj + "/*7m*.ms")[0]
+print("### concat")
+vis_12m = glob.glob(dir_project + this_proj + "/*12m.ms")[0]
+vis_7m = glob.glob(dir_project + this_proj + "/*7m.ms")[0]
 vis_12m7m = vis_12m.replace("12m","12m+7m")
 os.system("rm -rf " + vis_12m7m)
 concat(vis=[vis_12m,vis_7m], concatvis=vis_12m7m)
 
-#
-print("# dirty map creation: 7m")
-imagename="sim01_7m"
+# dirty map
+print("### dirty map creation: 7m")
+imagename = dir_project + this_proj + "/sim01_7m"
+dirty_continuum(vis_7m, imagename, fov=120, cell=1.0)
+
+print("### dirty map creation: 12m")
+imagename = dir_project + this_proj + "/sim01_12m"
 dirty_continuum(
 	vis_7m,
 	imagename,
 	fov=120,
-	cell=1.0)
+	cell=0.25)
+
+print("### dirty map creation: 7m")
+imagename = dir_project + this_proj + "/sim01_12+7m"
+dirty_continuum(
+	vis_7m,
+	imagename,
+	fov=120,
+	cell=0.25)
 
 #
 os.system("rm -rf *.last")
